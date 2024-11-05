4ed1f3b88ba276a01a2045b821a322d3568d0258
ypsitau <ypsitau@nifty.com>
2024-11-05 16:25:16 +0900

    implementing ST7789

--- ST7789/src/ST7789.cpp
+++ ST7789/src/ST7789.cpp
@@ -23,43 +23,29 @@ void ST7789::Raw::Initialize()
 	} else {
 		::spi_set_format(spi_, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
 	}
-
-
-	if (UsesCS()) {
-		gpio_init(gpio_CS_);
-	}
-	gpio_init(gpio_DC_);
-	gpio_init(gpio_RST_);
-	gpio_init(gpio_BL_);
-
-	if (UsesCS()) {
-		gpio_set_dir(gpio_CS_, GPIO_OUT);
-	}
-	gpio_set_dir(gpio_DC_, GPIO_OUT);
-	gpio_set_dir(gpio_RST_, GPIO_OUT);
-	gpio_set_dir(gpio_BL_, GPIO_OUT);
-
-	if (UsesCS()) {
-		::gpio_put(gpio_CS_, 1);
-	}
+	if (UsesCS()) ::gpio_init(gpio_CS_);
+	::gpio_init(gpio_DC_);
+	::gpio_init(gpio_RST_);
+	::gpio_init(gpio_BL_);
+	if (UsesCS()) ::gpio_set_dir(gpio_CS_, GPIO_OUT);
+	::gpio_set_dir(gpio_DC_, GPIO_OUT);
+	::gpio_set_dir(gpio_RST_, GPIO_OUT);
+	::gpio_set_dir(gpio_BL_, GPIO_OUT);
+	if (UsesCS()) ::gpio_put(gpio_CS_, 1);
 	::gpio_put(gpio_DC_, 1);
 	::gpio_put(gpio_RST_, 1);
 	::sleep_ms(100);
-	
 	// SWRESET (01h): Software Reset
 	SendCmd(0x01);
 	::sleep_ms(150);
-
 	// SLPOUT (11h): Sleep Out
 	SendCmd(0x11);
 	::sleep_ms(50);
-
 	// COLMOD (3Ah): Interface Pixel Format
 	// - RGB interface color format     = 65K of RGB interface
 	// - Control interface color format = 16bit/pixel
 	SendCmd(0x3a, 0x55);
 	::sleep_ms(10);
-
 	// MADCTL (36h): Memory Data Access Control
 	// - Page Address Order            = Top to Bottom
 	// - Column Address Order          = Left to Right
@@ -70,19 +56,15 @@ void ST7789::Raw::Initialize()
 	SendCmd(0x36, 0x00);
 	ColumnAddressSet(0, width_);
 	RowAddressSet(0, height_);
-
 	// INVON (21h): Display Inversion On
 	SendCmd(0x21);
 	::sleep_ms(10);
-
 	// NORON (13h): Normal Display Mode On
 	SendCmd(0x13);
 	::sleep_ms(10);
-
 	// DISPON (29h): Display On
 	SendCmd(0x29);
 	::sleep_ms(10);
-
 	::gpio_put(gpio_BL_, 1);
 }
 
@@ -94,16 +76,11 @@ void ST7789::Raw::SendCmd(uint8_t cmd, const void* data, int len)
 		::spi_set_format(spi_, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
 	}
 	dataModeFlag_ = false;
-
 	::sleep_us(1);
-	if (UsesCS()) {
-		::gpio_put(gpio_CS_, 0);
-	}
+	if (UsesCS()) ::gpio_put(gpio_CS_, 0);
 	::gpio_put(gpio_DC_, 0);
 	::sleep_us(1);
-	
 	::spi_write_blocking(spi_, &cmd, sizeof(cmd));
-	
 	if (len) {
 		::sleep_us(1);
 		::gpio_put(gpio_DC_, 1);
@@ -111,11 +88,8 @@ void ST7789::Raw::SendCmd(uint8_t cmd, const void* data, int len)
 		
 		::spi_write_blocking(spi_, reinterpret_cast<const uint8_t*>(data), len);
 	}
-
 	::sleep_us(1);
-	if (UsesCS()) {
-		::gpio_put(gpio_CS_, 1);
-	}
+	if (UsesCS()) ::gpio_put(gpio_CS_, 1);
 	::gpio_put(gpio_DC_, 1);
 	::sleep_us(1);
 }
@@ -124,32 +98,23 @@ void ST7789::Raw::PutPixel(uint16_t pixel)
 {
 	if (!dataModeFlag_) {
 		::sleep_us(1);
-		if (UsesCS()) {
-			::gpio_put(gpio_CS_, 0);
-		}
+		if (UsesCS()) ::gpio_put(gpio_CS_, 0);
 		::gpio_put(gpio_DC_, 0);
 		::sleep_us(1);
-
 		// RAMWR (2Ch): Memory Write
 		uint8_t cmd = 0x2c;
 		::spi_write_blocking(spi_, &cmd, sizeof(cmd));
-
 		::sleep_us(1);
-		if (UsesCS()) {
-			::gpio_put(gpio_CS_, 0);
-		}
+		if (UsesCS()) ::gpio_put(gpio_CS_, 0);
 		::gpio_put(gpio_DC_, 1);
 		::sleep_us(1);
-
 		if (UsesCS()) {
 			::spi_set_format(spi_, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
 		} else {
 			::spi_set_format(spi_, 16, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
 		}
-
 		dataModeFlag_ = true;
 	}
-
 	spi_write16_blocking(spi_, &pixel, 1);
 }
 
