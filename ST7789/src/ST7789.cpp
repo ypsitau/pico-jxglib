36ef0b73cfe87be9874e2f37e01b0e8521f23ceb
ypsitau <ypsitau@nifty.com>
2024-11-05 15:54:02 +0900

    implementing ST7789

--- ST7789/src/ST7789.cpp
+++ ST7789/src/ST7789.cpp
@@ -10,8 +10,44 @@ namespace jxglib {
 //------------------------------------------------------------------------------
 void ST7789::Initialize()
 {
-	
+
 }
 
+//------------------------------------------------------------------------------
+// ST7789::Raw
+//------------------------------------------------------------------------------
+void ST7789::Raw::SendCmd(uint8_t cmd, const void* data, int len)
+{
+	if (UsesCS()) {
+		spi_set_format(spi_, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+	} else {
+		spi_set_format(spi_, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
+	}
+	st7789_data_mode = false;
+
+	sleep_us(1);
+	if (UsesCS()) {
+		gpio_put(gpio_CS_, 0);
+	}
+	gpio_put(gpio_DC_, 0);
+	sleep_us(1);
+	
+	spi_write_blocking(spi_, &cmd, sizeof(cmd));
+	
+	if (len) {
+		sleep_us(1);
+		gpio_put(gpio_DC_, 1);
+		sleep_us(1);
+		
+		spi_write_blocking(spi_, reinterpret_cast<const uint8_t*>(data), len);
+	}
+
+	sleep_us(1);
+	if (UsesCS()) {
+		gpio_put(gpio_CS_, 1);
+	}
+	gpio_put(gpio_DC_, 1);
+	sleep_us(1);
+}
 
 }
--- ST7789/testapp/testapp.cpp
+++ ST7789/testapp/testapp.cpp
@@ -2,13 +2,11 @@
 #include "jxglib/ST7789.h"
 
 struct st7789_config {
-    spi_inst_t* spi;
-    uint gpio_din;
-    uint gpio_clk;
-    int gpio_cs;
-    uint gpio_dc;
-    uint gpio_rst;
-    uint gpio_bl;
+	spi_inst_t* spi;
+	uint gpio_dc;
+	uint gpio_rst;
+	uint gpio_bl;
+	int gpio_cs;
 };
 
 static struct st7789_config st7789_cfg;
@@ -18,237 +16,240 @@ static bool st7789_data_mode = false;
 
 static void st7789_SendCmd(uint8_t cmd, const uint8_t* data, size_t len)
 {
-    if (st7789_cfg.gpio_cs > -1) {
-        spi_set_format(st7789_cfg.spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
-    } else {
-        spi_set_format(st7789_cfg.spi, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
-    }
-    st7789_data_mode = false;
+	if (st7789_cfg.gpio_cs > -1) {
+		spi_set_format(st7789_cfg.spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+	} else {
+		spi_set_format(st7789_cfg.spi, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
+	}
+	st7789_data_mode = false;
 
-    sleep_us(1);
-    if (st7789_cfg.gpio_cs > -1) {
-        gpio_put(st7789_cfg.gpio_cs, 0);
-    }
-    gpio_put(st7789_cfg.gpio_dc, 0);
-    sleep_us(1);
-    
-    spi_write_blocking(st7789_cfg.spi, &cmd, sizeof(cmd));
-    
-    if (len) {
-        sleep_us(1);
-        gpio_put(st7789_cfg.gpio_dc, 1);
-        sleep_us(1);
-        
-        spi_write_blocking(st7789_cfg.spi, data, len);
-    }
+	sleep_us(1);
+	if (st7789_cfg.gpio_cs > -1) {
+		gpio_put(st7789_cfg.gpio_cs, 0);
+	}
+	gpio_put(st7789_cfg.gpio_dc, 0);
+	sleep_us(1);
+	
+	spi_write_blocking(st7789_cfg.spi, &cmd, sizeof(cmd));
+	
+	if (len) {
+		sleep_us(1);
+		gpio_put(st7789_cfg.gpio_dc, 1);
+		sleep_us(1);
+		
+		spi_write_blocking(st7789_cfg.spi, data, len);
+	}
 
-    sleep_us(1);
-    if (st7789_cfg.gpio_cs > -1) {
-        gpio_put(st7789_cfg.gpio_cs, 1);
-    }
-    gpio_put(st7789_cfg.gpio_dc, 1);
-    sleep_us(1);
+	sleep_us(1);
+	if (st7789_cfg.gpio_cs > -1) {
+		gpio_put(st7789_cfg.gpio_cs, 1);
+	}
+	gpio_put(st7789_cfg.gpio_dc, 1);
+	sleep_us(1);
 }
 
 void st7789_ColumnAddressSet(uint16_t xs, uint16_t xe)
 {
-    uint8_t data[] = {
-        static_cast<uint8_t>(xs >> 8),
-        static_cast<uint8_t>(xs & 0xff),
-        static_cast<uint8_t>(xe >> 8),
-        static_cast<uint8_t>(xe & 0xff),
-    };
+	uint8_t data[] = {
+		static_cast<uint8_t>(xs >> 8),
+		static_cast<uint8_t>(xs & 0xff),
+		static_cast<uint8_t>(xe >> 8),
+		static_cast<uint8_t>(xe & 0xff),
+	};
 
-    // CASET (2Ah): Column Address Set
-    st7789_SendCmd(0x2a, data, sizeof(data));
+	// CASET (2Ah): Column Address Set
+	st7789_SendCmd(0x2a, data, sizeof(data));
 }
 
 void st7789_RowAddressSet(uint16_t ys, uint16_t ye)
 {
-    uint8_t data[] = {
-        static_cast<uint8_t>(ys >> 8),
-        static_cast<uint8_t>(ys & 0xff),
-        static_cast<uint8_t>(ye >> 8),
-        static_cast<uint8_t>(ye & 0xff),
-    };
+	uint8_t data[] = {
+		static_cast<uint8_t>(ys >> 8),
+		static_cast<uint8_t>(ys & 0xff),
+		static_cast<uint8_t>(ye >> 8),
+		static_cast<uint8_t>(ye & 0xff),
+	};
 
-    // RASET (2Bh): Row Address Set
-    st7789_SendCmd(0x2b, data, sizeof(data));
+	// RASET (2Bh): Row Address Set
+	st7789_SendCmd(0x2b, data, sizeof(data));
 }
 
 void st7789_Initialize(const struct st7789_config* config, uint16_t width, uint16_t height)
 {
-    memcpy(&st7789_cfg, config, sizeof(st7789_cfg));
-    st7789_width = width;
-    st7789_height = height;
+	memcpy(&st7789_cfg, config, sizeof(st7789_cfg));
+	st7789_width = width;
+	st7789_height = height;
 
-    spi_init(st7789_cfg.spi, 125 * 1000 * 1000);
-    if (st7789_cfg.gpio_cs > -1) {
-        spi_set_format(st7789_cfg.spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
-    } else {
-        spi_set_format(st7789_cfg.spi, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
-    }
+	spi_init(st7789_cfg.spi, 125 * 1000 * 1000);
+	if (st7789_cfg.gpio_cs > -1) {
+		spi_set_format(st7789_cfg.spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+	} else {
+		spi_set_format(st7789_cfg.spi, 8, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
+	}
 
-    gpio_set_function(st7789_cfg.gpio_din, GPIO_FUNC_SPI);
-    gpio_set_function(st7789_cfg.gpio_clk, GPIO_FUNC_SPI);
 
-    if (st7789_cfg.gpio_cs > -1) {
-        gpio_init(st7789_cfg.gpio_cs);
-    }
-    gpio_init(st7789_cfg.gpio_dc);
-    gpio_init(st7789_cfg.gpio_rst);
-    gpio_init(st7789_cfg.gpio_bl);
+	if (st7789_cfg.gpio_cs > -1) {
+		gpio_init(st7789_cfg.gpio_cs);
+	}
+	gpio_init(st7789_cfg.gpio_dc);
+	gpio_init(st7789_cfg.gpio_rst);
+	gpio_init(st7789_cfg.gpio_bl);
 
-    if (st7789_cfg.gpio_cs > -1) {
-        gpio_set_dir(st7789_cfg.gpio_cs, GPIO_OUT);
-    }
-    gpio_set_dir(st7789_cfg.gpio_dc, GPIO_OUT);
-    gpio_set_dir(st7789_cfg.gpio_rst, GPIO_OUT);
-    gpio_set_dir(st7789_cfg.gpio_bl, GPIO_OUT);
+	if (st7789_cfg.gpio_cs > -1) {
+		gpio_set_dir(st7789_cfg.gpio_cs, GPIO_OUT);
+	}
+	gpio_set_dir(st7789_cfg.gpio_dc, GPIO_OUT);
+	gpio_set_dir(st7789_cfg.gpio_rst, GPIO_OUT);
+	gpio_set_dir(st7789_cfg.gpio_bl, GPIO_OUT);
 
-    if (st7789_cfg.gpio_cs > -1) {
-        gpio_put(st7789_cfg.gpio_cs, 1);
-    }
-    gpio_put(st7789_cfg.gpio_dc, 1);
-    gpio_put(st7789_cfg.gpio_rst, 1);
-    sleep_ms(100);
-    
-    // SWRESET (01h): Software Reset
-    st7789_SendCmd(0x01, NULL, 0);
-    sleep_ms(150);
+	if (st7789_cfg.gpio_cs > -1) {
+		gpio_put(st7789_cfg.gpio_cs, 1);
+	}
+	gpio_put(st7789_cfg.gpio_dc, 1);
+	gpio_put(st7789_cfg.gpio_rst, 1);
+	sleep_ms(100);
+	
+	// SWRESET (01h): Software Reset
+	st7789_SendCmd(0x01, NULL, 0);
+	sleep_ms(150);
 
-    // SLPOUT (11h): Sleep Out
-    st7789_SendCmd(0x11, NULL, 0);
-    sleep_ms(50);
+	// SLPOUT (11h): Sleep Out
+	st7789_SendCmd(0x11, NULL, 0);
+	sleep_ms(50);
 
-    // COLMOD (3Ah): Interface Pixel Format
-    // - RGB interface color format     = 65K of RGB interface
-    // - Control interface color format = 16bit/pixel
-    uint8_t tmp1[] = { 0x55 };
-    //st7789_SendCmd(0x3a, (uint8_t[]){ 0x55 }, 1);
-    st7789_SendCmd(0x3a, tmp1, 1);
-    sleep_ms(10);
+	// COLMOD (3Ah): Interface Pixel Format
+	// - RGB interface color format     = 65K of RGB interface
+	// - Control interface color format = 16bit/pixel
+	uint8_t tmp1[] = { 0x55 };
+	//st7789_SendCmd(0x3a, (uint8_t[]){ 0x55 }, 1);
+	st7789_SendCmd(0x3a, tmp1, 1);
+	sleep_ms(10);
 
-    // MADCTL (36h): Memory Data Access Control
-    // - Page Address Order            = Top to Bottom
-    // - Column Address Order          = Left to Right
-    // - Page/Column Order             = Normal Mode
-    // - Line Address Order            = LCD Refresh Top to Bottom
-    // - RGB/BGR Order                 = RGB
-    // - Display Data Latch Data Order = LCD Refresh Left to Right
-    uint8_t tmp2[] = { 0x00 };
-    //st7789_SendCmd(0x36, (uint8_t[]){ 0x00 }, 1);
-    st7789_SendCmd(0x36, tmp2, 1);
+	// MADCTL (36h): Memory Data Access Control
+	// - Page Address Order            = Top to Bottom
+	// - Column Address Order          = Left to Right
+	// - Page/Column Order             = Normal Mode
+	// - Line Address Order            = LCD Refresh Top to Bottom
+	// - RGB/BGR Order                 = RGB
+	// - Display Data Latch Data Order = LCD Refresh Left to Right
+	uint8_t tmp2[] = { 0x00 };
+	//st7789_SendCmd(0x36, (uint8_t[]){ 0x00 }, 1);
+	st7789_SendCmd(0x36, tmp2, 1);
    
-    st7789_ColumnAddressSet(0, width);
-    st7789_RowAddressSet(0, height);
+	st7789_ColumnAddressSet(0, width);
+	st7789_RowAddressSet(0, height);
 
-    // INVON (21h): Display Inversion On
-    st7789_SendCmd(0x21, NULL, 0);
-    sleep_ms(10);
+	// INVON (21h): Display Inversion On
+	st7789_SendCmd(0x21, NULL, 0);
+	sleep_ms(10);
 
-    // NORON (13h): Normal Display Mode On
-    st7789_SendCmd(0x13, NULL, 0);
-    sleep_ms(10);
+	// NORON (13h): Normal Display Mode On
+	st7789_SendCmd(0x13, NULL, 0);
+	sleep_ms(10);
 
-    // DISPON (29h): Display On
-    st7789_SendCmd(0x29, NULL, 0);
-    sleep_ms(10);
+	// DISPON (29h): Display On
+	st7789_SendCmd(0x29, NULL, 0);
+	sleep_ms(10);
 
-    gpio_put(st7789_cfg.gpio_bl, 1);
+	gpio_put(st7789_cfg.gpio_bl, 1);
 }
 
 void st7789_PutPixel(uint16_t pixel)
 {
-    if (!st7789_data_mode) {
-        sleep_us(1);
-        if (st7789_cfg.gpio_cs > -1) {
-            gpio_put(st7789_cfg.gpio_cs, 0);
-        }
-        gpio_put(st7789_cfg.gpio_dc, 0);
-        sleep_us(1);
+	if (!st7789_data_mode) {
+		sleep_us(1);
+		if (st7789_cfg.gpio_cs > -1) {
+			gpio_put(st7789_cfg.gpio_cs, 0);
+		}
+		gpio_put(st7789_cfg.gpio_dc, 0);
+		sleep_us(1);
 
-        // RAMWR (2Ch): Memory Write
-        uint8_t cmd = 0x2c;
-        spi_write_blocking(st7789_cfg.spi, &cmd, sizeof(cmd));
+		// RAMWR (2Ch): Memory Write
+		uint8_t cmd = 0x2c;
+		spi_write_blocking(st7789_cfg.spi, &cmd, sizeof(cmd));
 
-        sleep_us(1);
-        if (st7789_cfg.gpio_cs > -1) {
-            gpio_put(st7789_cfg.gpio_cs, 0);
-        }
-        gpio_put(st7789_cfg.gpio_dc, 1);
-        sleep_us(1);
+		sleep_us(1);
+		if (st7789_cfg.gpio_cs > -1) {
+			gpio_put(st7789_cfg.gpio_cs, 0);
+		}
+		gpio_put(st7789_cfg.gpio_dc, 1);
+		sleep_us(1);
 
-        if (st7789_cfg.gpio_cs > -1) {
-            spi_set_format(st7789_cfg.spi, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
-        } else {
-            spi_set_format(st7789_cfg.spi, 16, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
-        }
+		if (st7789_cfg.gpio_cs > -1) {
+			spi_set_format(st7789_cfg.spi, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+		} else {
+			spi_set_format(st7789_cfg.spi, 16, SPI_CPOL_1, SPI_CPHA_1, SPI_MSB_FIRST);
+		}
 
-        st7789_data_mode = true;
-    }
+		st7789_data_mode = true;
+	}
 
-    spi_write16_blocking(st7789_cfg.spi, &pixel, 1);
+	spi_write16_blocking(st7789_cfg.spi, &pixel, 1);
 }
 
 void st7789_SetCursor(uint16_t x, uint16_t y)
 {
-    st7789_ColumnAddressSet(x, st7789_width);
-    st7789_RowAddressSet(y, st7789_height);
+	st7789_ColumnAddressSet(x, st7789_width);
+	st7789_RowAddressSet(y, st7789_height);
 }
 
 void st7789_ScrollVertical(uint16_t row)
 {
-    uint8_t data[] = {
-        static_cast<uint8_t>((row >> 8) & 0xff),
-        static_cast<uint8_t>(row & 0x00ff)
-    };
+	uint8_t data[] = {
+		static_cast<uint8_t>((row >> 8) & 0xff),
+		static_cast<uint8_t>(row & 0x00ff)
+	};
 
-    // VSCSAD (37h): Vertical Scroll Start Address of RAM 
-    st7789_SendCmd(0x37, data, sizeof(data));
+	// VSCSAD (37h): Vertical Scroll Start Address of RAM 
+	st7789_SendCmd(0x37, data, sizeof(data));
 }
 
 void st7789_Fill(uint16_t pixel)
 {
-    int num_pixels = st7789_width * st7789_height;
+	int num_pixels = st7789_width * st7789_height;
 
-    st7789_SetCursor(0, 0);
+	st7789_SetCursor(0, 0);
 
-    for (int i = 0; i < num_pixels; i++) {
-        st7789_PutPixel(pixel);
-    }
+	for (int i = 0; i < num_pixels; i++) {
+		st7789_PutPixel(pixel);
+	}
 }
 
 //------------------------------------------------------------------------------
 int main()
 {
-    // lcd configuration
-    static const struct st7789_config lcd_config = {
-        .spi      = spi0,
-        .gpio_din = PICO_DEFAULT_SPI_TX_PIN,
-        .gpio_clk = PICO_DEFAULT_SPI_SCK_PIN,
-        .gpio_cs  = -1, // PICO_DEFAULT_SPI_CSN_PIN,
-        .gpio_dc  = 20,
-        .gpio_rst = 21,
-        .gpio_bl  = 22,
-    };
-    stdio_init_all();
-    // initialize the lcd
-    st7789_Initialize(&lcd_config, 240, 240);
+	static const struct st7789_config lcd_config = {
+		.spi      = spi0,
+		.gpio_dc  = 20,
+		.gpio_rst = 21,
+		.gpio_bl  = 22,
+		.gpio_cs  = -1, // PICO_DEFAULT_SPI_CSN_PIN,
+	};
+	stdio_init_all();
+	::gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
+	::gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
+	st7789_Initialize(&lcd_config, 240, 240);
 
-    // make screen black
-    st7789_Fill(0x0000);
+	st7789_Fill(0x0000);
 
-    while (1) {
-        // create a random x, y, and color value
-        int rand_x = rand() % 240;
-        int rand_y = rand() % 240;
-        uint16_t rand_color = rand() % 0xffff;
-        
-        // move the cursor to the random x and y position
-        st7789_SetCursor(rand_x, rand_y);
+	while (1) {
+		st7789_Fill(0x0000);
+		::sleep_ms(1000);
+		st7789_Fill(0xffff);
+		::sleep_ms(1000);
+	}
+#if 0
+	while (1) {
+		// create a random x, y, and color value
+		int rand_x = rand() % 240;
+		int rand_y = rand() % 240;
+		uint16_t rand_color = rand() % 0xffff;
+		
+		// move the cursor to the random x and y position
+		st7789_SetCursor(rand_x, rand_y);
 
-        // put the random color as the pixel value
-        st7789_PutPixel(rand_color);
-    }
+		// put the random color as the pixel value
+		st7789_PutPixel(rand_color);
+	}
+#endif
 }
