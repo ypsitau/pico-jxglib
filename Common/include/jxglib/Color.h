5e15cc48ac06cf5ae2a5a95b36f6637433c51f90
ypsitau <ypsitau@nifty.com>
2024-11-06 21:50:15 +0900

    implementing st7789

--- Common/include/jxglib/Color.h
+++ Common/include/jxglib/Color.h
@@ -14,12 +14,23 @@ namespace jxglib {
 struct Color {
 	uint8_t r, g, b;
 public:
-	static const Color Zero;
-	static const Color Black;
-	static const Color Red;
-	static const Color Green;
-	static const Color Blue;
-	static const Color White;
+	static const Color zero;
+	static const Color black;
+	static const Color silver;
+	static const Color gray;
+	static const Color white;
+	static const Color maroon;
+	static const Color red;
+	static const Color purple;
+	static const Color fuchsia;
+	static const Color green;
+	static const Color lime;
+	static const Color olive;
+	static const Color yellow;
+	static const Color navy;
+	static const Color blue;
+	static const Color teal;
+	static const Color aqua;
 public:
 	constexpr static uint16_t RGB565(uint8_t r, uint8_t g, uint8_t b) { return
 		((static_cast<uint16_t>(r) & 0xf8) << 8) |
--- Common/src/Color.cpp
+++ Common/src/Color.cpp
@@ -8,11 +8,22 @@ namespace jxglib {
 //------------------------------------------------------------------------------
 // Color
 //------------------------------------------------------------------------------
-const Color Color::Zero { 0, 0, 0 };
-const Color Color::Black { 0, 0, 0 };
-const Color Color::Red { 255, 0, 0 };
-const Color Color::Green { 0, 255, 0 };
-const Color Color::Blue { 0, 0, 255 };
-const Color Color::White { 255, 255, 255 };
+const Color Color::zero		{   0,   0,   0 };
+const Color Color::black	{   0,   0,   0 };
+const Color Color::silver	{ 192, 192, 192 };
+const Color Color::gray		{ 128, 128, 128 };
+const Color Color::white	{ 255, 255, 255 };
+const Color Color::maroon	{ 128,   0,   0 };
+const Color Color::red		{ 255,   0,   0 };
+const Color Color::purple	{ 128,   0, 128 };
+const Color Color::fuchsia	{ 255,   0, 255 };
+const Color Color::green	{   0, 128,   0 };
+const Color Color::lime		{   0, 255,   0 };
+const Color Color::olive	{ 128, 128,   0 };
+const Color Color::yellow	{ 255, 255,   0 };
+const Color Color::navy		{   0,   0, 128 };
+const Color Color::blue		{   0,   0, 255 };
+const Color Color::teal		{   0, 128, 128 };
+const Color Color::aqua		{   0, 255, 255 };
 
 }
