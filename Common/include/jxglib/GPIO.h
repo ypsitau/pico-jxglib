7ec181c27d16dc682860da46e790748d588e61b7
ypsitau <ypsitau@nifty.com>
2024-11-11 21:40:05 +0900

    implementing GPIO

--- Common/include/jxglib/GPIO.h
+++ Common/include/jxglib/GPIO.h
@@ -30,7 +30,64 @@ public:
 	const GPIO& set_function_PIO1() const			{ ::gpio_set_function(pin, GPIO_FUNC_PIO1); return *this; }
 	const GPIO& set_function_USB() const			{ ::gpio_set_function(pin, GPIO_FUNC_USB); return *this; }
 public:
-	const GPIO& pull_up() const						{ ::gpio_pull_up(pin); return *this; }
+	//const GPIO& set_function(enum gpio_function fn) const	{ ::gpio_set_function(pin, fn); return *this; }
+	//enum gpio_function get_function() const 				{ return ::gpio_get_function(pin); }
+	const GPIO& set_pulls(bool up, bool down) const			{ ::gpio_set_pulls(pin, up, down); return *this; }
+	const GPIO& pull_up() const								{ ::gpio_pull_up(pin); return *this; }
+	bool is_pulled_up() const								{ return ::gpio_is_pulled_up(pin); }
+	const GPIO& pull_down() const							{ ::gpio_pull_down(pin); return *this; }
+	bool is_pulled_down() const								{ return ::gpio_is_pulled_down(pin); }
+	const GPIO& disable_pulls() const						{ ::gpio_disable_pulls(pin); return *this; }
+	const GPIO& set_irqover(uint value) const				{ ::gpio_set_irqover(pin, value); return *this; }
+	const GPIO& set_outover(uint value) const				{ ::gpio_set_outover(pin, value); return *this; }
+	const GPIO& set_inover(uint value) const				{ ::gpio_set_inover(pin, value); return *this; }
+	const GPIO& set_oeover(uint value) const				{ ::gpio_set_oeover(pin, value); return *this; }
+	const GPIO& set_input_enabled(bool enabled) const		{ ::gpio_set_input_enabled(pin, enabled); return *this; }
+	const GPIO& set_input_hysteresis_enabled(bool enabled) const { ::gpio_set_input_hysteresis_enabled(pin, enabled); return *this; }
+	bool gpio_is_input_hysteresis_enabled() const			{ return ::gpio_is_input_hysteresis_enabled(pin); }
+	const GPIO& set_slew_rate(enum gpio_slew_rate slew) const { ::gpio_set_slew_rate(pin, slew); return *this; }
+	enum gpio_slew_rate gpio_get_slew_rate() const			{ return ::gpio_get_slew_rate(pin); }
+	const GPIO& set_drive_strength(enum gpio_drive_strength drive) const
+															{ ::gpio_set_drive_strength(pin, drive); return *this; }
+	enum gpio_drive_strength gpio_get_drive_strength() const{ return ::gpio_get_drive_strength(pin); }
+	const GPIO& set_irq_enabled(uint32_t event_mask, bool enabled) const
+															{ ::gpio_set_irq_enabled(pin, event_mask, enabled); return *this; }
+	void set_irq_callback(gpio_irq_callback_t callback)		{ ::gpio_set_irq_callback(callback); }
+	const GPIO& set_irq_enabled_with_callback(uint32_t event_mask, bool enabled, gpio_irq_callback_t callback) const
+															{ ::gpio_set_irq_enabled_with_callback(pin, event_mask, enabled, callback); return *this; }
+	const GPIO& set_dormant_irq_enabled(uint32_t event_mask, bool enabled) const
+															{ ::gpio_set_dormant_irq_enabled(pin, event_mask, enabled); return *this; }
+	uint32_t gpio_get_irq_event_mask() const				{ return ::gpio_get_irq_event_mask(pin); }
+	const GPIO& acknowledge_irq(uint32_t event_mask) const	{ ::gpio_acknowledge_irq(pin, event_mask); return *this; }
+	const GPIO& add_raw_irq_handler_with_order_priority_masked(uint32_t gpio_mask, irq_handler_t handler, uint8_t order_priority) const
+															{ ::gpio_add_raw_irq_handler_with_order_priority_masked(gpio_mask, handler, order_priority); return *this; }
+	const GPIO& add_raw_irq_handler_with_order_priority(irq_handler_t handler, uint8_t order_priority) const
+															{ ::gpio_add_raw_irq_handler_with_order_priority(pin, handler, order_priority); return *this; }
+	static void add_raw_irq_handler_masked(uint32_t gpio_mask, irq_handler_t handler)
+															{ add_raw_irq_handler_masked(gpio_mask, handler); }
+	const GPIO& add_raw_irq_handler(irq_handler_t handler) const { ::gpio_add_raw_irq_handler(pin, handler); return *this; }
+	const GPIO& remove_raw_irq_handler_masked(uint32_t gpio_mask, irq_handler_t handler)
+															{ ::gpio_remove_raw_irq_handler_masked(gpio_mask, handler); return *this; }
+	const GPIO& remove_raw_irq_handler(irq_handler_t handler) const { ::gpio_remove_raw_irq_handler(pin, handler); return *this; }
+	const GPIO& init() const								{ ::gpio_init(pin); return *this; }
+	const GPIO& deinit() const								{ ::gpio_deinit(pin); return *this; }
+	void init_mask(uint32_t gpio_mask)						{ ::gpio_init_mask(gpio_mask); }
+	bool get() const										{ return ::gpio_get(pin); }
+	static uint32_t get_all()								{ return ::gpio_get_all(); }
+	static void set_mask(uint32_t mask)						{ ::gpio_set_mask(mask); }
+	static void clr_mask(uint32_t mask)						{ ::gpio_clr_mask(mask); }
+	static void xor_mask(uint32_t mask)						{ ::gpio_xor_mask(mask); }
+	static void put_masked(uint32_t mask, uint32_t value)	{ ::gpio_put_masked(mask, value); }
+	static void put_all(uint32_t value)						{ ::gpio_put_all(value); }
+	const GPIO& put(bool value) const						{ ::gpio_put(pin, value); return *this; }
+	bool gpio_get_out_level() const							{ return ::gpio_get_out_level(pin); }
+	static void set_dir_out_masked(uint32_t mask)			{ ::gpio_set_dir_out_masked(mask); }
+	static void gpio_set_dir_in_masked(uint32_t mask)		{ ::gpio_set_dir_in_masked(mask); }
+	static void gpio_set_dir_masked(uint32_t mask, uint32_t value) { ::gpio_set_dir_masked(mask, value); }
+	void gpio_set_dir_all_bits(uint32_t values)				{ ::gpio_set_dir_all_bits(values); }
+	const GPIO& set_dir(bool out) const						{ ::gpio_set_dir(pin, out); return *this; }
+	bool gpio_is_dir_out() const							{ return ::gpio_is_dir_out(pin); }
+	uint gpio_get_dir() const								{ return ::gpio_get_dir(pin); }
 };
 
 class GPIO_Invalid_T : public GPIO {
