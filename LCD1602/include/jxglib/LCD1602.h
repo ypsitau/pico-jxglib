ad90a5510b56db4d0e327ae5d5da9085bf592011
ypsitau <ypsitau@nifty.com>
2024-11-17 08:19:37 +0900

    Implementing LCD1602

--- LCD1602/include/jxglib/LCD1602.h
+++ LCD1602/include/jxglib/LCD1602.h
@@ -20,6 +20,26 @@ public:
 	public:
 		Raw(i2c_inst_t* i2c, uint8_t addr) : i2c_{i2c}, addr_{addr} {}
 	public:
+		uint8_t GetAddr() const { return addr_; }
+	public:
+		void WriteByte(uint8_t value) const {
+			::i2c_write_blocking(i2c_, addr_, &value, 1, false);
+		}
+		void SendNibble(uint8_t value) {
+			const uint64_t usecDelay = 600;
+			WriteByte(value);
+			::sleep_us(usecDelay);
+			WriteByte(value | 0x04);
+			::sleep_us(usecDelay);
+			WriteByte(value);
+			::sleep_us(usecDelay);
+		}
+		// bit3: backlight
+		// bit0: character/command
+		void SendByte(uint8_t value, uint8_t mode) {
+			SendNibble((value & 0xf0) | mode);
+			SendNibble((value << 4) | mode);
+		}
 		
 	};
 public:
