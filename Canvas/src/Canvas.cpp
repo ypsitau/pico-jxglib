a566f253afff232e8285b0fd559ad329e6c40a07
ypsitau <ypsitau@nifty.com>
2024-11-18 21:22:16 +0900

    implementing Canvas

--- Canvas/src/Canvas.cpp
+++ Canvas/src/Canvas.cpp
@@ -62,6 +62,30 @@ void Canvas::DispatcherRGB565::DrawRectFill_(Image& imageOwn, int x, int y, int 
 void Canvas::DispatcherRGB565::DrawBitmap_(Image& imageOwn, int x, int y, const void* data, int width, int height,
 	const Color& color, const Color* pColorBg, int scaleX, int scaleY) const
 {
+	int nDots = width * height;
+	int bytes = (width + 7) / 8 * height;
+	const uint8_t* pSrcLeft = reinterpret_cast<const uint8_t*>(data);
+	const ColorRGB565 colorFg(color);
+	const ColorRGB565 colorBg(pColorBg? ColorRGB565(*pColorBg) : ColorRGB565::black);
+	using Writer = Image::Writer<Image::PutColorRGB565_DstRGB565>;
+	Image::Writer writer(Writer::HorzFromNW(imageOwn, x, y, width, height));
+	for (int iRow = 0; iRow < height; iRow++) {
+		const uint8_t* pSrc;
+		for (int iScaleY = 0; iScaleY < scaleY; iScaleY++) {
+			pSrc = pSrcLeft;
+			int iBit = 0;
+			uint8_t bits = *pSrc++;
+			for (int iCol = 0; iCol < width; iCol++, iBit++, bits <<= 1) {
+				if (iBit == 8) {
+					iBit = 0;
+					bits = *pSrc++;
+				}
+				const ColorRGB565& color = (bits & 0x80)? colorFg : colorBg;
+				for (int iScaleX = 0; iScaleX < scaleX; iScaleX++) writer.WriteForward(color);
+			}
+		}
+		pSrcLeft = pSrc;
+	}
 }
 
 void Canvas::DispatcherRGB565::DrawImage_(Image& imageOwn, int x, int y, const Image& image, const Rect* pRectClip, ImageDir imageDir) const
@@ -76,7 +100,7 @@ void Canvas::DispatcherRGB565::DrawImage_(Image& imageOwn, int x, int y, const I
 	if (!AdjustRange(&x, &width, 0, imageOwn.GetWidth(), &xSkip)) return;
 	if (!AdjustRange(&y, &height, 0, imageOwn.GetHeight(), &ySkip)) return;
 	using Writer = Image::Writer<Image::PutColorRGB565_DstRGB565>;
-	Image::Writer writer(Writer::HorzFromNW(imageOwn, 0, 0, imageOwn.GetWidth(), imageOwn.GetHeight()));
+	Image::Writer writer(Writer::HorzFromNW(imageOwn, x, y, width, height));
 	if (image.GetFormat().IsGray()) {
 		using Reader = Image::Reader<Image::GetColorRGB565_SrcGray>;
 		Image::Reader reader(Reader::Create(image, xSkip, ySkip, width, height, imageDir));
--- test/test-Canvas/test-Canvas.cpp
+++ test/test-Canvas/test-Canvas.cpp
@@ -3,6 +3,7 @@
 #include "jxglib/Canvas.h"
 #include "jxglib/ST7789.h"
 #include "image/image_cat_240x320.h"
+#include "Font/shinonome16.h"
 
 using namespace jxglib;
 
@@ -20,6 +21,19 @@ int main()
 	//isplay.DrawRectFill(0, 0, 100, 100);
 	int x = 0, y = 0;
 	//canvas.Clear();
+	const char* str =
+		"親譲りの無鉄砲で子供のときから損ばかりしている。小学校の時分学校の二階から飛び降りて一週間腰を"
+		"抜かしたことがある。なぜそんなむやみなことをしたかと聞く人があるかもしれない。"
+		"別段深い理由でもない。新築の二階から首を出していたら同級生の一人が冗談に幾ら威張っても、"
+		"そこから飛び降りることはできまい。弱虫やーい。とはやしたからである。"
+		"小使いに負ぶさって帰ってきたとき、親父が大きな眼をして二階から飛び降りて腰を抜かすやつが"
+		"あるかといったから、この次は抜かさずに飛んでみせますと答えた。";
+	canvas.SetFont(Font::shinonome16);
+	//canvas.DrawImage(0, 0, image_cat_240x320, nullptr, Canvas::ImageDir::Rotate0);
+	canvas.SetColor(Color::white);
+	canvas.DrawStringWrap(0, 0, str);
+	canvas.Refresh();
+#if 0
 	for (;;) {
 		canvas.DrawImage(0, 0, image_cat_240x320, nullptr, Canvas::ImageDir::Rotate0);
 		canvas.Refresh();
@@ -30,4 +44,5 @@ int main()
 		canvas.DrawImage(0, 0, image_cat_240x320, nullptr, Canvas::ImageDir::Rotate270);
 		canvas.Refresh();
 	}
+#endif
 }
