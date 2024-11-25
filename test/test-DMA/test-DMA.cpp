c75c6ed4198ec67676e5be58ceec5bfa7730dce0
ypsitau <ypsitau@nifty.com>
2024-11-25 21:50:53 +0900

    implementing DMA

--- test/test-DMA/test-DMA.cpp
+++ test/test-DMA/test-DMA.cpp
@@ -67,11 +67,44 @@ void test_MemoryToPeripheral()
 	channel.unclaim();
 }
 
+void test_MemoryToMemory_SniffCRC()
+{
+	const char src[] = "DMA: Memory to Memory";
+	char dst[count_of(src)];
+	DMA::Channel channel(DMA::claim_unused_channel(true));
+	DMA::ChannelConfig config;
+	DMA::sniffer_set_data_accumulator(static_cast<uint32_t>(-1));
+	DMA::sniffer_set_output_reverse_enabled(true);
+	config.set_enable(true)
+		.set_transfer_data_size(DMA_SIZE_8)
+		.set_read_increment(true)
+		.set_write_increment(true)
+		.set_dreq(DREQ_FORCE) // see RP2040 Datasheet 2.5.3.1 System DREQ Table
+		.set_chain_to(channel)
+		.set_ring_to_read(0)
+		.set_bswap(false)
+		.set_irq_quiet(false)
+		.set_sniff_enable(true) // enable CRC calculation
+		.set_high_priority(false);
+	channel.sniffer_enable(DMA_SNIFF_CTRL_CALC_VALUE_CRC32R, true)
+		.set_config(config)
+		.set_read_addr(src)
+		.set_write_addr(dst)
+		.set_trans_count_trig(count_of(src));
+	channel.wait_for_finish_blocking();
+	channel.unclaim();
+	uint32_t crc = DMA::sniffer_get_data_accumulator();
+	::printf("Transferred by DMA#%d: %s\n", static_cast<uint>(channel), dst);
+	::printf("CRC: %08x %08x\n", crc, CalcCRC32(src, count_of(src), static_cast<uint32_t>(-1)));
+}
+
 void test_MemoryToPeripheral_SniffCRC()
 {
 	const char src[] = "Memory to Peripheral (UART)\r\n";
 	DMA::Channel channel(DMA::claim_unused_channel(true));
 	DMA::ChannelConfig config;
+	DMA::sniffer_set_data_accumulator(static_cast<uint32_t>(-1));
+	DMA::sniffer_set_output_reverse_enabled(true);
 	config.set_enable(true)
 		.set_transfer_data_size(DMA_SIZE_8)
 		.set_read_increment(true)
@@ -83,10 +116,8 @@ void test_MemoryToPeripheral_SniffCRC()
 		.set_irq_quiet(false)
 		.set_sniff_enable(true) // enable CRC calculation
 		.set_high_priority(false);
-	DMA::sniffer_set_data_accumulator(static_cast<uint32_t>(-1));
-	DMA::sniffer_set_output_reverse_enabled(true);
-	channel.sniffer_enable(DMA_SNIFF_CTRL_CALC_VALUE_CRC32R, true);
-	channel.set_config(config)
+	channel.sniffer_enable(DMA_SNIFF_CTRL_CALC_VALUE_CRC32R, true)
+		.set_config(config)
 		.set_read_addr(src)
 		.set_write_addr(&::uart_get_hw(uart_default)->dr)
 		.set_trans_count_trig(count_of(src));
@@ -165,8 +196,10 @@ void test_MemoryToPeripheral_Chain()
 int main()
 {
 	::stdio_init_all();
+	::printf("----\n");
 	//test_MemoryToMemory();
 	//test_MemoryToPeripheral();
 	//test_MemoryToPeripheral_Chain();
+	test_MemoryToMemory_SniffCRC();
 	test_MemoryToPeripheral_SniffCRC();
 }
