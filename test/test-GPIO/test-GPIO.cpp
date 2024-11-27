ef1e9bbf43ef9988f85ee10c9b972eacc78760fe
ypsitau <ypsitau@nifty.com>
2024-11-27 14:19:46 +0900

    added test-GPIO

--- test/test-GPIO/test-GPIO.cpp
+++ test/test-GPIO/test-GPIO.cpp
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "jxglib/Common.h"
 
+using namespace jxglib;
 
 int main()
 {
-    stdio_init_all();
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+	stdio_init_all();
+	GPIO12.init().set_dir(GPIO_OUT).set_drive_strength(GPIO_DRIVE_STRENGTH_2MA).put(true);
+	GPIO13.init().set_dir(GPIO_OUT).set_drive_strength(GPIO_DRIVE_STRENGTH_4MA).put(true);
+	GPIO14.init().set_dir(GPIO_OUT).set_drive_strength(GPIO_DRIVE_STRENGTH_8MA).put(true);
+	GPIO15.init().set_dir(GPIO_OUT).set_drive_strength(GPIO_DRIVE_STRENGTH_12MA).put(true);
+	while (true) {
+		printf("Hello, world!\n");
+		sleep_ms(1000);
+	}
 }
