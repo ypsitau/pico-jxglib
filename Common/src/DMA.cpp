05cde335ff27f6e40ff4d792c246a59b0aae13ba
ypsitau <ypsitau@nifty.com>
2024-11-27 21:18:53 +0900

    implementing UART

---
+++ Common/src/DMA.cpp
@@ -0,0 +1,32 @@
+//==============================================================================
+// DMA.cpp
+//==============================================================================
+#include "jxglib/DMA.h"
+
+//------------------------------------------------------------------------------
+// DMA
+//------------------------------------------------------------------------------
+namespace jxglib {
+
+const DMA::Channel DMA::Channel0(0);
+const DMA::Channel DMA::Channel1(1);
+const DMA::Channel DMA::Channel2(2);
+const DMA::Channel DMA::Channel3(3);
+const DMA::Channel DMA::Channel4(4);
+const DMA::Channel DMA::Channel5(5);
+const DMA::Channel DMA::Channel6(6);
+const DMA::Channel DMA::Channel7(7);
+const DMA::Channel DMA::Channel8(8);
+const DMA::Channel DMA::Channel9(9);
+const DMA::Channel DMA::Channel10(10);
+const DMA::Channel DMA::Channel11(11);
+
+const DMA::Timer DMA::Timer0(0);
+const DMA::Timer DMA::Timer1(1);
+const DMA::Timer DMA::Timer2(2);
+const DMA::Timer DMA::Timer3(3);
+
+const DMA::IRQ_n DMA::IRQ_0(0);
+const DMA::IRQ_n DMA::IRQ_1(1);
+
+}
