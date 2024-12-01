100e2290f88e917f03d3a851487de21367f12e12
ypsitau <ypsitau@nifty.com>
2024-12-01 09:42:06 +0900

    implementing PIO

--- UART/test-UART/test-UART.cpp
+++ UART/test-UART/test-UART.cpp
@@ -7,10 +7,10 @@ using namespace jxglib;
 int main()
 {
 	::stdio_init_all();
+	Printable::SetStandardOutput(UART::Default);
 	//uint8_t buff[256];
 	//for (int i = 0; i < count_of(buff); i++) buff[i] = static_cast<uint8_t>(i + 0x10);
 	const uint8_t* buff = reinterpret_cast<const uint8_t*>(0x10000000);
-	Printable::SetStandardOutput(UART::Default);
 	Println("--------");
 	Dump.Data8Bit()(buff, 32);
 	Println();
