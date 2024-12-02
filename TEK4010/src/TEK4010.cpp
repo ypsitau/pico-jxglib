e0e7d14784e5e8032170b3ba365dec1e21c1a70f
ypsitau <ypsitau@nifty.com>
2024-12-02 15:45:53 +0900

    implementing TEK4010

--- TEK4010/src/TEK4010.cpp
+++ TEK4010/src/TEK4010.cpp
@@ -17,19 +17,38 @@ void Tek_Graph(Printable& printable, int vector_no)
 	printable.PutCharRaw(vector_no|0x60); //線種を選ぶ
 }
 
+void PutCoordX(Printable& printable, uint16_t coord)
+{
+	printable.PutCharRaw((1 << 5) | static_cast<char>((coord >> 5) & 0x1f));
+	printable.PutCharRaw((0 << 5) | (1 << 6) | static_cast<char>(coord & 0x1f));
+}
+
+void PutCoordY(Printable& printable, uint16_t coord)
+{
+	printable.PutCharRaw((1 << 5) | static_cast<char>((coord >> 5) & 0x1f));
+	printable.PutCharRaw((1 << 5) | (1 << 6) | static_cast<char>(coord & 0x1f));
+}
+
 #if 1
-void Tek_Line(Printable& printable, int xs,int ys,int xg,int yg,int line_no)
+void Tek_Line(Printable& printable, uint16_t xs,uint16_t ys,uint16_t xg,uint16_t yg,uint16_t line_no)
 {
 	unsigned char x0,y0,x1,y1;
 	Tek_Graph(printable, line_no); //グラフィックモードに移行(線種を指定)
 
+#if 1
+	PutCoordY(printable, ys);
+	PutCoordX(printable, xs);
+	PutCoordY(printable, yg);
+	PutCoordX(printable, xg);
+#endif
+#if 0
 	x0=(unsigned char)(xs&0x1F); //下位5byteを取り出す
 	x0=x0|0x40; //Tag-Bitをセット
-	y0=(unsigned char)(ys&0x1F); //下位5byteを取り出す
-	y0=y0|0x60; //Tag-Bitをセット
-
 	x1=(unsigned char)((xs>>5)&0x1F); //上位byteを5個右シフトしたあと下位5byteを取り出す
 	x1=x1|0x20; //Tag-Bitをセット
+
+	y0=(unsigned char)(ys & 0x1F); //下位5byteを取り出す
+	y0=y0|0x60; //Tag-Bitをセット
 	y1=(unsigned char)((ys>>5)&0x1F); //上位byteを5個右シフトしたあと下位5byteを取り出す
 	y1=y1|0x20; //Tag-Bitをセット
 
@@ -37,7 +56,6 @@ void Tek_Line(Printable& printable, int xs,int ys,int xg,int yg,int line_no)
 	printable.PutCharRaw(y0); //座標送信
 	printable.PutCharRaw(x1); //座標送信
 	printable.PutCharRaw(x0); //座標送信
-
 
 	x0=(unsigned char)(xg&0x1F); //下位5byteを取り出す
 	x0=x0|0x40; //Tag-Bitをセット
@@ -53,6 +71,7 @@ void Tek_Line(Printable& printable, int xs,int ys,int xg,int yg,int line_no)
 	printable.PutCharRaw(y0); //座標送信
 	printable.PutCharRaw(x1); //座標送信
 	printable.PutCharRaw(x0); //座標送信
+#endif
 
 	//Tek_Alpha(); //キャラクタモードに戻す
 }
@@ -95,7 +114,7 @@ void TEK4010::Test()
 		printable_.Printf("hello\n");
 	}
 #endif
-	for (int i = 0; i < 480; i += 5) {
+	for (uint16_t i = 0; i < 480; i += 5) {
 		Tek_Line(printable_, 0, 240, 600, i, i);
 	}
 	Tek_Char_PosSet(printable_, 420, 20);
