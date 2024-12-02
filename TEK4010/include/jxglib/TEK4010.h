b56502f50c9e3dde700f3c0107bb48dd6e101294
ypsitau <ypsitau@nifty.com>
2024-12-02 14:19:55 +0900

    added TEK4010

---
+++ TEK4010/include/jxglib/TEK4010.h
@@ -0,0 +1,24 @@
+//==============================================================================
+// jxglib/Display.h
+//==============================================================================
+#ifndef PICO_JXGLIB_DISPLAY_H
+#define PICO_JXGLIB_DISPLAY_H
+#include "pico/stdlib.h"
+#include "jxglib/Drawable.h"
+
+namespace jxglib {
+
+//------------------------------------------------------------------------------
+// Display
+//------------------------------------------------------------------------------
+struct Display : public Drawable {
+public:
+	using DisplayDir = Image::SequencerDir;
+public:
+	Display(uint32_t capabilities, const Format& format, int width, int height) :
+			Drawable(capabilities, format, width, height) {}
+};
+
+}
+
+#endif
