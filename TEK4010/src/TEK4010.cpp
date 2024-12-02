a8b35e842f36d18a3ebee9ac5dac35e75c08554b
ypsitau <ypsitau@nifty.com>
2024-12-02 15:30:16 +0900

    implementing TEK4010

--- TEK4010/src/TEK4010.cpp
+++ TEK4010/src/TEK4010.cpp
@@ -5,11 +5,16 @@
 
 namespace jxglib {
 
+void Tek_Alpha(Printable& printable)
+{
+	printable.PutCharRaw(0x1F); //US　アルファモードへ戻す
+}
+
 void Tek_Graph(Printable& printable, int vector_no)
 {
-	printable.PutChar(0x1D); //GS
-	printable.PutChar(0x1B); //ESC
-	printable.PutChar(vector_no|0x60); //線種を選ぶ
+	printable.PutCharRaw(0x1D); //GS
+	printable.PutCharRaw(0x1B); //ESC
+	printable.PutCharRaw(vector_no|0x60); //線種を選ぶ
 }
 
 #if 1
@@ -28,10 +33,10 @@ void Tek_Line(Printable& printable, int xs,int ys,int xg,int yg,int line_no)
 	y1=(unsigned char)((ys>>5)&0x1F); //上位byteを5個右シフトしたあと下位5byteを取り出す
 	y1=y1|0x20; //Tag-Bitをセット
 
-	printable.PutChar(y1); //座標送信
-	printable.PutChar(y0); //座標送信
-	printable.PutChar(x1); //座標送信
-	printable.PutChar(x0); //座標送信
+	printable.PutCharRaw(y1); //座標送信
+	printable.PutCharRaw(y0); //座標送信
+	printable.PutCharRaw(x1); //座標送信
+	printable.PutCharRaw(x0); //座標送信
 
 
 	x0=(unsigned char)(xg&0x1F); //下位5byteを取り出す
@@ -44,14 +49,36 @@ void Tek_Line(Printable& printable, int xs,int ys,int xg,int yg,int line_no)
 	y1=(unsigned char)((yg>>5)&0x1F); //上位byteを5個右シフトしたあと下位5byteを取り出す
 	y1=y1|0x20; //Tag-Bitをセット
 
-	printable.PutChar(y1); //座標送信
-	printable.PutChar(y0); //座標送信
-	printable.PutChar(x1); //座標送信
-	printable.PutChar(x0); //座標送信
+	printable.PutCharRaw(y1); //座標送信
+	printable.PutCharRaw(y0); //座標送信
+	printable.PutCharRaw(x1); //座標送信
+	printable.PutCharRaw(x0); //座標送信
 
 	//Tek_Alpha(); //キャラクタモードに戻す
 }
 #endif
+
+void Tek_Char_PosSet(Printable& printable, int xs,int ys)
+{
+	unsigned char x0,y0,x1,y1;
+
+	printable.PutCharRaw(0x1D); //GS
+	x0=(unsigned char)(xs&0x1F); //下位5byteを取り出す
+	x0=x0|0x40; //Tag-Bitをセット
+	y0=(unsigned char)(ys&0x1F); //下位5byteを取り出す
+	y0=y0|0x60; //Tag-Bitをセット
+
+	x1=(unsigned char)((xs>>5)&0x1F); //上位byteを5個右シフトしたあと下位5byteを取り出す
+	x1=x1|0x20; //Tag-Bitをセット
+	y1=(unsigned char)((ys>>5)&0x1F); //上位byteを5個右シフトしたあと下位5byteを取り出す
+	y1=y1|0x20; //Tag-Bitをセット
+
+	printable.PutCharRaw(y1); //座標送信
+	printable.PutCharRaw(y0); //座標送信
+	printable.PutCharRaw(x1); //座標送信
+	printable.PutCharRaw(x0); //座標送信
+	Tek_Alpha(printable); //キャラクタモードに戻す
+}
 
 //------------------------------------------------------------------------------
 // TEK4010
@@ -71,7 +98,8 @@ void TEK4010::Test()
 	for (int i = 0; i < 480; i += 5) {
 		Tek_Line(printable_, 0, 240, 600, i, i);
 	}
-
+	Tek_Char_PosSet(printable_, 420, 20);
+	printable_.PrintfRaw("Hello World");
 }
 
 }
