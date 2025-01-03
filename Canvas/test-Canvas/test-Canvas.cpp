4d1a536b8e7555dbe971a8e6d0a205c4894176bc
ypsitau <ypsitau@nifty.com>
2025-01-03 18:07:55 +0900

    implementing Canvas

--- Canvas/test-Canvas/test-Canvas.cpp
+++ Canvas/test-Canvas/test-Canvas.cpp
@@ -15,14 +15,14 @@ int main()
 	::spi_init(spi1, 125 * 1000 * 1000);
 	GPIO2.set_function_SPI0_SCK();
 	GPIO3.set_function_SPI0_TX();
-	GPIO14.set_function_SPI1_SCK().set_drive_strength(GPIO_DRIVE_STRENGTH_12MA);
-	GPIO15.set_function_SPI1_TX().set_drive_strength(GPIO_DRIVE_STRENGTH_12MA);
-	ILI9341 display(spi1, 240, 320, {RST: GPIO11, DC: GPIO10, CS: GPIO12, BL: GPIO13});
+	GPIO14.set_function_SPI1_SCK();
+	GPIO15.set_function_SPI1_TX();
+	//ILI9341 display(spi1, 240, 320, {RST: GPIO11, DC: GPIO10, CS: GPIO12, BL: GPIO13});
+	ST7789 display(spi1, 240, 320, {RST: GPIO10, DC: GPIO11, CS: GPIO12, BL: GPIO13});
 	Display::Dir displayDir = Display::Dir::Rotate0;
 	display.Initialize(displayDir);
 	Canvas canvas;
-	canvas.AttachOutput(display, Canvas::DrawDir::Rotate0);
-	//canvas.AttachOutput(display, {50, 50, 100, 150}, Canvas::AttachDir::Rotate0);
+	canvas.AttachOutput(display, Canvas::DrawDir::Rotate0, {50, 50, 100, 150});
 	Drawable_TestCase::DrawString(canvas);
 	//Drawable_TestCase::RotateImage(canvas);
 }
