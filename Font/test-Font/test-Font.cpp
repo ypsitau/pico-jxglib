2accd8e8852b8d36766ef440478b4e41a2b975cd
ypsitau <ypsitau@nifty.com>
2025-01-08 14:11:57 +0900

    deleted font files

--- Font/test-Font/test-Font.cpp
+++ Font/test-Font/test-Font.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "jxglib/Font/misaki_gothic-level2.h"
 #include "jxglib/Font/shinonome12-level2.h"
 #include "jxglib/Font/shinonome14-level2.h"
 #include "jxglib/Font/shinonome16-level2.h"
 #include "jxglib/Font/shinonome18.h"
 #include "jxglib/Font/sisd24x32.h"
 #include "jxglib/Font/sisd8x16.h"
+#include "jxglib/Font/unifont-level2.h"
 
 using namespace jxglib;
 
@@ -43,10 +45,12 @@ void CheckFontSet(const FontSet& fontSet)
 int main()
 {
 	::stdio_init_all();
+	CheckFontSet(Font::misaki_gothic);
 	CheckFontSet(Font::shinonome12);
 	CheckFontSet(Font::shinonome14);
 	CheckFontSet(Font::shinonome16);
 	CheckFontSet(Font::shinonome18);
 	CheckFontSet(Font::sisd24x32);
 	CheckFontSet(Font::sisd8x16);
+	CheckFontSet(Font::unifont);
 }
