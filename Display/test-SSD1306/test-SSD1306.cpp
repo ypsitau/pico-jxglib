f9c91738ff2085e2f4f5f23c9484db48c2f6da60
ypsitau <ypsitau@nifty.com>
2025-01-10 15:50:38 +0900

    modified GPIO assign

--- Display/test-SSD1306/test-SSD1306.cpp
+++ Display/test-SSD1306/test-SSD1306.cpp
@@ -9,8 +9,8 @@ int main()
 {
 	::stdio_init_all();
 	::i2c_init(i2c1, 400000);
-	GPIO26.set_function_I2C1_SDA().pull_up();
-	GPIO27.set_function_I2C1_SCL().pull_up();
+	GPIO6.set_function_I2C1_SDA().pull_up();
+	GPIO7.set_function_I2C1_SCL().pull_up();
 	SSD1306 display(i2c1);
 	display.Initialize();
 	Drawable_TestCase::DrawString(display);
