5dd77f95aee4e7d0f4d7060cf5eb6bf10e345143
ypsitau <ypsitau@nifty.com>
2025-01-16 16:28:15 +0900

    implementing USB

--- USB/hid_boot_interface/hid_boot_interface.cpp
+++ USB/hid_boot_interface/hid_boot_interface.cpp
@@ -2,13 +2,30 @@
 #include <stdio.h>
 #include <string.h>
 #include "pico/stdlib.h"
-#include "tusb.h"
 #include "usb_descriptors.h"
 #include "jxglib/GPIO.h"
+#include "jxglib/USB.h"
 
 void hid_task(void);
 
 using namespace jxglib;
+
+const USB::Device::Descriptor deviceDesc = {
+	.bLength            = sizeof(USB::Device::Descriptor),
+	.bDescriptorType    = TUSB_DESC_DEVICE,
+	.bcdUSB             = 0x0200,
+	.bDeviceClass       = 0x00,
+	.bDeviceSubClass    = 0x00,
+	.bDeviceProtocol    = 0x00,
+	.bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
+	.idVendor           = 0xCafe,
+	.idProduct          = 0x4000 | (CFG_TUD_CDC << 0) | (CFG_TUD_MSC << 1) | (CFG_TUD_HID << 2) | (CFG_TUD_MIDI << 3) | (CFG_TUD_VENDOR << 4),
+	.bcdDevice          = 0x0100,
+	.iManufacturer      = 0x01,
+	.iProduct           = 0x02,
+	.iSerialNumber      = 0x03,
+	.bNumConfigurations = 0x01
+};
 
 int main(void)
 {
@@ -19,32 +36,19 @@ int main(void)
 	GPIO19.init().set_dir_IN().pull_up();
 	GPIO20.init().set_dir_IN().pull_up();
 	GPIO21.init().set_dir_IN().pull_up();
-	::tud_init(BOARD_TUD_RHPORT);
+	USB::Device::Initialize(BOARD_TUD_RHPORT, deviceDesc);
+	//::tud_init(BOARD_TUD_RHPORT);
 	for (;;) {
+		//USB::Device::Task();
 		::tud_task();
 		::hid_task();
 	}
 	return 0;
 }
 
-void tud_mount_cb(void)
-{
-}
-
-void tud_umount_cb(void)
-{
-}
 
 // remote_wakeup_en : if host allow us  to perform remote wakeup
 // Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en)
-{
-}
-
-void tud_resume_cb(void)
-{
-	//tud_mounted();
-}
 
 uint32_t board_millis()
 {
@@ -133,49 +137,4 @@ void hid_task(void)
 		}
 	}
 #endif
-}
-
-// Invoked when received SET_PROTOCOL request
-// protocol is either HID_PROTOCOL_BOOT (0) or HID_PROTOCOL_REPORT (1)
-void tud_hid_set_protocol_cb(uint8_t instance, uint8_t protocol)
-{
-	// TODO set a indicator for user
-}
-
-// Invoked when sent REPORT successfully to host
-// Application can use this to send the next report
-// Note: For composite reports, report[0] is report ID
-void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint16_t len)
-{
-}
-
-// Invoked when received GET_REPORT control request
-// Application must fill buffer report's content and return its length.
-// Return zero will cause the stack to STALL request
-uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
-{
-	return 0;
-}
-
-// Invoked when received SET_REPORT control request or
-// received data on OUT endpoint (Report ID = 0, Type = 0 )
-void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
-{
-	// keyboard interface
-	if (instance == ITF_NUM_KEYBOARD) {
-		// Set keyboard LED e.g Capslock, Numlock etc...
-		if (report_type == HID_REPORT_TYPE_OUTPUT) {
-			// bufsize should be (at least) 1
-			if (bufsize < 1) return;
-			uint8_t const kbd_leds = buffer[0];
-			if (kbd_leds & KEYBOARD_LED_CAPSLOCK) {
-				// Capslock On: disable blink, turn led on
-				//blink_interval_ms = 0;
-				//board_led_write(true);
-			} else {
-				// Caplocks Off: back to normal blink
-				//board_led_write(false);
-			}
-		}
-	}
 }
