aee4149bf86a1fa88e5d796a5887b6165c137c57
ypsitau <ypsitau@nifty.com>
2025-02-05 15:40:37 +0900

    implementing Flash

--- Flash/include/jxglib/Flash.h
+++ Flash/include/jxglib/Flash.h
@@ -12,9 +12,45 @@ namespace jxglib {
 //------------------------------------------------------------------------------
 // Flash
 //------------------------------------------------------------------------------
-struct Flash {
+class Flash {
+public:
+	static const uint32_t Timeout = UINT32_MAX;
+	static const uint32_t Address = XIP_BASE;
+	static const uint32_t BlockSize = FLASH_BLOCK_SIZE;
+	static const uint32_t SectorSize = FLASH_SECTOR_SIZE;
+	static const uint32_t PageSize = FLASH_PAGE_SIZE;
+public:
+	struct Param_Erase { uint32_t offset; size_t bytes; };
+	struct Param_Program { uint32_t offset; const void* data; size_t bytes; };
 public:
 	Flash() {}
+public:
+	static uint32_t GetAddress(uint32_t offset) { return XIP_BASE + offset; }
+	template<typename T = void>
+	static const T* GetPointer(uint32_t offset) { return reinterpret_cast<const T*>(XIP_BASE + offset); }
+	static void Erase(uint32_t offset, size_t bytes) {
+		::flash_range_erase(offset, bytes);
+	}
+	static void EraseSafe(uint32_t offset, size_t bytes) {
+		Param_Erase param { offset, bytes };
+		::flash_safe_execute(EraseSafe_, &param, Timeout);
+	}
+	static void Program(uint32_t offset, const void* data, size_t bytes) {
+		::flash_range_program(offset, reinterpret_cast<const uint8_t*>(data), bytes);
+	}
+	static void ProgramSafe(uint32_t offset, const void* data, size_t bytes) {
+		Param_Program param { offset, data, bytes };
+		::flash_safe_execute(ProgramSafe_, &param, Timeout);
+	}
+private:
+	static void EraseSafe_(void* param) {
+		auto& paramEx = *reinterpret_cast<Param_Erase*>(param);
+		Erase(paramEx.offset, paramEx.bytes);
+	}
+	static void ProgramSafe_(void* param) {
+		auto& paramEx = *reinterpret_cast<Param_Program*>(param);
+		Program(paramEx.offset, paramEx.data, paramEx.bytes);
+	}
 };
 
 }
--- Flash/test-Flash/test-Flash.cpp
+++ Flash/test-Flash/test-Flash.cpp
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pico/stdlib.h"
+#include "jxglib/Printable.h"
 #include "jxglib/Flash.h"
 
 using namespace jxglib;
 
+int main()
+{
+	uint32_t offset = 1024 * 1024;
+	::stdio_init_all();
+	uint8_t buff[Flash::PageSize];
+	for (uint i = 0; i < sizeof(buff); ++i) buff[i] = static_cast<uint8_t>(i);
+	Flash::EraseSafe(offset, Flash::SectorSize * 1);
+	Dump.AddrStart(Flash::GetAddress(offset))(Flash::GetPointer<void>(offset), Flash::PageSize * 1);
+	Flash::ProgramSafe(offset, buff, Flash::PageSize * 1);
+	Dump.AddrStart(Flash::GetAddress(offset))(Flash::GetPointer<void>(offset), Flash::PageSize * 1);
+}
+
+#if 0
 // We're going to erase and reprogram a region 256k from the start of flash.
 // Once done, we can access this at XIP_BASE + 256k.
 #define FLASH_TARGET_OFFSET (256 * 1024)
@@ -12,65 +26,66 @@ using namespace jxglib;
 const uint8_t *flash_target_contents = (const uint8_t *) (XIP_BASE + FLASH_TARGET_OFFSET);
 
 void print_buf(const uint8_t *buf, size_t len) {
-    for (size_t i = 0; i < len; ++i) {
-        printf("%02x", buf[i]);
-        if (i % 16 == 15)
-            printf("\n");
-        else
-            printf(" ");
-    }
+	for (size_t i = 0; i < len; ++i) {
+		printf("%02x", buf[i]);
+		if (i % 16 == 15)
+			printf("\n");
+		else
+			printf(" ");
+	}
 }
 
 // This function will be called when it's safe to call flash_range_erase
 static void call_flash_range_erase(void *param) {
-    uint32_t offset = (uint32_t)param;
-    flash_range_erase(offset, FLASH_SECTOR_SIZE);
+	uint32_t offset = (uint32_t)param;
+	flash_range_erase(offset, FLASH_SECTOR_SIZE);
 }
 
 // This function will be called when it's safe to call flash_range_program
 static void call_flash_range_program(void *param) {
-    uint32_t offset = ((uintptr_t*)param)[0];
-    const uint8_t *data = (const uint8_t *)((uintptr_t*)param)[1];
-    flash_range_program(offset, data, FLASH_PAGE_SIZE);
+	uint32_t offset = ((uintptr_t*)param)[0];
+	const uint8_t *data = (const uint8_t *)((uintptr_t*)param)[1];
+	flash_range_program(offset, data, FLASH_PAGE_SIZE);
 }
 
 int main() {
-    stdio_init_all();
-    uint8_t random_data[FLASH_PAGE_SIZE];
-    for (uint i = 0; i < FLASH_PAGE_SIZE; ++i)
-        random_data[i] = rand() >> 16;
+	stdio_init_all();
+	uint8_t random_data[FLASH_PAGE_SIZE];
+	for (uint i = 0; i < FLASH_PAGE_SIZE; ++i)
+		random_data[i] = rand() >> 16;
 
-    printf("Generated random data:\n");
-    print_buf(random_data, FLASH_PAGE_SIZE);
+	printf("Generated random data:\n");
+	print_buf(random_data, FLASH_PAGE_SIZE);
 
-    // Note that a whole number of sectors must be erased at a time.
-    printf("\nErasing target region...\n");
+	// Note that a whole number of sectors must be erased at a time.
+	printf("\nErasing target region...\n");
 
-    // Flash is "execute in place" and so will be in use when any code that is stored in flash runs, e.g. an interrupt handler
-    // or code running on a different core.
-    // Calling flash_range_erase or flash_range_program at the same time as flash is running code would cause a crash.
-    // flash_safe_execute disables interrupts and tries to cooperate with the other core to ensure flash is not in use
-    // See the documentation for flash_safe_execute and its assumptions and limitations
-    int rc = flash_safe_execute(call_flash_range_erase, (void*)FLASH_TARGET_OFFSET, UINT32_MAX);
-    hard_assert(rc == PICO_OK);
+	// Flash is "execute in place" and so will be in use when any code that is stored in flash runs, e.g. an interrupt handler
+	// or code running on a different core.
+	// Calling flash_range_erase or flash_range_program at the same time as flash is running code would cause a crash.
+	// flash_safe_execute disables interrupts and tries to cooperate with the other core to ensure flash is not in use
+	// See the documentation for flash_safe_execute and its assumptions and limitations
+	int rc = flash_safe_execute(call_flash_range_erase, (void*)FLASH_TARGET_OFFSET, UINT32_MAX);
+	hard_assert(rc == PICO_OK);
 
-    printf("Done. Read back target region:\n");
-    print_buf(flash_target_contents, FLASH_PAGE_SIZE);
+	printf("Done. Read back target region:\n");
+	print_buf(flash_target_contents, FLASH_PAGE_SIZE);
 
-    printf("\nProgramming target region...\n");
-    uintptr_t params[] = { FLASH_TARGET_OFFSET, (uintptr_t)random_data};
-    rc = flash_safe_execute(call_flash_range_program, params, UINT32_MAX);
-    hard_assert(rc == PICO_OK);
-    printf("Done. Read back target region:\n");
-    print_buf(flash_target_contents, FLASH_PAGE_SIZE);
+	printf("\nProgramming target region...\n");
+	uintptr_t params[] = { FLASH_TARGET_OFFSET, (uintptr_t)random_data};
+	rc = flash_safe_execute(call_flash_range_program, params, UINT32_MAX);
+	hard_assert(rc == PICO_OK);
+	printf("Done. Read back target region:\n");
+	print_buf(flash_target_contents, FLASH_PAGE_SIZE);
 
-    bool mismatch = false;
-    for (uint i = 0; i < FLASH_PAGE_SIZE; ++i) {
-        if (random_data[i] != flash_target_contents[i])
-            mismatch = true;
-    }
-    if (mismatch)
-        printf("Programming failed!\n");
-    else
-        printf("Programming successful!\n");
+	bool mismatch = false;
+	for (uint i = 0; i < FLASH_PAGE_SIZE; ++i) {
+		if (random_data[i] != flash_target_contents[i])
+			mismatch = true;
+	}
+	if (mismatch)
+		printf("Programming failed!\n");
+	else
+		printf("Programming successful!\n");
 }
+#endif
