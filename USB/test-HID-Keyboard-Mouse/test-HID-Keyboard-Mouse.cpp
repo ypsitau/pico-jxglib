042d04f506037df35b2ef36f2279b52cc8c5e6ae
ypsitau <ypsitau@nifty.com>
2025-02-13 14:53:39 +0900

    implementing samples

--- USB/test-HID-Keyboard-Mouse/test-HID-Keyboard-Mouse.cpp
+++ USB/test-HID-Keyboard-Mouse/test-HID-Keyboard-Mouse.cpp
@@ -7,6 +7,13 @@
 
 using namespace jxglib;
 
+auto& GPIO_ARROW_LEFT			= GPIO16;
+auto& GPIO_ARROW_UP				= GPIO17;
+auto& GPIO_ARROW_DOWN			= GPIO18;
+auto& GPIO_ARROW_RIGHT			= GPIO19;
+auto& GPIO_CURSOR_UP_LEFT		= GPIO20;
+auto& GPIO_CURSOR_DOWN_RIGHT	= GPIO21;
+
 //-----------------------------------------------------------------------------
 // Keyboard
 //-----------------------------------------------------------------------------
@@ -23,8 +30,10 @@ public:
 // Mouse
 //-----------------------------------------------------------------------------
 class Mouse : public USBD::Mouse {
+private:
+	bool senseFlagPrev_;
 public:
-	Mouse(USBD::Device& device) : USBD::Mouse(device, "RaspberryPi Pico Mouse", 0x82) {}
+	Mouse(USBD::Device& device) : USBD::Mouse(device, "RaspberryPi Pico Mouse", 0x82), senseFlagPrev_{false} {}
 public:
 	virtual void OnTask() override;
 };
@@ -51,12 +60,12 @@ int main(void)
 	Keyboard keyboard(device);
 	Mouse mouse(device);
 	device.Initialize();
-	GPIO16.init().set_dir_IN().pull_up();
-	GPIO17.init().set_dir_IN().pull_up();
-	GPIO18.init().set_dir_IN().pull_up();
-	GPIO19.init().set_dir_IN().pull_up();
-	GPIO20.init().set_dir_IN().pull_up();
-	GPIO21.init().set_dir_IN().pull_up();
+	GPIO_ARROW_LEFT			.init().set_dir_IN().pull_up();
+	GPIO_ARROW_UP			.init().set_dir_IN().pull_up();
+	GPIO_ARROW_DOWN			.init().set_dir_IN().pull_up();
+	GPIO_ARROW_RIGHT		.init().set_dir_IN().pull_up();
+	GPIO_CURSOR_UP_LEFT		.init().set_dir_IN().pull_up();
+	GPIO_CURSOR_DOWN_RIGHT	.init().set_dir_IN().pull_up();
 	for (;;) {
 		device.Task();
 	}
@@ -73,10 +82,10 @@ void Keyboard::OnTask()
 	uint8_t modifier  = 0;
 	uint8_t keycode[6] = { 0 };
 	int nKeycode = 0;
-	if (!GPIO16.get()) keycode[nKeycode++] = HID_KEY_ARROW_LEFT;
-	if (!GPIO17.get()) keycode[nKeycode++] = HID_KEY_ARROW_UP;
-	if (!GPIO18.get()) keycode[nKeycode++] = HID_KEY_ARROW_DOWN;
-	if (!GPIO19.get()) keycode[nKeycode++] = HID_KEY_ARROW_RIGHT;
+	if (!GPIO_ARROW_LEFT.get())		keycode[nKeycode++] = HID_KEY_ARROW_LEFT;
+	if (!GPIO_ARROW_UP.get())		keycode[nKeycode++] = HID_KEY_ARROW_UP;
+	if (!GPIO_ARROW_DOWN.get())		keycode[nKeycode++] = HID_KEY_ARROW_DOWN;
+	if (!GPIO_ARROW_RIGHT.get())	keycode[nKeycode++] = HID_KEY_ARROW_RIGHT;
 	if (::tud_suspended()) {
 		// Wake up host if we are in suspend mode and REMOTE_WAKEUP feature is enabled by host
 		if (nKeycode > 0) ::tud_remote_wakeup();
@@ -96,16 +105,13 @@ void Keyboard::OnTask()
 void Mouse::OnTask()
 {
 	if (!hid_ready()) return;
-	if (!GPIO20.get()) {
-		uint8_t report_id = 0;
-		uint8_t button_mask = 0;
-		int8_t x = -5, y = -5, vertical = 0, horizontal = 0;
-		hid_mouse_report(report_id, button_mask, x, y, vertical, horizontal);
-	}
-	if (!GPIO21.get()) {
-		uint8_t report_id = 0;
-		uint8_t button_mask = 0;
-		int8_t x = 5, y = 5, vertical = 0, horizontal = 0;
-		hid_mouse_report(report_id, button_mask, x, y, vertical, horizontal);
-	}
+	bool senseFlag = false;
+	uint8_t report_id = 0;
+	uint8_t buttons = 0;
+	int8_t x = 0, y = 0;
+	int8_t vertical = 0, horizontal = 0;
+	if (!GPIO_CURSOR_UP_LEFT.get())		{ senseFlag = true; x = y = -5; }
+	if (!GPIO_CURSOR_DOWN_RIGHT.get())	{ senseFlag = true; x = y = 5; }
+	if (senseFlag || senseFlagPrev_) hid_mouse_report(report_id, buttons, x, y, vertical, horizontal);
+	senseFlagPrev_ = senseFlag;
 }
