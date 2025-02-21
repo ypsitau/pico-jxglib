5d366cf96c5d0238ab946246dd43b78e26f792d3
ypsitau <ypsitau@nifty.com>
2025-02-21 16:09:06 +0900

    implementing mmc

--- device/MMC/test-MMC/test-MMC.cpp
+++ device/MMC/test-MMC/test-MMC.cpp
@@ -10,16 +10,34 @@ using namespace jxglib;
 
 #if 1
 
+void SendByte(spi_inst_t* spi, uint8_t data)
+{
+	::spi_write_blocking(spi, &data, 1);
+}
+
+uint8_t RecvByte(spi_inst_t* spi)
+{
+	uint8_t rtn;
+	::spi_read_blocking(spi, 0xff, &rtn, 1);
+	return rtn;
+}
+
 void SendCmd(spi_inst_t* spi, uint8_t cmd, uint32_t arg, uint8_t crc = 0x01)
 {
-	uint8_t buff[6];
-	buff[0] = cmd;
-	buff[1] = static_cast<uint8_t>(arg >> 24);
-	buff[2] = static_cast<uint8_t>(arg >> 16);
-	buff[3] = static_cast<uint8_t>(arg >> 8);
-	buff[4] = static_cast<uint8_t>(arg >> 0);
-	buff[5] = crc;
-	::spi_write_blocking(spi, buff, sizeof(buff));
+	//uint8_t buff[6];
+	//buff[0] = cmd;
+	//buff[1] = static_cast<uint8_t>(arg >> 24);
+	//buff[2] = static_cast<uint8_t>(arg >> 16);
+	//buff[3] = static_cast<uint8_t>(arg >> 8);
+	//buff[4] = static_cast<uint8_t>(arg >> 0);
+	//buff[5] = crc;
+	//::spi_write_blocking(spi, buff, sizeof(buff));
+	SendByte(spi, cmd);
+	SendByte(spi, static_cast<uint8_t>(arg >> 24));
+	SendByte(spi, static_cast<uint8_t>(arg >> 16));
+	SendByte(spi, static_cast<uint8_t>(arg >> 8));
+	SendByte(spi, static_cast<uint8_t>(arg >> 0));
+	SendByte(spi, crc);
 }
 
 void SendCMD0(spi_inst_t* spi)
@@ -34,35 +52,35 @@ void SendCMD8(spi_inst_t* spi)
 	SendCmd(spi, 0x48, 0x1aa, 0x86 | 0x01);
 }
 
-uint8_t RecvByte(spi_inst_t* spi)
-{
-	uint8_t rtn;
-	::spi_read_blocking(spi, 0xff, &rtn, 1);
-	return rtn;
-}
 
 int main()
 {
+	const auto& GPIO_SCK = GPIO2;
+	const auto& GPIO_TX = GPIO3;
+	const auto& GPIO_RX = GPIO4;
+	const auto& GPIO_CS = GPIO5;
 	::stdio_init_all();
 	spi_inst_t* spi = spi0;
-	GPIO2.set_function_SPI0_SCK();
-	GPIO3.set_function_SPI0_TX().pull_up();
-	GPIO4.set_function_SPI0_RX();
-	GPIO5.init().set_dir_OUT();
-	::spi_set_format(spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST); 
-	GPIO4.init().set_dir_OUT();
-	GPIO4.put(1);	// DI
-	GPIO5.put(1);	// CS
-	::sleep_ms(10);	// at least 1 msec
+	GPIO_SCK.set_function_SPI0_SCK();
+	GPIO_TX.set_function_SPI0_TX().pull_up();
+	GPIO_RX.set_function_SPI0_RX();
+	GPIO_CS.init().set_dir_OUT();
 	::spi_init(spi, 100 * 1000);
+	::spi_set_format(spi, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST); 
+	GPIO_TX.init().set_dir_OUT();
+	GPIO_TX.put(1);	// DI
+	GPIO_CS.put(1);	// CS
+	::sleep_ms(10);	// at least 1 msec
 	for (int i = 0; i < 80; i++) RecvByte(spi);
-	GPIO4.init().set_function_SPI0_RX();
-	GPIO5.put(0);	// CS
+	GPIO_TX.init().set_function_SPI0_TX();
+	GPIO_CS.put(0);	// CS
+	::printf("----\n");
 	for (;;) {
 		SendCMD0(spi);
 		uint8_t rtn = RecvByte(spi);
 		::printf("%02x\n", rtn);
 		if (rtn == 0x01) break;
+		::sleep_ms(1000);
 	}
 	uint8_t rtn;
 	SendCMD8(spi);
