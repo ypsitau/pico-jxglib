2c05ee2e50ac52b9870a977f6d332f34927bafec
ypsitau <ypsitau@nifty.com>
2025-02-24 15:41:00 +0900

    implementing SDCard

--- device/SDCard/test-SDCard/test-SDCard.cpp
+++ device/SDCard/test-SDCard/test-SDCard.cpp
@@ -10,7 +10,6 @@
 
 using namespace jxglib;
 
-
 class SDCard {
 public:
 	struct Partition {
@@ -26,7 +25,7 @@ public:
 		Packed_uint32(LbaSize);
 	};
 private:
-	static const int _CMD_TIMEOUT					= 100;
+	static const int CMD_TRIAL_COUNT				= 100;
 	static const uint8_t _R1_IDLE_STATE				= 1 << 0;
 	static const uint8_t _R1_ERASE_RESET			= 1 << 1;
 	static const uint8_t _R1_ILLEGAL_COMMAND		= 1 << 2;
@@ -41,66 +40,64 @@ public:
 	static const uint baudrateSlow = 100 * 1000;	// 100kHz
 private:
 	spi_inst_t* spi_;
-	const GPIO& cs_;
+	const GPIO& gpio_CS_;
 	uint baudrate_;
-	uint8_t cmdbuf_[6];
 	uint8_t tokenbuf_[1];
 	int cdv_;
+	int nSectors_;
 public:
-	SDCard(spi_inst_t* spi, const GPIO& cs, uint baudrate);
+	SDCard(spi_inst_t* spi, const GPIO& gpio_CS, uint baudrate);
 	bool Initialize();
 	bool ReadBlock(int lba, uint8_t* buf, int nBlocks);
 	bool WriteBlock(int lba, const uint8_t* buf, int nBlocks);
 public:
+	int GetSectors() const { return nSectors_; }
 	static void PrintMBR(const uint8_t* bufSector);
 private:
-	int cmd(uint8_t cmd, uint32_t arg, uint8_t crc, int final = 0, bool release = true, bool skip1 = false);
+	int SendCmd(uint8_t cmd, uint32_t arg, uint8_t crc, int final = 0, bool release = true, bool skip1 = false);
 	bool readinto(uint8_t* buf, int bytes);
-private:
 	bool write(uint8_t token, const uint8_t* buf, int bytes);
 	bool write_token(uint8_t token);
-	void SPIRead(uint8_t* data, int bytes, uint8_t repeated_tx_data) { spi_read_blocking(spi_, repeated_tx_data, data, bytes); }
+	void SPIRead(uint8_t* data, int bytes, uint8_t token) { spi_read_blocking(spi_, token, data, bytes); }
 	void SPIWrite(const uint8_t* data, int bytes) { spi_write_blocking(spi_, data, bytes); }
 	void SPIWriteByte(uint8_t data) { spi_write_blocking(spi_, &data, 1); }
 };
 
-SDCard::SDCard(spi_inst_t* spi, const GPIO& cs, uint baudrate) : spi_{spi}, cs_{cs}, baudrate_{baudrate}
+SDCard::SDCard(spi_inst_t* spi, const GPIO& gpio_CS, uint baudrate) :
+		spi_{spi}, gpio_CS_{gpio_CS}, baudrate_{baudrate}, cdv_{0}, nSectors_{0}
 {
 }
 
 bool SDCard::Initialize()
 {
-	// init CS pin
-	cs_.put(1);
-
-	// init SPI bus; use low data rate for initialisation
+	gpio_CS_.init().set_dir_OUT();
+	gpio_CS_.put(1);
 	::spi_init(spi_, baudrateSlow);
 	::spi_set_format(spi_, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST); 
 	// clock card at least 100 cycles with cs high
 	for (int i = 0; i < 16; i++) SPIWriteByte(0xff);
-
-	::printf("check %d\n", __LINE__);
 	// CMD0: init card; should return _R1_IDLE_STATE (allow 5 attempts)
 	bool successFlag = false;
 	for (int i = 0; i < 5; i++) {
-		if (cmd(0, 0, 0x95) == _R1_IDLE_STATE) {
+		if (SendCmd(0, 0, 0x95) == _R1_IDLE_STATE) {
 			successFlag = true;
 			break;
 		}
 	}
-	if (!successFlag) return false; // no SD card
-	::printf("check %d\n", __LINE__);
+	if (!successFlag) {
+		return false; // no SD card
+	}
 	// CMD8: determine card version
-	int r = cmd(8, 0x01aa, 0x87, 4);
+	int r = SendCmd(8, 0x01aa, 0x87, 4);
 	if (r == _R1_IDLE_STATE) {
 		// v2 card
 		successFlag = false;
-		for (int i = 0; i < _CMD_TIMEOUT; i++) {
+		for (int iTrial = 0; iTrial < CMD_TRIAL_COUNT; iTrial++) {
 			sleep_ms(50);
-			cmd(58, 0, 0, 4);
-			cmd(55, 0, 0);
-			if (cmd(41, 0x40000000, 0) == 0) {
-				cmd(58, 0, 0, -4); // 4-byte response, negative means keep the first byte
+			SendCmd(58, 0, 0, 4);
+			SendCmd(55, 0, 0);
+			if (SendCmd(41, 0x40000000, 0) == 0) {
+				SendCmd(58, 0, 0, -4); // 4-byte response, negative means keep the first byte
 				uint8_t ocr = tokenbuf_[0];  // get first byte of response, which is OCR
 				if (!(ocr & 0x40)) {
 					// SDSC card, uses byte addressing in read/write/erase commands
@@ -113,14 +110,16 @@ bool SDCard::Initialize()
 				break;
 			}
 		}
-		if (!successFlag) return false;
+		if (!successFlag) {
+			return false;
+		}
 	} else if (r == (_R1_IDLE_STATE | _R1_ILLEGAL_COMMAND)) {
 		// v1 card
 		successFlag = false;
-		for (int i = 0; i < _CMD_TIMEOUT; i++) {
+		for (int iTrial = 0; iTrial < CMD_TRIAL_COUNT; iTrial++) {
 			sleep_ms(50);
-			cmd(55, 0, 0);
-			if (cmd(41, 0, 0) == 0) {
+			SendCmd(55, 0, 0);
+			if (SendCmd(41, 0, 0) == 0) {
 				// SDSC card, uses byte addressing in read/write/erase commands
 				cdv_ = 512;
 				// print("[SDCard] v1 card")
@@ -128,64 +127,58 @@ bool SDCard::Initialize()
 				break;
 			}
 		}
-		if (!successFlag) return false;
+		if (!successFlag) {
+			return false;
+		}
 	} else {
 		return false; // couldn't determine SD card version
 	}
-	::printf("check %d\n", __LINE__);
 	// get the number of sectors
 	// CMD9: response R2 (R1 byte + 16-byte block read)
-	if (cmd(9, 0, 0, 0, false) != 0) {
+	if (SendCmd(9, 0, 0, 0, false) != 0) {
 		return false; // no response from SD card
 	}
-	::printf("check %d\n", __LINE__);
 	uint8_t csd[16];
 	::memset(csd, 0x00, sizeof(csd));
 	readinto(csd, sizeof(csd));
 	Dump.NoAddr()(csd, sizeof(csd));
-	int sectors, c_size, c_size_mult, capacity, read_bl_len;
+	int c_size, c_size_mult, capacity, read_bl_len;
 	if ((csd[0] & 0xc0) == 0x40) {  // CSD version 2.0
-		sectors = ((csd[8] << 8 | csd[9]) + 1) * 1024;
+		nSectors_ = ((csd[8] << 8 | csd[9]) + 1) * 1024;
 	} else if ((csd[0] & 0xc0) == 0x00) { // CSD version 1.0 (old, <=2GB)
 		c_size = (csd[6] & 0b11) << 10 | csd[7] << 2 | csd[8] >> 6;
 		c_size_mult = (csd[9] & 0b11) << 1 | csd[10] >> 7;
 		read_bl_len = csd[5] & 0b1111;
 		capacity = (c_size + 1) * (1 << (c_size_mult + 2)) * (1 << read_bl_len);
-		sectors = capacity / 512;
+		nSectors_ = capacity / 512;
 	} else {
 		return false; // SD card CSD format not supported
 	}
-	printf("sectors: %d\n", sectors);
-	::printf("check %d\n", __LINE__);
-
 	// CMD16: set block length to 512 bytes
-	if (cmd(16, 512, 0) != 0) {
+	if (SendCmd(16, 512, 0) != 0) {
 		return false; // can't set 512 block size
 	}
-
-	::printf("check %d\n", __LINE__);
 	// set to high data rate now that it's initialised
 	::spi_init(spi_, baudrate_);
 	::spi_set_format(spi_, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST); 
 	return true;
 }
 
-int SDCard::cmd(uint8_t cmd, uint32_t arg, uint8_t crc, int final, bool release, bool skip1)
+int SDCard::SendCmd(uint8_t cmd, uint32_t arg, uint8_t crc, int final, bool release, bool skip1)
 {
-	cs_.put(0);
+	gpio_CS_.put(0);
 	// create and send the command
-	uint8_t* buf = cmdbuf_;
+	uint8_t buf[6];
 	buf[0] = 0x40 | cmd;
 	buf[1] = static_cast<uint8_t>(arg >> 24);
 	buf[2] = static_cast<uint8_t>(arg >> 16);
 	buf[3] = static_cast<uint8_t>(arg >> 8);
 	buf[4] = static_cast<uint8_t>(arg);
 	buf[5] = crc;
-	SPIWrite(buf, 6);
+	SPIWrite(buf, sizeof(buf));
 	if (skip1) SPIRead(tokenbuf_, 1, 0xff);
-
 	// wait for the response (response[7] == 0)
-	for (int i = 0; i < _CMD_TIMEOUT; i++) {
+	for (int iTrial = 0; iTrial < CMD_TRIAL_COUNT; iTrial++) {
 		SPIRead(tokenbuf_, 1, 0xff);
 		uint8_t response = tokenbuf_[0];
 		if (!(response & 0x80)) {
@@ -199,24 +192,23 @@ int SDCard::cmd(uint8_t cmd, uint32_t arg, uint8_t crc, int final, bool release,
 				SPIWriteByte(0xff);
 			}
 			if (release) {
-				cs_.put(1);
+				gpio_CS_.put(1);
 				SPIWriteByte(0xff);
 			}
 			return response;
 		}
 	}
-	// timeout
-	cs_.put(1);
+	gpio_CS_.put(1);
 	SPIWriteByte(0xff);
 	return -1;
 }
 
 bool SDCard::readinto(uint8_t* buf, int bytes)
 {
-	cs_.put(0);
+	gpio_CS_.put(0);
 	// read until start byte (0xff)
 	bool successFlag = false;
-	for (int i = 0; i < _CMD_TIMEOUT; i++) {
+	for (int iTrial = 0; iTrial < CMD_TRIAL_COUNT; iTrial++) {
 		SPIRead(tokenbuf_, 1, 0xff);
 		if (tokenbuf_[0] == _TOKEN_DATA) {
 			successFlag = true;
@@ -225,7 +217,7 @@ bool SDCard::readinto(uint8_t* buf, int bytes)
 		::sleep_ms(1);
 	}
 	if (!successFlag) {
-		cs_.put(1);
+		gpio_CS_.put(1);
 		return false; // timeout waiting for response
 	}
 	// read data
@@ -233,14 +225,14 @@ bool SDCard::readinto(uint8_t* buf, int bytes)
 	// read checksum
 	SPIWriteByte(0xff);
 	SPIWriteByte(0xff);
-	cs_.put(1);
+	gpio_CS_.put(1);
 	SPIWriteByte(0xff);
 	return true;
 }
 
 bool SDCard::write(uint8_t token, const uint8_t* buf, int bytes)
 {
-	cs_.put(0);
+	gpio_CS_.put(0);
 	// send: start of block, data, checksum
 	uint8_t rtn;
 	SPIRead(&rtn, 1, token);
@@ -250,7 +242,7 @@ bool SDCard::write(uint8_t token, const uint8_t* buf, int bytes)
 	// check the response
 	SPIRead(&rtn, 1, 0xff);
 	if ((rtn & 0x1f) != 0x05) {
-		cs_.put(1);
+		gpio_CS_.put(1);
 		SPIWriteByte(0xff);
 		return false;
 	}
@@ -259,14 +251,14 @@ bool SDCard::write(uint8_t token, const uint8_t* buf, int bytes)
 		SPIRead(&rtn, 1, 0xff);
 		if (rtn != 0x00) break;
 	}
-	cs_.put(1);
+	gpio_CS_.put(1);
 	SPIWriteByte(0xff);
 	return true;
 }
 
 bool SDCard::write_token(uint8_t token)
 {
-	cs_.put(0);
+	gpio_CS_.put(0);
 	uint8_t rtn;
 	SPIRead(&rtn, 1, token);
 	SPIWriteByte(0xff);
@@ -275,7 +267,7 @@ bool SDCard::write_token(uint8_t token)
 		SPIRead(&rtn, 1, 0xff);
 		if (rtn != 0x00) break;
 	}
-	cs_.put(1);
+	gpio_CS_.put(1);
 	SPIWriteByte(0xff);
 	return true;
 }
@@ -287,18 +279,18 @@ bool SDCard::ReadBlock(int lba, uint8_t* buf, int nBlocks)
 	SPIWriteByte(0xff);
 	if (nBlocks == 1) {
 		// CMD17: set read address for single block
-		if (cmd(17, lba * cdv_, 0, 0, false) != 0) {
+		if (SendCmd(17, lba * cdv_, 0, 0, false) != 0) {
 			// release the card
-			cs_.put(1);
+			gpio_CS_.put(1);
 			return false; // EIO
 		}
 		// receive the data and release card
 		readinto(buf, nBlocks * 512);
 	} else {
 		// CMD18: set read address for multiple blocks
-		if (cmd(18, lba * cdv_, 0, 0, false) != 0) {
+		if (SendCmd(18, lba * cdv_, 0, 0, false) != 0) {
 			// release the card
-			cs_.put(1);
+			gpio_CS_.put(1);
 			return false; // EIO
 		}
 		int offset = 0;
@@ -307,7 +299,7 @@ bool SDCard::ReadBlock(int lba, uint8_t* buf, int nBlocks)
 			readinto(buf + offset, 512);
 			offset += 512;
 		}
-		if (cmd(12, 0, 0xff, 0, true, true) != 0) {
+		if (SendCmd(12, 0, 0xff, 0, true, true) != 0) {
 			return false; // EIO
 		}
 	}
@@ -321,14 +313,14 @@ bool SDCard::WriteBlock(int lba, const uint8_t* buf, int nBlocks)
 	SPIWriteByte(0xff);
 	if (nBlocks == 1) {
 		// CMD24: set write address for single block
-		if (cmd(24, lba * cdv_, 0) != 0) {
+		if (SendCmd(24, lba * cdv_, 0) != 0) {
 			return false; // EIO
 		}
 		// send the data
 		write(_TOKEN_DATA, buf, nBlocks * 512);
 	} else {
 		// CMD25: set write address for first block
-		if (cmd(25, lba * cdv_, 0) != 0) {
+		if (SendCmd(25, lba * cdv_, 0) != 0) {
 			return false; // EIO
 		}
 		// send the data
@@ -341,14 +333,6 @@ bool SDCard::WriteBlock(int lba, const uint8_t* buf, int nBlocks)
 	}
 	return true;
 }
-
-/*
-	def ioctl(self, op, arg):
-		if op == 4:  // get number of blocks
-			return self.sectors
-		if op == 5:  // get block size in bytes
-			return 512
-*/
 
 void SDCard::PrintMBR(const uint8_t* bufSector)
 {
@@ -366,17 +350,11 @@ void SDCard::PrintMBR(const uint8_t* bufSector)
 
 int main()
 {
-	const auto& GPIO_SCK = GPIO2;
-	const auto& GPIO_TX = GPIO3;
-	const auto& GPIO_RX = GPIO4;
-	const auto& GPIO_CS = GPIO5;
 	::stdio_init_all();
-	spi_inst_t* spi = spi0;
-	GPIO_SCK.set_function_SPI0_SCK();
-	GPIO_TX.set_function_SPI0_TX();
-	GPIO_RX.set_function_SPI0_RX();
-	GPIO_CS.init().set_dir_OUT();
-	SDCard sdCard(spi, GPIO_CS, 10 * 1000 * 1000);
+	GPIO2.set_function_SPI0_SCK();
+	GPIO3.set_function_SPI0_TX();
+	GPIO4.set_function_SPI0_RX();
+	SDCard sdCard(spi0, GPIO5, 10 * 1000 * 1000);
 	sdCard.Initialize();
 	uint8_t buf[512];
 	sdCard.ReadBlock(0, buf, 1);
