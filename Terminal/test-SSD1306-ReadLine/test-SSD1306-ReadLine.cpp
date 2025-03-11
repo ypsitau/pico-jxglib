75ec456f0bec67fcee6a1e5120bc36b806c8c610
ypsitau <ypsitau@nifty.com>
2025-03-11 16:16:27 +0900

    implementing Terminal samples

---
+++ Terminal/test-SSD1306-ReadLine/test-SSD1306-ReadLine.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "jxglib/Terminal.h"
+#include "jxglib/SSD1306.h"
+#include "jxglib/Font/shinonome12-japanese-level2.h"
+
+using namespace jxglib;
+
+Terminal terminal;
+
+int main()
+{
+	::stdio_init_all();
+	::i2c_init(i2c0, 400 * 1000);
+	GPIO4.set_function_I2C0_SDA().pull_up();
+	GPIO5.set_function_I2C0_SCL().pull_up();
+	SSD1306 display(i2c0, 0x3c);
+	display.Initialize();
+	terminal.AttachOutput(display);
+	terminal.AttachInputStdio();
+	terminal.SetFont(Font::shinonome12).SetSpacingRatio(1., 1)
+		.SetColor(Color::white).SetColorBg(Color::black)
+		.SetColorInEdit(Color::white).SetColorCursor(Color::white)
+		.ClearScreen();
+	for (;;) {
+		::printf("%s\n", terminal.ReadLine(">"));
+	}
+}
---
+++ Terminal/test-ST7789-ReadLine/test-ST7789-ReadLine.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "jxglib/Terminal.h"
+#include "jxglib/ST7789.h"
+#include "jxglib/Font/shinonome16-japanese-level2.h"
+
+using namespace jxglib;
+
+Terminal terminal;
+
+int main()
+{
+	::stdio_init_all();
+	::spi_init(spi1, 125 * 1000 * 1000);
+	GPIO14.set_function_SPI1_SCK();
+	GPIO15.set_function_SPI1_TX();
+	ST7789 display(spi1, 240, 320, {RST: GPIO10, DC: GPIO11, CS: GPIO12, BL: GPIO13});
+	display.Initialize(Display::Dir::Rotate0);
+	terminal.AttachOutput(display);
+	terminal.AttachInputStdio();
+	terminal.SetFont(Font::shinonome16).SetSpacingRatio(1., 1)
+		.SetColor(Color::white).SetColorBg(Color::black)
+		.SetColorInEdit(Color::white).SetColorCursor(Color::white)
+		.ClearScreen();
+	for (;;) {
+		::printf("%s\n", terminal.ReadLine(">"));
+	}
+}
