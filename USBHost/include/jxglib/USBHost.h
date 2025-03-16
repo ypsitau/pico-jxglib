b58cc6609e17d8b93d7f0caada1c40f8fa0fc51d
ypsitau <ypsitau@nifty.com>
2025-03-16 15:09:09 +0900

    implementing USBHost

--- USBHost/include/jxglib/USBHost.h
+++ USBHost/include/jxglib/USBHost.h
@@ -5,18 +5,62 @@
 #define PICO_JXGLIB_USBHOST_H
 #include "pico/stdlib.h"
 #include "tusb.h"
-#include "jxglib/Common.h"
+#include "jxglib/FIFOBuff.h"
+#include "jxglib/Tickable.h"
+#include "jxglib/Keyboard.h"
 
 namespace jxglib {
 
 //------------------------------------------------------------------------------
 // USBHost
 //------------------------------------------------------------------------------
-class USBHost {
+class USBHost : public Tickable {
+public:
+	class Keyboard : public jxglib::Keyboard, public Tickable {
+	private:
+		uint8_t modifier_;
+		uint8_t keycode_[6];
+		int cntHold_;
+		FIFOBuff<KeyData, 8> fifoKeyData_;
+	public:
+		static const uint8_t convTbl_101Keyboard[128][2];
+	public:
+		Keyboard();
+	public:
+		void OnReport(uint8_t devAddr, uint8_t iInstance, const hid_keyboard_report_t& report);
+	public:
+		// virtual function of jxglib::Keyboard
+		virtual bool GetKeyData(KeyData& keyData) override;
+	public:
+		// virtual function of Tickable
+		virtual void OnTick() override;
+	public:
+		static KeyData CreateKeyData(uint8_t keycode, uint8_t modifier);
+	};
+	class Mouse {
+	public:
+		Mouse();
+	public:
+		void OnReport(uint8_t devAddr, uint8_t iInstance, const hid_mouse_report_t& report);
+	};
+public:
+	static USBHost* Instance;
+private:
+	Keyboard keyboard_;
+	Mouse mouse_;
 public:
 	USBHost();
 public:
-
+	void Initialize(uint8_t rhport = BOARD_TUH_RHPORT);
+public:
+	static Keyboard& GetKeyboard() { return Instance->keyboard_; }
+	static Mouse& GetMouse() { return Instance->mouse_; }
+public:
+	// virtual function of Tickable
+	virtual void OnTick() override;
+public:
+	virtual void OnMount(uint8_t devAddr) {}
+	virtual void OnUmount(uint8_t devAddr) {}
 };
 
 }
--- USBHost/src/USBHost.cpp
+++ USBHost/src/USBHost.cpp
@@ -8,34 +8,46 @@ namespace jxglib {
 //------------------------------------------------------------------------------
 // USBHost
 //------------------------------------------------------------------------------
+USBHost* USBHost::Instance = nullptr;
+
 USBHost::USBHost()
 {
+	Instance = this;
+}
+
+void USBHost::Initialize(uint8_t rhport)
+{
+	::tuh_init(rhport);
+}
+
+void USBHost::OnTick()
+{
+	::tuh_task();
 }
 
 extern "C" {
 
 void tuh_mount_cb(uint8_t devAddr)
 {
-	::printf("tuh_mount_cb(%d)\n", devAddr);
+	USBHost::Instance->OnMount(devAddr);
 }
 
 void tuh_umount_cb(uint8_t devAddr)
 {
-	::printf("tuh_umount_cb(%d)\n", devAddr);
+	USBHost::Instance->OnUmount(devAddr);
 }
 
 #define MAX_REPORT  4
 
-static struct
-{
+static struct {
 	uint8_t report_count;
 	tuh_hid_report_info_t report_info[MAX_REPORT];
-}hid_info[CFG_TUH_HID];
+} hid_info[CFG_TUH_HID];
 
-void tuh_hid_mount_cb(uint8_t devAddr, uint8_t instance, const uint8_t* descReport, uint16_t descLen)
+void tuh_hid_mount_cb(uint8_t devAddr, uint8_t iInstance, const uint8_t* descReport, uint16_t descLen)
 {
-	::printf("tuh_hid_mount_cb(%d, %d)\n", devAddr, instance);
-	uint8_t itfProtocol = ::tuh_hid_interface_protocol(devAddr, instance);
+	::printf("tuh_hid_mount_cb(%d, %d)\n", devAddr, iInstance);
+	uint8_t itfProtocol = ::tuh_hid_interface_protocol(devAddr, iInstance);
 	::printf("HID Interface Protocol = %s\r\n",
 		(itfProtocol == HID_ITF_PROTOCOL_NONE)? "None" :
 		(itfProtocol == HID_ITF_PROTOCOL_KEYBOARD)? "Keyboard" :
@@ -43,46 +55,107 @@ void tuh_hid_mount_cb(uint8_t devAddr, uint8_t instance, const uint8_t* descRepo
 	// By default host stack will use activate boot protocol on supported interface.
 	// Therefore for this simple example, we only need to parse generic report descriptor (with built-in parser)
 	if (itfProtocol == HID_ITF_PROTOCOL_NONE) {
-		hid_info[instance].report_count = ::tuh_hid_parse_report_descriptor(
-							hid_info[instance].report_info, MAX_REPORT, descReport, descLen);
-		::printf("HID has %u reports \r\n", hid_info[instance].report_count);
+		hid_info[iInstance].report_count = ::tuh_hid_parse_report_descriptor(
+							hid_info[iInstance].report_info, MAX_REPORT, descReport, descLen);
+		::printf("HID has %u reports \r\n", hid_info[iInstance].report_count);
 	}
 	// request to receive report
 	// tuh_hid_report_received_cb() will be invoked when report is available
-	if (!::tuh_hid_receive_report(devAddr, instance)) {
+	if (!::tuh_hid_receive_report(devAddr, iInstance)) {
 		::printf("Error: cannot request to receive report\r\n");
 	}
 }
 
-void tuh_hid_umount_cb(uint8_t devAddr, uint8_t instance)
+void tuh_hid_umount_cb(uint8_t devAddr, uint8_t iInstance)
 {
-	::printf("tuh_hid_umount_cb(%d, %d)\n", devAddr, instance);
+	::printf("tuh_hid_umount_cb(%d, %d)\n", devAddr, iInstance);
 }
 
-void tuh_hid_report_received_cb(uint8_t devAddr, uint8_t instance, const uint8_t* report, uint16_t len)
+void tuh_hid_report_received_cb(uint8_t devAddr, uint8_t iInstance, const uint8_t* report, uint16_t len)
 {
-	uint8_t const itfProtocol = ::tuh_hid_interface_protocol(devAddr, instance);
-	::printf("report\n");
+	uint8_t const itfProtocol = ::tuh_hid_interface_protocol(devAddr, iInstance);
 	switch (itfProtocol) {
 	case HID_ITF_PROTOCOL_NONE: {
 		break;
 	}
 	case HID_ITF_PROTOCOL_KEYBOARD: {
-		auto reportSpecific = *reinterpret_cast<const hid_keyboard_report_t*>(report);
-
+		USBHost::GetKeyboard().OnReport(devAddr, iInstance, *reinterpret_cast<const hid_keyboard_report_t*>(report));
 		break;
 	}
 	case HID_ITF_PROTOCOL_MOUSE: {
-		auto reportSpecific = *reinterpret_cast<const hid_mouse_report_t*>(report);
+		USBHost::GetMouse().OnReport(devAddr, iInstance, *reinterpret_cast<const hid_mouse_report_t*>(report));
 		break;
 	}
 	default: break;
 	}
-	if (!::tuh_hid_receive_report(devAddr, instance)) {
+	if (!::tuh_hid_receive_report(devAddr, iInstance)) {
 		printf("Error: cannot request to receive report\r\n");
 	}
 }
 
+}
+
+//------------------------------------------------------------------------------
+// USBHost::Keyboard
+//------------------------------------------------------------------------------
+const uint8_t USBHost::Keyboard::convTbl_101Keyboard[128][2] = { HID_KEYCODE_TO_ASCII };
+
+USBHost::Keyboard::Keyboard() : Tickable(100), modifier_{0}, cntHold_{0}
+{
+	::memset(keycode_, 0x00, sizeof(keycode_));
+}
+
+void USBHost::Keyboard::OnReport(uint8_t devAddr, uint8_t iInstance, const hid_keyboard_report_t& report)
+{
+	modifier_ = report.modifier;
+	::memcpy(keycode_, report.keycode, sizeof(keycode_));
+	if (keycode_[0]) {
+		fifoKeyData_.WriteData(CreateKeyData(keycode_[0], modifier_));
+		cntHold_ = 5;
+	}
+}
+
+bool USBHost::Keyboard::GetKeyData(KeyData& keyData)
+{
+	if (fifoKeyData_.HasData()) {
+		keyData = fifoKeyData_.ReadData();
+		return true;
+	}
+	return false;
+}
+
+void USBHost::Keyboard::OnTick()
+{
+	if (cntHold_ > 0) {
+		cntHold_--;
+		return;
+	}
+	if (keycode_[0]) {
+		fifoKeyData_.WriteData(CreateKeyData(keycode_[0], modifier_));
+	}
+}
+
+KeyData USBHost::Keyboard::CreateKeyData(uint8_t keycode, uint8_t modifier)
+{
+	bool isShiftDown = !!(modifier & (KEYBOARD_MODIFIER_LEFTSHIFT | KEYBOARD_MODIFIER_RIGHTSHIFT));
+	char charCode = (keycode >= 128)? 0 : convTbl_101Keyboard[keycode][isShiftDown? 1 : 0];
+	return (charCode == 0)? KeyData(keycode, true, modifier) : KeyData(charCode, false, modifier);
+}
+
+//------------------------------------------------------------------------------
+// USBHost::Mouse
+//------------------------------------------------------------------------------
+USBHost::Mouse::Mouse()
+{
+}
+
+void USBHost::Mouse::OnReport(uint8_t devAddr, uint8_t iInstance, const hid_mouse_report_t& report)
+{
+	report.buttons;
+	report.pan;
+	report.wheel;
+	report.x;
+	report.y;
 }
 
 }
