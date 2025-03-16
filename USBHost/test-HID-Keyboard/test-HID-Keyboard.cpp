a12857500057365c44be6ed27ad182d003f522f0
ypsitau <ypsitau@nifty.com>
2025-03-17 06:20:36 +0900

    added test-HID-Keyboard

---
+++ USBHost/test-HID-Keyboard/test-HID-Keyboard.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "jxglib/USBHost.h"
+
+using namespace jxglib;
+
+int main()
+{
+	::stdio_init_all();
+	USBHost host;
+	host.Initialize();
+	::printf("----\n");
+	for (;;) {
+		KeyData keyData;
+		if (USBHost::GetKeyboard().GetKeyData(keyData)) {
+			if (keyData.IsKeyCode()) {
+				::printf("VK_%s\n", keyData.GetKeyCodeName());
+			} else if (keyData.GetCharCode() <= 'Z' - '@') {
+				::printf("Ctrl+%c\n", keyData.GetCharCode() + '@');
+			} else {
+				::printf("'%c'\n", keyData.GetCharCode());
+			}
+		}
+		Tickable::Sleep(100);
+	}
+}
