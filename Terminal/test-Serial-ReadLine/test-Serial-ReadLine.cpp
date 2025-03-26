6ea705b02bc8228b0e9cd92f46d4d39c959b4205
ypsitau <ypsitau@nifty.com>
2025-03-26 21:49:19 +0900

    modified test-Serial

--- Terminal/test-Serial-ReadLine/test-Serial-ReadLine.cpp
+++ Terminal/test-Serial-ReadLine/test-Serial-ReadLine.cpp
@@ -6,6 +6,15 @@ using namespace jxglib;
 
 Serial::Terminal terminal;
 
+GPIO::Key keyA(GPIO16, 'Z', GPIO::Key::PullUp);
+GPIO::Key keyB(GPIO17, 'X', GPIO::Key::PullUp);
+GPIO::Key keyLeft(GPIO18, VK_LEFT, GPIO::Key::PullUp);
+GPIO::Key keyUp(GPIO19, VK_UP, GPIO::Key::PullUp);
+GPIO::Key keyDown(GPIO20, VK_DOWN, GPIO::Key::PullUp);
+GPIO::Key keyRight(GPIO21, VK_RIGHT, GPIO::Key::PullUp);
+
+GPIO::Keyboard keyboard;
+
 TickableEntry(hoge, 300)
 {
 	static bool value = false;
@@ -17,7 +26,9 @@ int main()
 {
 	::stdio_init_all();
 	GPIO2.init().set_dir_OUT();
+	keyboard.Initialize();
 	terminal.Initialize();
+	//terminal.AttachInput(keyboard);
 	terminal.AttachInput(Stdio::Keyboard::Instance);
 	terminal.AttachOutput(Stdio::Printable::Instance);
 	terminal.Println("ReadLine Test Program");
