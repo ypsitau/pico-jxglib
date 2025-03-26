50884e84e33482bcd9db458c0c24d874285a9727
ypsitau <ypsitau@nifty.com>
2025-03-26 16:53:07 +0900

    implementing GPIO::Keyboard

--- Common/test-GPIO-Keyboard/test-GPIO-Keyboard.cpp
+++ Common/test-GPIO-Keyboard/test-GPIO-Keyboard.cpp
@@ -4,15 +4,16 @@
 
 using namespace jxglib;
 
+GPIO::Key keyLeft(GPIO10, VK_LEFT);
+GPIO::Key keyUp(GPIO11, VK_UP);
+GPIO::Key keyDown(GPIO12, VK_DOWN);
+GPIO::Key keyRight(GPIO13, VK_RIGHT);
+
 GPIO::Keyboard keyboard;
 
 int main()
 {
 	::stdio_init_all();
-	GPIO::Key keyLeft(GPIO10, VK_LEFT);
-	GPIO::Key keyUp(GPIO11, VK_UP);
-	GPIO::Key keyDown(GPIO12, VK_DOWN);
-	GPIO::Key keyRight(GPIO13, VK_RIGHT);
 	while (true) {
 		printf("Hello, world!\n");
 		sleep_ms(1000);
