1c39208371fcb18a93f6339b0e9a7ac16d6cefb8
ypsitau <ypsitau@nifty.com>
2025-04-06 22:08:47 +0900

    added test-ExtendSign

---
+++ Common/test-ExtendSign/test-ExtendSign.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <malloc.h>
+#include "pico/stdlib.h"
+#include "jxglib/Common.h"
+
+using namespace jxglib;
+
+template<typename T = int32_t> T SignExtend(uint32_t value, int nBits)
+{
+	int nBitsShift = 32 - nBits;
+	return static_cast<T>(static_cast<int32_t>(value << nBitsShift) >> nBitsShift);
+}
+
+int main()
+{
+	::stdio_init_all();
+	::printf("%d\n", SignExtend<>(0b111, 3));
+	::printf("%d\n", SignExtend<int8_t> (0b01111111, 8));
+	::printf("%d\n", SignExtend<int16_t>(0b01111111, 8));
+	::printf("%d\n", SignExtend<int32_t>(0b01111111, 8));
+	::printf("%d\n", SignExtend<int8_t> (0b01111110, 8));
+	::printf("%d\n", SignExtend<int16_t>(0b01111110, 8));
+	::printf("%d\n", SignExtend<int32_t>(0b01111110, 8));
+	::printf("%d\n", SignExtend<int8_t> (0b11111111, 8));
+	::printf("%d\n", SignExtend<int16_t>(0b11111111, 8));
+	::printf("%d\n", SignExtend<int32_t>(0b11111111, 8));
+	::printf("%d\n", SignExtend<int8_t> (0b11111110, 8));
+	::printf("%d\n", SignExtend<int16_t>(0b11111110, 8));
+	::printf("%d\n", SignExtend<int32_t>(0b11111110, 8));
+	::printf("%d\n", SignExtend<int8_t> (0b11111111111111, 14));
+	::printf("%d\n", SignExtend<int16_t>(0b11111111111111, 14));
+	::printf("%d\n", SignExtend<int32_t>(0b11111111111111, 14));
+	::printf("%d\n", SignExtend<int8_t> (0b11111111111110, 14));
+	::printf("%d\n", SignExtend<int16_t>(0b11111111111110, 14));
+	::printf("%d\n", SignExtend<int32_t>(0b11111111111110, 14));
+	for (;;) tight_loop_contents();
+}
