7b859b40488bf7881dbcb09cf29e33104b684cb2
ypsitau <ypsitau@nifty.com>
2025-04-07 16:48:59 +0900

    implementing USBHost

--- USBHost/src/USBHost.cpp
+++ USBHost/src/USBHost.cpp
@@ -264,8 +264,8 @@ void tuh_hid_mount_cb(uint8_t devAddr, uint8_t iInstance, const uint8_t* descRep
 							hid_info[iInstance].report_info, MAX_REPORT, descReport, descLen);
 		::printf("HID has %u reports \r\n", hid_info[iInstance].report_count);
 	}
-	ReportDescriptorHandler_Print handler;
-	USBHost::ParseReportDescriptor(handler, descReport, descLen);
+	//ReportDescriptorHandler_Print handler;
+	//USBHost::ParseReportDescriptor(handler, descReport, descLen);
 	// request to receive report
 	// tuh_hid_report_received_cb() will be invoked when report is available
 	if (!::tuh_hid_receive_report(devAddr, iInstance)) {
