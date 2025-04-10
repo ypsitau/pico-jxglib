7565fe6db5994f1ad31cd0fc6d136dc913138af9
ypsitau <ypsitau@nifty.com>
2025-04-10 14:39:57 +0900

    implementing Terminal

--- Terminal/test-SSD1306-ReadLine/test-SSD1306-ReadLine.cpp
+++ Terminal/test-SSD1306-ReadLine/test-SSD1306-ReadLine.cpp
@@ -2,7 +2,7 @@
 #include "pico/stdlib.h"
 #include "jxglib/USBHost.h"
 #include "jxglib/SSD1306.h"
-#include "jxglib/Font/shinonome12-japanese-level2.h"
+#include "jxglib/Font/shinonome12.h"
 
 using namespace jxglib;
 
@@ -17,7 +17,7 @@ int main()
 	GPIO5.set_function_I2C0_SCL().pull_up();
 	SSD1306 display(i2c0, 0x3c);
 	display.Initialize();
-	terminal.AttachDisplay(display).AttachKeyboard(USBHost::GetKeyboard().SetCapsLockAsCtrl())
+	terminal.AttachKeyboard(USBHost::GetKeyboard().SetCapsLockAsCtrl()).AttachDisplay(display)
 		.SetFont(Font::shinonome12).SetSpacingRatio(1., 1)
 		.SetColor(Color::white).SetColorBg(Color::black)
 		.SetColorInEdit(Color::white).SetColorCursor(Color::white)
--- Terminal/test-ST7789-ReadLine/test-ST7789-ReadLine.cpp
+++ Terminal/test-ST7789-ReadLine/test-ST7789-ReadLine.cpp
@@ -17,7 +17,7 @@ int main()
 	GPIO15.set_function_SPI1_TX();
 	ST7789 display(spi1, 240, 320, {RST: GPIO10, DC: GPIO11, CS: GPIO12, BL: GPIO13});
 	display.Initialize(Display::Dir::Rotate0);
-	terminal.AttachDisplay(display).AttachKeyboard(USBHost::GetKeyboard().SetCapsLockAsCtrl())
+	terminal.AttachKeyboard(USBHost::GetKeyboard().SetCapsLockAsCtrl()).AttachDisplay(display)
 		.SetFont(Font::shinonome16).SetSpacingRatio(1., 1)
 		.SetColor(Color::white).SetColorBg(Color::black)
 		.SetColorInEdit(Color::white).SetColorCursor(Color::white)
