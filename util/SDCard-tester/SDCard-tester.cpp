9a46fbb864c8038bd4b6c1638dce4656076e9f02
ypsitau <ypsitau@nifty.com>
2025-05-15 22:22:59 +0900

    implementing about-me

--- util/SDCard-tester/SDCard-tester.cpp
+++ util/SDCard-tester/SDCard-tester.cpp
@@ -14,8 +14,11 @@ SDCard sdCard(spi0, 10 * 1000 * 1000, {CS: GPIO5});	// 10MHz
 
 int main()
 {
-	bi_decl(bi_3pins_with_func(GPIO2, GPIO3, GPIO4, GPIO_FUNC_SPI));
-	bi_decl(bi_1pin_with_name(GPIO5, "CS"));
+	//bi_decl(bi_3pins_with_func(GPIO2, GPIO3, GPIO4, GPIO_FUNC_SPI));
+	bi_decl(bi_1pin_with_name(GPIO2, "SD Card SCK"));
+	bi_decl(bi_1pin_with_name(GPIO3, "SD Card MOSI"));
+	bi_decl(bi_1pin_with_name(GPIO4, "SD Card MISO"));
+	bi_decl(bi_1pin_with_name(GPIO5, "SD Card CS"));
 	::stdio_init_all();
 	GPIO2.set_function_SPI0_SCK();
 	GPIO3.set_function_SPI0_TX();
