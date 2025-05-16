dc786bba3931bae7b385935942df252593e5995a
ypsitau <ypsitau@nifty.com>
2025-05-16 16:42:36 +0900

    implementing BinaryInfo

---
+++ Common/test-BinaryInfo/test-BinaryInfo.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "jxglib/BinaryInfo.h"
+
+using namespace jxglib;
+
+int main()
+{
+	bi_decl(bi_1pin_with_func(GPIO2, GPIO_FUNC_I2C));
+	bi_decl(bi_2pins_with_func(GPIO3, GPIO4, GPIO_FUNC_PIO0));
+	bi_decl(bi_3pins_with_func(GPIO5, GPIO6, GPIO7, GPIO_FUNC_PIO1));
+	bi_decl(bi_4pins_with_func(GPIO8, GPIO9, GPIO10, GPIO11, GPIO_FUNC_PWM));
+	bi_decl(bi_5pins_with_func(GPIO12, GPIO13, GPIO14, GPIO15, GPIO16, GPIO_FUNC_USB));
+	bi_decl(bi_1pin_with_name(GPIO17, "name1"));
+	bi_decl(bi_2pins_with_names(GPIO18, "name1", GPIO19, "name2"));
+	bi_decl(bi_3pins_with_names(GPIO20, "name1", GPIO21, "name2", GPIO22, "name3"));
+	bi_decl(bi_4pins_with_names(GPIO23, "name1", GPIO24, "name2", GPIO25, "name3", GPIO26, "name4"));
+	::stdio_init_all();
+	auto& tout = Stdio::Instance;
+	BinaryInfo::PrintProgramInformation(tout);    
+	tout.Println();
+	BinaryInfo::PrintFixedPinInformation(tout);    
+	tout.Println();
+	BinaryInfo::PrintBuildInformation(tout);    
+	tout.Println();
+	BinaryInfo::PrintMemoryMap(tout);    
+	//BinaryInfo::Print(tout);    
+	for (;;) ::tight_loop_contents();
+}
--- Shell/src/ShellCmd_Basic.cpp
+++ Shell/src/ShellCmd_Basic.cpp
@@ -5,6 +5,7 @@
 #include "pico/binary_info.h"
 #include "hardware/clocks.h"
 #include "jxglib/Shell.h"
+#include "jxglib/BinaryInfo.h"
 
 #if defined(__riscv)
 	#define CPU_ARCH "RISC-V"
@@ -14,24 +15,7 @@
 	#define CPU_ARCH "Unknown"
 #endif
 
-extern char __flash_binary_start;
-extern char __flash_binary_end;
-extern char __data_start__;
-extern char __data_end__;
-extern char __bss_start__;
-extern char __bss_end__;
-extern char __heap_start;
-extern char __heap_end;
-extern char __stack;
-#if defined(__arm__)
-extern uint32_t ram_vector_table;
-#endif
-extern uint32_t __binary_info_start;
-extern uint32_t __binary_info_end;
-extern uint32_t __address_mapping_table;
-
 namespace jxglib::ShellCmd_Basic {
-
 
 //-----------------------------------------------------------------------------
 // about-platform
@@ -50,281 +34,18 @@ ShellCmd_Named(about_cpu, "about-platform", "prints information about the platfo
 	return 0;
 }
 
-class BinaryInfo {
-public:
-	static const char* GetString(uint32_t id);
-	static void PrintGPIOUsage(Printable& tout);
-	static void Print(Printable& tout);
-	static const binary_info_t** GetInfoStart() { return reinterpret_cast<const binary_info_t**>(&__binary_info_start); }
-	static const binary_info_t** GetInfoEnd() { return reinterpret_cast<const binary_info_t**>(&__binary_info_end); }
-	static const char* GetPinFuncName(int func);
-};
-
-const char* BinaryInfo::GetString(uint32_t id)
-{
-	for (auto ppInfo = GetInfoStart(); ppInfo != GetInfoEnd(); ++ppInfo) {
-		const binary_info_t* pInfo = *ppInfo;
-		if (pInfo->type == BINARY_INFO_TYPE_ID_AND_STRING) {
-			auto& info = *reinterpret_cast<const binary_info_id_and_string_t*>(pInfo);
-			if (info.id == id) return info.value;
-		}
-	}
-	return "";
-}
-
-void BinaryInfo::PrintGPIOUsage(Printable& tout)
-{
-	bool firstInfoFlag = true;
-	for (auto ppInfo = GetInfoEnd(); ppInfo != GetInfoStart(); --ppInfo) {
-		const binary_info_t* pInfo = *(ppInfo - 1);
-		switch (pInfo->type) {
-		case BINARY_INFO_TYPE_PINS_WITH_FUNC: {
-			auto& info = *reinterpret_cast<const binary_info_pins_with_func_t*>(pInfo);
-			uint32_t pin_encoding = info.pin_encoding;
-			uint32_t encodingFormat = pin_encoding & 0b111;
-			pin_encoding >>= 3;
-			uint32_t pinFunc = pin_encoding & 0b1111;
-			pin_encoding >>= 4;
-			switch (encodingFormat) {
-			case BI_PINS_ENCODING_RANGE: {
-				uint32_t pinLow = pin_encoding & 0b11111;
-				pin_encoding >>= 5;
-				uint32_t pinHigh = pin_encoding & 0b11111;
-				pin_encoding >>= 5;
-				if (firstInfoFlag) {
-					tout.Println();
-					firstInfoFlag = false;
-				}
-				tout.Printf("GPIO %d-%d: %s\n", pinLow, pinHigh, GetPinFuncName(pinFunc));
-				break;
-			}
-			case BI_PINS_ENCODING_MULTI: {
-				uint32_t pinPrev = -1;
-				if (firstInfoFlag) {
-					tout.Println();
-					firstInfoFlag = false;
-				}
-				tout.Printf("GPIO ");
-				for (int i = 0; i < 5; ++i) {
-					uint32_t pin = pin_encoding & 0b11111;
-					pin_encoding >>= 5;
-					if (pin == pinPrev) break;
-					tout.Printf("%s%d", (i == 0)? "" : ", ", pin);
-					pinPrev = pin;
-				}
-				tout.Printf(": %s\n", GetPinFuncName(pinFunc));
-				break;
-			}
-			default:
-				break;
-			}
-			break;
-		}
-		case BINARY_INFO_TYPE_PINS_WITH_NAME: {
-			auto& info = *reinterpret_cast<const binary_info_pins_with_name_t*>(pInfo);
-			uint32_t pin_mask = info.pin_mask;
-			bool firstFlag = true;
-			if (firstInfoFlag) {
-				tout.Println();
-				firstInfoFlag = false;
-			}
-			tout.Printf("GPIO ");
-			for (int pin = 0; pin_mask; pin++, pin_mask >>= 1) {
-				if (pin_mask & 1) {
-					tout.Printf("%s%d", firstFlag? "" : ", ", pin);
-					firstFlag = false;
-				}
-			}
-			tout.Printf(": %s\n", info.label);
-			break;
-		}
-		case BINARY_INFO_TYPE_PINS64_WITH_FUNC: {
-			auto& info = *reinterpret_cast<const binary_info_pins64_with_func_t*>(pInfo);
-			break;
-		}
-		case BINARY_INFO_TYPE_PINS64_WITH_NAME: {
-			auto& info = *reinterpret_cast<const binary_info_pins64_with_name_t*>(pInfo);
-			break;
-		}
-		default:
-			break;
-		}
-	}
-}
-
-void BinaryInfo::Print(Printable& tout)
-{
-	for (auto ppInfo = GetInfoStart(); ppInfo != GetInfoEnd(); ++ppInfo) {
-		const binary_info_t* pInfo = *ppInfo;
-		tout.Printf("%04x %04x\n", pInfo->tag, pInfo->type);
-		switch (pInfo->type) {
-		case BINARY_INFO_TYPE_RAW_DATA: {
-			auto& info = *reinterpret_cast<const binary_info_raw_data_t*>(pInfo);
-			break;
-		}
-		case BINARY_INFO_TYPE_SIZED_DATA: {
-			auto& info = *reinterpret_cast<const binary_info_sized_data_t*>(pInfo);
-			tout.Printf("Sized data %d bytes\n", info.length);
-			tout.Dump(info.bytes, info.length);
-			break;
-		}
-		case BINARY_INFO_TYPE_BINARY_INFO_LIST_ZERO_TERMINATED: {
-			auto& info = *reinterpret_cast<const binary_info_list_zero_terminated_t*>(pInfo);
-			break;
-		}
-		case BINARY_INFO_TYPE_BSON: {
-			auto& info = *reinterpret_cast<const binary_info_sized_data_t*>(pInfo);
-			break;
-		}
-		case BINARY_INFO_TYPE_ID_AND_INT: {
-			auto& info = *reinterpret_cast<const binary_info_id_and_int_t*>(pInfo);
-			tout.Printf("%08x: %08x\n", info.id, info.value);
-			break;
-		}
-		case BINARY_INFO_TYPE_ID_AND_STRING: {
-			auto& info = *reinterpret_cast<const binary_info_id_and_string_t*>(pInfo);
-			tout.Printf("%08x: %s\n", info.id, info.value);
-			break;
-		}
-		case BINARY_INFO_TYPE_BLOCK_DEVICE: {
-			auto& info = *reinterpret_cast<const binary_info_id_and_string_t*>(pInfo);
-			break;
-		}
-		case BINARY_INFO_TYPE_PINS_WITH_FUNC: {
-			auto& info = *reinterpret_cast<const binary_info_pins_with_func_t*>(pInfo);
-			uint32_t pin_encoding = info.pin_encoding;
-			uint32_t encodingFormat = pin_encoding & 0b111;
-			pin_encoding >>= 3;
-			uint32_t pinFunc = pin_encoding & 0b1111;
-			pin_encoding >>= 4;
-			switch (encodingFormat) {
-			case BI_PINS_ENCODING_RANGE: {
-				uint32_t pinLow = pin_encoding & 0b11111;
-				pin_encoding >>= 5;
-				uint32_t pinHigh = pin_encoding & 0b11111;
-				pin_encoding >>= 5;
-				tout.Printf("Pins %d-%d: %08x\n", pinLow, pinHigh, pinFunc);
-				break;
-			}
-			case BI_PINS_ENCODING_MULTI: {
-				tout.Printf("Pins");
-				uint32_t pinPrev = -1;
-				for (int i = 0; i < 5; ++i) {
-					uint32_t pin = pin_encoding & 0b11111;
-					pin_encoding >>= 5;
-					if (pin == pinPrev) break;
-					tout.Printf(" %d", pin);
-					pinPrev = pin;
-				}
-				tout.Printf(": %08x\n", pinFunc);
-				break;
-			}
-			default:
-				break;
-			}
-			break;
-		}
-		case BINARY_INFO_TYPE_PINS_WITH_NAME: {
-			auto& info = *reinterpret_cast<const binary_info_pins_with_name_t*>(pInfo);
-			uint32_t pin_mask = info.pin_mask;
-			tout.Printf("Pins");
-			for (int pin = 0; pin_mask; pin++, pin_mask >>= 1) {
-				if (pin_mask & 1) tout.Printf(" %d", pin);
-			}
-			tout.Printf(": %s\n", info.label);
-			break;
-		}
-		case BINARY_INFO_TYPE_NAMED_GROUP: {
-			auto& info = *reinterpret_cast<const binary_info_id_and_string_t*>(pInfo);
-			break;
-		}
-		case BINARY_INFO_TYPE_PTR_INT32_WITH_NAME: {
-			auto& info = *reinterpret_cast<const binary_info_ptr_int32_with_name_t*>(pInfo);
-			break;
-		}
-		case BINARY_INFO_TYPE_PTR_STRING_WITH_NAME: {
-			auto& info = *reinterpret_cast<const binary_info_ptr_string_with_name_t*>(pInfo);
-			break;
-		}
-		case BINARY_INFO_TYPE_PINS64_WITH_FUNC: {
-			auto& info = *reinterpret_cast<const binary_info_pins64_with_func_t*>(pInfo);
-			break;
-		}
-		case BINARY_INFO_TYPE_PINS64_WITH_NAME: {
-			auto& info = *reinterpret_cast<const binary_info_pins64_with_name_t*>(pInfo);
-			break;
-		}
-		default:
-			break;
-		}
-	}
-}
-
-const char* BinaryInfo::GetPinFuncName(int pinFunc)
-{
-#if defined(PICO_RP2040)
-	switch (pinFunc) {
-	case GPIO_FUNC_XIP:		return "XIP"; 
-	case GPIO_FUNC_SPI:		return "SPI"; 
-	case GPIO_FUNC_UART:	return "UART"; 
-	case GPIO_FUNC_I2C:		return "I2C"; 
-	case GPIO_FUNC_PWM:		return "PWM"; 
-	case GPIO_FUNC_SIO:		return "SIO"; 
-	case GPIO_FUNC_PIO0:	return "PIO0"; 
-	case GPIO_FUNC_PIO1:	return "PIO1"; 
-	case GPIO_FUNC_GPCK:	return "GPCK"; 
-	case GPIO_FUNC_USB:		return "USB"; 
-	default:				return "unknown";
-	}
-#elif defined(PICO_RP2350)
-	switch (pinFunc) {
-	case GPIO_FUNC_HSTX:	return "HSTX";
-	case GPIO_FUNC_SPI:		return "SPI";
-	case GPIO_FUNC_UART:	return "UART";
-	case GPIO_FUNC_I2C:		return "I2C";
-	case GPIO_FUNC_PWM:		return "PWM";
-	case GPIO_FUNC_SIO:		return "SIO";
-	case GPIO_FUNC_PIO0:	return "PIO0";
-	case GPIO_FUNC_PIO1:	return "PIO1";
-	case GPIO_FUNC_PIO2:	return "PIO2";
-	case GPIO_FUNC_GPCK:	return "GPCK";
-	//case GPIO_FUNC_XIP_CS1:	return "XIP_CS1";
-	//case GPIO_FUNC_CORESIGHT_TRACE:	return "CORESIGHT_TRACE";
-	case GPIO_FUNC_USB:		return "USB";
-	case GPIO_FUNC_UART_AUX:return "UART_AUX";
-	default:				return "unknown";
-	}
-#else
-	return "unknown"
-#endif
-}
-
 //-----------------------------------------------------------------------------
 // about-me
 //-----------------------------------------------------------------------------
 ShellCmd_Named(about_me, "about-me", "prints information about this own program")
 {
-	tout.Printf("Name        %s\n", BinaryInfo::GetString(BINARY_INFO_ID_RP_PROGRAM_NAME));
-	tout.Printf("Version     %s\n", BinaryInfo::GetString(BINARY_INFO_ID_RP_PROGRAM_VERSION_STRING));
-	tout.Printf("Date        %s\n", BinaryInfo::GetString(BINARY_INFO_ID_RP_PROGRAM_BUILD_DATE_STRING));
-	//tout.Printf("binary end      %s\n", BinaryInfo::GetString(BINARY_INFO_ID_RP_BINARY_END));
-	tout.Printf("URL         %s\n", BinaryInfo::GetString(BINARY_INFO_ID_RP_PROGRAM_URL));
-	tout.Printf("Description %s\n", BinaryInfo::GetString(BINARY_INFO_ID_RP_PROGRAM_DESCRIPTION));
-	tout.Printf("Feature     %s\n", BinaryInfo::GetString(BINARY_INFO_ID_RP_PROGRAM_FEATURE));
-	tout.Printf("Attribute   %s\n", BinaryInfo::GetString(BINARY_INFO_ID_RP_PROGRAM_BUILD_ATTRIBUTE));
-	tout.Printf("SDK version %s\n", BinaryInfo::GetString(BINARY_INFO_ID_RP_SDK_VERSION));
-	tout.Printf("Pico board  %s\n", BinaryInfo::GetString(BINARY_INFO_ID_RP_PICO_BOARD));
-	tout.Printf("Boot2       %s\n", BinaryInfo::GetString(BINARY_INFO_ID_RP_BOOT2_NAME));
-	BinaryInfo::PrintGPIOUsage(tout);
+	BinaryInfo::PrintProgramInformation(tout);    
 	tout.Println();
-	tout.Printf("Flash  0x%p-0x%p %7d\n", &__flash_binary_start, &__flash_binary_end, &__flash_binary_end - &__flash_binary_start);
-#if defined(__arm__)
-	tout.Printf("Vector 0x%p-0x%p %7d\n", &ram_vector_table, &ram_vector_table + PICO_RAM_VECTOR_TABLE_SIZE, PICO_RAM_VECTOR_TABLE_SIZE * sizeof(void*));
-#endif
-	tout.Printf("Data   0x%p-0x%p %7d\n", &__data_start__, &__data_end__, &__data_end__ - &__data_start__);
-	tout.Printf("Bss    0x%p-0x%p %7d\n", &__bss_start__, &__bss_end__, &__bss_end__ - &__bss_start__);
-	tout.Printf("Heap   0x%p-0x%p %7d\n", &__heap_start, &__heap_end, &__heap_end - &__heap_start);
-	tout.Printf("Stack  0x%p-0x%p %7d\n", &__heap_end, &__stack, &__stack - &__heap_end);
+	BinaryInfo::PrintFixedPinInformation(tout);    
+	tout.Println();
+	BinaryInfo::PrintBuildInformation(tout);    
+	tout.Println();
+	BinaryInfo::PrintMemoryMap(tout);    
 	//BinaryInfo::Print(tout);
 	return 0;
 }
