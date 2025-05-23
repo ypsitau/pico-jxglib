a15a8e004ec47e6f09e68c0117dbb501c934f29a
ypsitau <ypsitau@nifty.com>
2025-05-23 15:34:36 +0900

    implementing FS

--- FS/test-Composite/test-Composite.cpp
+++ FS/test-Composite/test-Composite.cpp
@@ -13,15 +13,16 @@ using namespace jxglib;
 int main()
 {
 	::stdio_init_all();
-	FAT::Flash fat1("a", 0x1010'0000, 0x0004'0000); // XIP address and size
-	FAT::Flash fat2("b", 0x1014'0000, 0x0004'0000); // XIP address and size
 	GPIO2.set_function_SPI0_SCK();
 	GPIO3.set_function_SPI0_TX();
 	GPIO4.set_function_SPI0_RX();
-	FAT::SDCard fat3("c", spi0, 10'000'000, {CS: GPIO5});
-	FAT::RAMDisk fat4("d", 100000);
-	LFS::Flash lfs1("e", 0x1018'0000, 0x0004'0000); // XIP address and size
-	LFS::Flash lfs2("f", 0x101c'0000, 0x0004'0000); // XIP address and size
+	LFS::Flash lfs1("a", 0x1018'0000, 0x0004'0000); // XIP address and size
+	LFS::Flash lfs2("b", 0x101c'0000, 0x0004'0000); // XIP address and size
+	FAT::Flash fat1("c", 0x1010'0000, 0x0004'0000); // XIP address and size
+	FAT::Flash fat2("d", 0x1014'0000, 0x0004'0000); // XIP address and size
+	FAT::SDCard fat3("e", spi0, 10'000'000, {CS: GPIO5});
+	//FAT::SDCard fat4("f", spi0, 10'000'000, {CS: GPIO6});
+	FAT::RAMDisk fat5("g", 100'000);
 	Serial::Terminal terminal;
 	Shell::AttachTerminal(terminal.Initialize());
 	for (;;) Tickable::Tick();
