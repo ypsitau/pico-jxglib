ae803e30775364c82d344850fa84857ce514a027
ypsitau <ypsitau@nifty.com>
2025-05-24 11:59:53 +0900

    implementing FS

--- FS/test-Composite/test-Composite.cpp
+++ FS/test-Composite/test-Composite.cpp
@@ -16,13 +16,12 @@ int main()
 	GPIO2.set_function_SPI0_SCK();
 	GPIO3.set_function_SPI0_TX();
 	GPIO4.set_function_SPI0_RX();
-	LFS::Flash lfs1("a", 0x1018'0000, 0x0004'0000); // XIP address and size
-	LFS::Flash lfs2("b", 0x101c'0000, 0x0004'0000); // XIP address and size
-	FAT::Flash fat1("c", 0x1010'0000, 0x0004'0000); // XIP address and size
-	FAT::Flash fat2("d", 0x1014'0000, 0x0004'0000); // XIP address and size
-	FAT::SDCard fat3("e", spi0, 10'000'000, {CS: GPIO5});
-	//FAT::SDCard fat4("f", spi0, 10'000'000, {CS: GPIO6});
-	FAT::RAMDisk fat5("g", 100'000);
+	LFS::Flash		drive_A("a", 0x1010'0000, 0x0004'0000); 	// Flash address and size 256kB
+	LFS::Flash		drive_B("b", 0x1014'0000, 0x0004'0000); 	// Flash address and size 256kB
+	FAT::Flash		drive_C("c", 0x1018'0000, 0x0004'0000); 	// Flash address and size 256kB
+	FAT::Flash		drive_D("d", 0x101c'0000, 0x0004'0000); 	// Flash address and size 256kB
+	FAT::SDCard		drive_E("e", spi0, 10'000'000, {CS: GPIO5});// SDCard on SPI0 10MHz
+	FAT::RAMDisk	drive_F("f", 0x2'0000);						// RAM Disk 128kB
 	Serial::Terminal terminal;
 	Shell::AttachTerminal(terminal.Initialize());
 	for (;;) Tickable::Tick();
