a5d2c4c90c3ba560b04e531050be345a268d0f9a
ypsitau <ypsitau@nifty.com>
2025-06-05 21:35:24 +0900

    implementing USBMSC

--- FS/FAT/src/USBMSC.cpp
+++ FS/FAT/src/USBMSC.cpp
@@ -28,35 +28,35 @@ DSTATUS USBMSC::status()
 DSTATUS USBMSC::initialize()
 {
 	//::printf("initialize\n");
-	//sdCard_.Initialize();
 	return 0x00;
 }
 
 DRESULT USBMSC::read(BYTE* buff, LBA_t sector, UINT count)
 {
 	//::printf("read(sector=%d, count=%d)\n", sector, count);
-	//return sdCard_.ReadBlock(sector, buff, count)? RES_OK : RES_ERROR;
+	if (!msc_.IsMounted()) return RES_NOTRDY;
 	return msc_.read10(buff, sector, count)? RES_OK : RES_ERROR;
 }
 
 DRESULT USBMSC::write(const BYTE* buff, LBA_t sector, UINT count)
 {
 	//::printf("write(sector=%d, count=%d)\n", sector, count);
-	//return sdCard_.WriteBlock(sector, buff, count)? RES_OK : RES_ERROR;
+	if (!msc_.IsMounted()) return RES_NOTRDY;
 	return msc_.write10(buff, sector, count)? RES_OK : RES_ERROR;
 }
 
 DRESULT USBMSC::ioctl_CTRL_SYNC()
 {
 	//::printf("ioctl(CTRL_SYNC)\n");
+	if (!msc_.IsMounted()) return RES_NOTRDY;
 	return RES_OK;
 }
 
 DRESULT USBMSC::ioctl_GET_SECTOR_COUNT(LBA_t* pSectorCount)
 {
 	//::printf("ioctl(GET_SECTOR_COUNT)\n");
-	//*pSectorCount = sdCard_.GetSectorCount();
-	//*pSectorCount = msc_.getCapacity();
+	*pSectorCount = 0;
+	if (!msc_.IsMounted()) return RES_NOTRDY;
 	*pSectorCount = msc_.get_block_count();
 	return RES_OK;
 }
@@ -64,6 +64,8 @@ DRESULT USBMSC::ioctl_GET_SECTOR_COUNT(LBA_t* pSectorCount)
 DRESULT USBMSC::ioctl_GET_SECTOR_SIZE(WORD* pSectorSize)
 {
 	//::printf("ioctl(GET_SECTOR_SIZE)\n");
+	*pSectorSize = 0;
+	if (!msc_.IsMounted()) return RES_NOTRDY;
 	*pSectorSize = msc_.get_block_size();
 	return RES_OK;
 }
@@ -71,6 +73,8 @@ DRESULT USBMSC::ioctl_GET_SECTOR_SIZE(WORD* pSectorSize)
 DRESULT USBMSC::ioctl_GET_BLOCK_SIZE(DWORD* pBlockSize)
 {
 	//::printf("ioctl(GET_BLOCK_SIZE)\n");
+	*pBlockSize = 0;
+	if (!msc_.IsMounted()) return RES_NOTRDY;
 	*pBlockSize = 1 << 0;
 	return RES_OK;
 }
@@ -78,7 +82,7 @@ DRESULT USBMSC::ioctl_GET_BLOCK_SIZE(DWORD* pBlockSize)
 DRESULT USBMSC::ioctl_CTRL_TRIM(LBA_t startLBA, LBA_t endLBA)
 {
 	//::printf("ioctl(CTRL_TRIM)\n");
-	return RES_OK;
+	return msc_.IsMounted()? RES_OK : RES_NOTRDY;
 }
 
 }
