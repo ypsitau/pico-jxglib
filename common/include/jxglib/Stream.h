f511ec943dcc21e92aa14f25720e5255760e00db
ypsitau <ypsitau@nifty.com>
2025-06-24 14:19:28 +0900

    implmenting YModem

--- common/include/jxglib/Stream.h
+++ common/include/jxglib/Stream.h
@@ -14,7 +14,7 @@ namespace jxglib {
 //------------------------------------------------------------------------------
 class Stream : public Printable, public Readable {
 public:
-	virtual int Read(void* buff, int bytesBuff) = 0;
+	//virtual int Read(void* buff, int bytesBuff) = 0;
 	virtual int Write(const void* buff, int bytesBuff) = 0;
 public:
 	bool WriteTo(Stream&& streamTo);
