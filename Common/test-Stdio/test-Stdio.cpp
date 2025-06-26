0ae4c343ab2f63bbff8956149d57930c42c30ffd
ypsitau <ypsitau@nifty.com>
2025-06-26 13:14:49 +0900

    implementing Stdio as Stream

--- Common/test-Stdio/test-Stdio.cpp
+++ Common/test-Stdio/test-Stdio.cpp
@@ -6,6 +6,13 @@ using namespace jxglib;
 
 int main()
 {
-    ::stdio_init_all();
-    Stdio::Instance.Printf("Hello, World!\n");
+	::stdio_init_all();
+	Stream& stream = Stdio::Instance;
+	for (;;) {
+		char buff[256];
+		for (int i = 0; i < sizeof(buff); ) {
+			i += stream.Read(buff + i, sizeof(buff) - i);
+		}
+		Dump(buff, sizeof(buff));
+	}
 }
