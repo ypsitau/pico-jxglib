c50fcc2e67afbc40770c2e7458649fd35798af8e
ypsitau <ypsitau@nifty.com>
2025-07-03 16:03:14 +0900

    implementing pico-jxgLABO

--- util/pico-jxgLABO/pico-jxgLABO.cpp
+++ util/pico-jxgLABO/pico-jxgLABO.cpp
@@ -4,12 +4,14 @@
 #include "jxglib/Serial.h"
 #include "jxglib/Shell.h"
 #include "jxglib/I2C.h"
+#include "jxglib/FAT/Flash.h"
 
 using namespace jxglib;
 
 int main()
 {
 	::stdio_init_all();
+	FAT::Flash drive("*C:", 0x1010'0000, 0x0010'0000); // Flash address and size 1MB
 	Serial::Terminal terminal;
 	Shell::AttachTerminal(terminal.Initialize());
 	for (;;) Tickable::Tick();
