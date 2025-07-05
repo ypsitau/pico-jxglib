659767581948d43ce8f063abda71acc780358b50
ypsitau <ypsitau@nifty.com>
2025-07-05 22:23:30 +0900

    implementing USBDevice

--- USBDevice/test-MSC-Flash/test-MSC-Flash.cpp
+++ USBDevice/test-MSC-Flash/test-MSC-Flash.cpp
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "jxglib/Shell.h"
 #include "jxglib/Serial.h"
 #include "jxglib/USBDevice/Flash.h"
@@ -16,19 +13,20 @@ int main(void)
 	::stdio_init_all(); 
 	USBDevice::Controller deviceController({
 		bcdUSB:				0x0200,
-		bDeviceClass:		0x00,
-		bDeviceSubClass:	0x00,
-		bDeviceProtocol:	0x00,
+		bDeviceClass:		TUSB_CLASS_MISC,
+		bDeviceSubClass:	MISC_SUBCLASS_COMMON,
+		bDeviceProtocol:	MISC_PROTOCOL_IAD,
 		bMaxPacketSize0:	CFG_TUD_ENDPOINT0_SIZE,
 		idVendor:			0xcafe,
-		idProduct:			USBDevice::GenerateSpecificProductId(0x4000),
+		idProduct:			USBDevice::GenerateSpecificProductId(0x4431),
 		bcdDevice:			0x0100,
 	}, 0x0409, "RPi Flash", "RPi Flash Device", "3141592653");
 	USBDevice::Flash flash(deviceController, 0x1010'0000, 0x010'0000, 0x01, 0x81);
+	
+	deviceController.Initialize();
+	//flash.Initialize();
 	FAT::Flash fat("*C:", 0x1010'0000, 0x010'0000);
 	Serial::Terminal terminal;
 	Shell::AttachTerminal(terminal.Initialize());
-	deviceController.Initialize();
-	flash.Initialize();
 	for (;;) Tickable::Tick();
 }
--- USBDevice/test-Serial/test-Serial.cpp
+++ USBDevice/test-Serial/test-Serial.cpp
@@ -1,5 +1,7 @@
 #include "jxglib/Shell.h"
+#include "jxglib/USBDevice/Flash.h"
 #include "jxglib/USBDevice/Serial.h"
+#include "jxglib/FAT/Flash.h"
 
 using namespace jxglib;
 
@@ -19,9 +21,11 @@ int main(void)
 		idProduct:			USBDevice::GenerateSpecificProductId(0x4000),
 		bcdDevice:			0x0100,
 	}, 0x0409, "CDC Stream", "CDC Stream Product", "0123456");
+	USBDevice::Flash flash(deviceController, 0x1010'0000, 0x010'0000, 0x03, 0x83);
 	USBDevice::Serial serial(deviceController, "CDC Stream", 0x81, 0x02, 0x82);
 	deviceController.Initialize();
 	serial.Initialize();
+	FAT::Flash fat("*C:", 0x1010'0000, 0x010'0000);
 	Serial::Terminal terminal;
 	terminal.AttachKeyboard(serial.GetKeyboard()).AttachPrintable(serial);
 	Shell::AttachTerminal(terminal.Initialize());
