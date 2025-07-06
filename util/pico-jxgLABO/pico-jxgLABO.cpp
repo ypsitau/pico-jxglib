1da01c9afb6c6b0c586f5dff641a6d5d9ed8fa40
ypsitau <ypsitau@nifty.com>
2025-07-06 19:01:25 +0900

    updated pico-jxgLABO

--- util/pico-jxgLABO/pico-jxgLABO.cpp
+++ util/pico-jxgLABO/pico-jxgLABO.cpp
@@ -1,16 +1,13 @@
 #include "jxglib/Shell.h"
+#include "jxglib/Serial.h"
 #include "jxglib/USBDevice/MSCDrive.h"
 #include "jxglib/USBDevice/CDCSerial.h"
 #include "jxglib/FAT/Flash.h"
 
 using namespace jxglib;
 
-//-----------------------------------------------------------------------------
-// main
-//-----------------------------------------------------------------------------
 int main(void)
 {
-	::stdio_init_all(); 
 	USBDevice::Controller deviceController({
 		bcdUSB:				0x0200,
 		bDeviceClass:		TUSB_CLASS_MISC,
@@ -20,13 +17,13 @@ int main(void)
 		idVendor:			0xcafe,
 		idProduct:			USBDevice::GenerateSpecificProductId(0x4000),
 		bcdDevice:			0x0100,
-	}, 0x0409, "CDC Stream", "CDC Stream Product", "0123456");
-	USBDevice::MSCDrive mscDrive(deviceController, 0x03, 0x83);
-	USBDevice::CDCSerial cdcSerial(deviceController, "CDC Serial", 0x81, 0x02, 0x82);
+	}, 0x0409, "jxglib", "pico-jxgLABO", "0123456");
+	FAT::Flash fat("*G:", 0x1010'0000, 0x010'0000);
+	USBDevice::MSCDrive mscDrive(deviceController, 0x01, 0x81);
+	USBDevice::CDCSerial cdcSerial(deviceController, "CDCSerial", 0x82, 0x03, 0x83);
 	deviceController.Initialize();
+	mscDrive.Initialize(fat);
 	cdcSerial.Initialize();
-	FAT::Flash fat("*C:", 0x1010'0000, 0x010'0000);
-	mscDrive.AttachDrive(fat);
 	Serial::Terminal terminal;
 	terminal.AttachKeyboard(cdcSerial.GetKeyboard()).AttachPrintable(cdcSerial);
 	Shell::AttachTerminal(terminal.Initialize());
