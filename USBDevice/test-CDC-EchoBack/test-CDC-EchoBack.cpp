f12556e35f56c1454298798d42c5a8e5a1c7b567
ypsitau <ypsitau@nifty.com>
2025-07-07 15:28:01 +0900

    reimplemented CDC-EchoBack

--- USBDevice/test-CDC-EchoBack/test-CDC-EchoBack.cpp
+++ USBDevice/test-CDC-EchoBack/test-CDC-EchoBack.cpp
@@ -4,26 +4,6 @@
 using namespace jxglib;
 
 //-----------------------------------------------------------------------------
-// CDC_EchoBack
-//-----------------------------------------------------------------------------
-class CDC_EchoBack : public USBDevice::CDC {
-public:
-	CDC_EchoBack(USBDevice::Controller& deviceController, const char* name, uint8_t endpNotif, uint8_t endpBulkOut, uint8_t endpBulkIn) :
-				USBDevice::CDC(deviceController, name, endpNotif, 8, endpBulkOut, endpBulkIn, 64, 10) {}
-public:
-	virtual void OnTick() override;
-};
-
-void CDC_EchoBack::OnTick()
-{
-	if (!cdc_available()) return;
-	char buff[64];
-	int bytes = cdc_read(buff, sizeof(buff));
-	cdc_write(buff, bytes);
-	cdc_write_flush();
-}
-
-//-----------------------------------------------------------------------------
 // main
 //-----------------------------------------------------------------------------
 int main(void)
@@ -39,8 +19,16 @@ int main(void)
 		idProduct:			USBDevice::GenerateSpecificProductId(0x4000),
 		bcdDevice:			0x0100,
 	}, 0x0409, "CDC EchoBack", "CDC EchoBack Product", "0123456");
-	CDC_EchoBack cdc(deviceController, "EchoBack Normal", 0x81, 0x02, 0x82);
+	USBDevice::CDC cdc(deviceController, "EchoBack Normal", 0x81, 8, 0x02, 0x82, 64);
 	deviceController.Initialize();
 	cdc.Initialize();
-	for (;;) Tickable::Tick();
+	for (;;) {
+		if (cdc.cdc_available()) {
+			char buff[64];
+			int bytes = cdc.cdc_read(buff, sizeof(buff));
+			cdc.cdc_write(buff, bytes);
+			cdc.cdc_write_flush();
+		}
+		Tickable::Tick();
+	}
 }
