9d0eef0232814d0248355bb9cc7a0cb246ff0a8c
ypsitau <ypsitau@nifty.com>
2025-07-07 15:35:48 +0900

    reimplemented CDC-EchoBack

--- USBDevice/test-CDC-Multi/test-CDC-Multi.cpp
+++ USBDevice/test-CDC-Multi/test-CDC-Multi.cpp
@@ -5,44 +5,6 @@
 using namespace jxglib;
 
 //-----------------------------------------------------------------------------
-// CDC_EchoBack
-//-----------------------------------------------------------------------------
-class CDC_EchoBack : public USBDevice::CDC {
-public:
-	enum Mode { Normal, Upper, Crypt };
-private:
-	Mode mode_;
-public:
-	CDC_EchoBack(USBDevice::Controller& deviceController, const char* name, uint8_t endpNotif, uint8_t endpBulkOut, uint8_t endpBulkIn, Mode mode) :
-				USBDevice::CDC(deviceController, name, endpNotif, 8, endpBulkOut, endpBulkIn, 64, 10), mode_{mode} {}
-public:
-	virtual void OnTick() override;
-};
-
-void CDC_EchoBack::OnTick()
-{
-	if (!cdc_available()) return;
-	char buff[64];
-	int bytes = cdc_read(buff, sizeof(buff));
-	switch (mode_) {
-	case Mode::Normal: {
-		break;
-	}
-	case Mode::Upper: {
-		for (int i = 0; i < bytes; i++) buff[i] = toupper(buff[i]);
-		break;
-	}
-	case Mode::Crypt: {
-		for (int i = 0; i < bytes; i++) buff[i] = buff[i] + 1;
-		break;
-	}
-	default: break;
-	}
-	cdc_write(buff, bytes);
-	cdc_write_flush();
-}
-
-//-----------------------------------------------------------------------------
 // main
 //-----------------------------------------------------------------------------
 int main(void)
@@ -58,12 +20,32 @@ int main(void)
 		idProduct:			USBDevice::GenerateSpecificProductId(0x4000),
 		bcdDevice:			0x0100,
 	}, 0x0409, "CDC Test", "CDC Test Product", "0123456");
-	CDC_EchoBack cdc1(deviceController, "EchoBack Normal", 0x81, 0x02, 0x82, CDC_EchoBack::Mode::Normal);
-	CDC_EchoBack cdc2(deviceController, "EchoBack Upper", 0x83, 0x04, 0x84, CDC_EchoBack::Mode::Upper);
-	CDC_EchoBack cdc3(deviceController, "EchoBack Crypt", 0x85, 0x06, 0x86, CDC_EchoBack::Mode::Crypt);
+	USBDevice::CDC cdc1(deviceController, "EchoBack Normal", 0x81, 8, 0x02, 0x82, 64);
+	USBDevice::CDC cdc2(deviceController, "EchoBack Upper", 0x83, 8, 0x04, 0x84, 64);
+	USBDevice::CDC cdc3(deviceController, "EchoBack Crypt", 0x85, 8, 0x06, 0x86, 64);
 	deviceController.Initialize();
 	cdc1.Initialize();
 	cdc2.Initialize();
 	cdc3.Initialize();
-	for (;;) Tickable::Tick();
+	for (;;) {
+		char buff[64];
+		if (cdc1.cdc_available()) {
+			int bytes = cdc1.cdc_read(buff, sizeof(buff));
+			cdc1.cdc_write(buff, bytes);
+			cdc1.cdc_write_flush();
+		}
+		if (cdc2.cdc_available()) {
+			int bytes = cdc2.cdc_read(buff, sizeof(buff));
+			for (int i = 0; i < bytes; i++) buff[i] = toupper(buff[i]);
+			cdc2.cdc_write(buff, bytes);
+			cdc2.cdc_write_flush();
+		}
+		if (cdc3.cdc_available()) {
+			int bytes = cdc3.cdc_read(buff, sizeof(buff));
+			for (int i = 0; i < bytes; i++) buff[i] = buff[i] + 1;
+			cdc3.cdc_write(buff, bytes);
+			cdc3.cdc_write_flush();
+		}
+		Tickable::Tick();
+	}
 }
