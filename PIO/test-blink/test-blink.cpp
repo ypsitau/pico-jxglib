2b8509ed40a5d9b538d511888cc6ba89e5a2cd28
ypsitau <ypsitau@nifty.com>
2025-07-15 22:55:33 +0900

    implementing test-blink

--- PIO/test-blink/test-blink.cpp
+++ PIO/test-blink/test-blink.cpp
@@ -9,6 +9,7 @@ int main()
 	const GPIO& gpioLED = GPIO15;
 	::stdio_init_all();
 	PIO::Program program;
+#if 0
 	program
 	.program("hello")
 	.L("loop")
@@ -16,13 +17,32 @@ int main()
 		.out("pins",	1)
 		.jmp("loop")
 	.end();
+#else
+	program
+	.program("blink")
+		.pull().block()
+		.out("y",		32)			// y <- osr[31:0], osr >>= 32
+	.wrap_target()
+		.mov("x",		"y")
+		.set("pins",	0b1)
+	.L("loop1")
+		.jmp("x--",		"loop1")	// Delay for (x + 1) cycles
+		.mov("x",		"y")
+		.set("pins",	0b0)
+	.L("loop2")
+		.jmp("x--",		"loop2")	// Delay for (x + 1) cycles
+	.wrap()
+	.end();
+#endif
 	PIO::StateMachine sm(program);
 	sm.ClaimResource();
+	sm.config.set_set_pin(gpioLED);
 	sm.config.set_out_pin(gpioLED);
 	sm.pio.gpio_init(gpioLED);
 	sm.set_pindir_out(gpioLED);
 	sm.init();
 	sm.set_enabled();
+#if 0
 	while (::getchar_timeout_us(0) == PICO_ERROR_TIMEOUT) {
 		sm.put_blocking(1);
 		::sleep_ms(500);
@@ -30,5 +50,8 @@ int main()
 		::sleep_ms(500);
 	}
 	sm.UnclaimResource();
+#else
+	sm.put((125000000 / (2 * 2)) - 3);
+#endif
 	for (;;) ::tight_loop_contents();
 }
