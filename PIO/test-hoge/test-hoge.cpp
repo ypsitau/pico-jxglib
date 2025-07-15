e6e8c6f12db54528854e0778eafd2486974ac4a8
ypsitau <ypsitau@nifty.com>
2025-07-15 21:01:40 +0900

    implementing test-ho

--- PIO/test-hoge/test-hoge.cpp
+++ PIO/test-hoge/test-hoge.cpp
@@ -5,24 +5,6 @@
 
 using namespace jxglib;
 
-void blink_pin_forever(pio_t pio, uint sm, uint offset, uint pin, uint freq)
-{
-	pio_gpio_init(pio, pin);
-	pio_sm_set_consecutive_pindirs(pio, sm, pin, 1, true);
-    pio_sm_config c = pio_get_default_sm_config();
-    sm_config_set_wrap(&c, offset + blink_wrap_target, offset + blink_wrap);
-	//pio_sm_config c = blink_program_get_default_config(offset);
-	sm_config_set_set_pins(&c, pin, 1);
-	pio_sm_init(pio, sm, offset, &c);
-	pio_sm_set_enabled(pio, sm, true);
-
-	printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-	// PIO counter program takes 3 more cycles in total than we pass as
-	// input (wait for n + 1; mov; jmp)
-	pio->txf[sm] = (125000000 / (2 * freq)) - 3;
-}
-
 int main()
 {
 	::stdio_init_all();
@@ -43,13 +25,28 @@ int main()
 	.wrap()				// Blink forever!
 	.end();
 
-
+	uint sm = 0;
+	uint pin = 15;
+	uint freq = 5;
 	// PIO Blinking example
 	pio_t pio = pio0;
 	uint offset = pio_add_program(pio, blink_program_jxg);
 	printf("Loaded program at %d\n", offset);
 	
-	blink_pin_forever(pio, 0, offset, 15, 3);
+	pio_gpio_init(pio, pin);
+	pio_sm_set_consecutive_pindirs(pio, sm, pin, 1, true);
+    pio_sm_config c = pio_get_default_sm_config();
+    sm_config_set_wrap(&c, offset + blink_wrap_target, offset + blink_wrap);
+	//pio_sm_config c = blink_program_get_default_config(offset);
+	sm_config_set_set_pins(&c, pin, 1);
+	pio_sm_init(pio, sm, offset, &c);
+	pio_sm_set_enabled(pio, sm, true);
+
+	printf("Blinking pin %d at %d Hz\n", pin, freq);
+
+	// PIO counter program takes 3 more cycles in total than we pass as
+	// input (wait for n + 1; mov; jmp)
+	pio->txf[sm] = (125000000 / (2 * freq)) - 3;
 	// For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
 
 	while (true) {
