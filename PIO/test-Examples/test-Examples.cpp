733317fff1df349e409aa9313cf150614fc59a0c
ypsitau <ypsitau@nifty.com>
2025-07-18 17:12:32 +0900

    implementing PIO

--- PIO/test-Examples/test-Examples.cpp
+++ PIO/test-Examples/test-Examples.cpp
@@ -16,75 +16,79 @@ int main()
 	do {
 		PIO::Program program;
 		program
-		.program("spi_cpha0")
-		.side_set(1)
-			.out("pins", 1)		.side(0)	[1] // Stall here on empty (sideset proceeds even if
-			.in("pins", 1)		.side(1)	[1] // instruction stalls, so we stall with SCK low)
+		.program("addition")
+			.pull()									// Pull first operand
+			.mov("x", "~osr")						// Store complement of first operand in x
+			.pull()									// Pull second operand
+			.mov("y", "osr")						// Store second operand in y
+			.jmp("test")							// this loop is equivalent to the following C code:
+		.L("incr")									// while (y--)
+			.jmp("x--", "test")						//     x--;
+		.L("test")									// This has the effect of subtracting y from x, eventually.
+			.jmp("y--", "incr")
+			.mov("isr", "~x")						// Store result (complement of x) in ISR
+			.push()									// Push result to FIFO
 		.end();
-		CheckProgram(program, spi_cpha0_program);
+		CheckProgram(program, addition_program);
 	} while (0);
 	do {
 		PIO::Program program;
 		program
-		.program("spi_cpha1")
-		.side_set(1)
-			.out("x", 1)		.side(0)		// Stall here on empty (keep SCK deasserted)
-			.mov("pins", "x")	.side(1)	[1]	// Output data, assert SCK (mov pins uses OUT mapping)
-			.in("pins", 1)		.side(0)		// Input data, deassert SCK
-		.end();
-		CheckProgram(program, spi_cpha1_program);
-	} while (0);
-	do {
-		PIO::Program program;
-		program
-		.program("uart_rx")
-		.L("start")
-			.wait(0, "pin", 0)					// Stall until start bit is asserted
-			.set("x", 7)					[10]// Preload bit counter, then delay until halfway through
-		.L("bitloop")
-			.in("pins", 1)						// Shift data bit into ISR
-			.jmp("x--", "bitloop")			[6]	// Loop 8 times, each loop iteration is 8 cycles
-			.jmp("pin", "good_stop")			// Check stop bit (should be high)
-			.irq(4).rel()						// Either a framing error or a break. Set a sticky flag,
-			.wait(1, "pin", 0)					// and wait for line to return to idle state.
-			.jmp("start")						// Don't push data if we didn't see good framing.
-		.L("good_stop")							// No delay before returning to start; a little slack is
-			.push()								// important in case the TX clock is slightly too fast
-		.end();
-		CheckProgram(program, uart_rx_program);
-	} while (0);
-	do {
-		PIO::Program program;
-		program
-		.program("uart_tx")
+		.program("differential_manchester_tx")
 		.side_set(1).opt()
-			.pull()			.side(1)		[7]	// Assert stop bit, or stall with line in idle state
-			.set("x", 7)	.side(0)		[7]	// Preload bit counter, assert start bit for 8 clocks
-		.L("bitloop")							// This loop will run 8 times (8n1 UART)
-			.out("pins", 1)						// Shift 1 bit from OSR to the first OUT pin
-			.jmp("x--", "bitloop")			[6]	// Each loop iteration is 8 cycles.
+		.L("start")									// public start:
+		.L("initial_high")
+			.out("x", 1)							// Start of bit period: always assert transition
+			.jmp("!x", "high_0")	.side(1)	[6]	// Test the data bit we just shifted out of OSR
+		.L("high_1")
+			.nop()
+			.jmp("initial_high")	.side(0)	[6]	// For `1` bits, also transition in the middle
+		.L("high_0")
+			.jmp("initial_low")					[7]	// Otherwise, the line is stable in the middle
+		.L("initial_low")
+			.out("x", 1)							// Always shift 1 bit from OSR to X so we can
+			.jmp("!x", "low_0")		.side(0)	[6]	// branch on it. Autopull refills OSR for us.
+		.L("low_1")
+			.nop()
+			.jmp("initial_low")		.side(1)	[6]	// If there are two transitions, return to
+		.L("low_0")
+			.jmp("initial_high")				[7]	// the initial line state is flipped!
 		.end();
-		CheckProgram(program, uart_tx_program);
+		CheckProgram(program, differential_manchester_tx_program);
 	} while (0);
 	do {
-		const uint T1 = 3;
-		const uint T2 = 3;
-		const uint T3 = 4;
 		PIO::Program program;
 		program
-		.program("ws2812")
-		.side_set(1)
-		.wrap_target()
+		.program("i2c")
+		.side_set(1).opt().pindirs()
+		.L("do_nack")
+			.jmp("y--", "entry_point")				// Continue if NAK was expected
+			.irq("wait", 0).rel()					// Otherwise stop, ask for help
+		.L("do_byte")
+			.set("x", 7)							// Loop 8 times
 		.L("bitloop")
-			.out("x", 1)			.side(0)	[T3 - 1]	// Side-set still takes place when instruction stalls
-			.jmp("!x", "do_zero")	.side(1)	[T1 - 1]	// Branch on the bit we shifted out. Positive pulse
-		.L("do_one")
-			.jmp("bitloop")			.side(1)	[T2 - 1]	// Continue driving high, for a long pulse
-		.L("do_zero")
-			.nop()					.side(0)	[T2 - 1]	// Or drive low, for a short pulse
+			.out("pindirs", 1)				[7]		// Serialise write data (all-ones if reading)
+			.nop()					.side(1)[2]		// SCL rising edge
+			.wait(1, "pin", 1)				[4]		// Allow clock to be stretched
+			.in("pins", 1)					[7]		// Sample read data in middle of SCL pulse
+			.jmp("x--", "bitloop")	.side(0)[7]		// SCL falling edge
+		// Handle ACK pulse
+			.out("pindirs", 1)				[7]		// On reads, we provide the ACK.
+			.nop()					.side(1)[7]		// SCL rising edge
+			.wait(1, "pin", 1)				[7]		// Allow clock to be stretched
+			.jmp("pin", "do_nack")	.side(0)[2]		// Test SDA for ACK/NAK, fall through if ACK
+		.L("entry_point")							// public entry_point:
+		.wrap_target()
+			.out("x", 6)							// Unpack Instr count
+			.out("y", 1)							// Unpack the NAK ignore bit
+			.jmp("!x", "do_byte")					// Instr == 0, this is a data record.
+			.out("null", 32)						// Instr > 0, remainder of this OSR is invalid
+		.L("do_exec")
+			.out("exec", 16)						// Execute one instruction per FIFO word
+			.jmp("x--", "do_exec")					// Repeat n + 1 times
 		.wrap()
 		.end();
-		CheckProgram(program, ws2812_program);
+		CheckProgram(program, i2c_program);
 	} while (0);
 	do {
 		PIO::Program program;
@@ -221,79 +225,75 @@ int main()
 	do {
 		PIO::Program program;
 		program
-		.program("i2c")
-		.side_set(1).opt().pindirs()
-		.L("do_nack")
-			.jmp("y--", "entry_point")				// Continue if NAK was expected
-			.irq("wait", 0).rel()					// Otherwise stop, ask for help
-		.L("do_byte")
-			.set("x", 7)							// Loop 8 times
+		.program("spi_cpha0")
+		.side_set(1)
+			.out("pins", 1)		.side(0)	[1] // Stall here on empty (sideset proceeds even if
+			.in("pins", 1)		.side(1)	[1] // instruction stalls, so we stall with SCK low)
+		.end();
+		CheckProgram(program, spi_cpha0_program);
+	} while (0);
+	do {
+		PIO::Program program;
+		program
+		.program("spi_cpha1")
+		.side_set(1)
+			.out("x", 1)		.side(0)		// Stall here on empty (keep SCK deasserted)
+			.mov("pins", "x")	.side(1)	[1]	// Output data, assert SCK (mov pins uses OUT mapping)
+			.in("pins", 1)		.side(0)		// Input data, deassert SCK
+		.end();
+		CheckProgram(program, spi_cpha1_program);
+	} while (0);
+	do {
+		PIO::Program program;
+		program
+		.program("uart_rx")
+		.L("start")
+			.wait(0, "pin", 0)					// Stall until start bit is asserted
+			.set("x", 7)					[10]// Preload bit counter, then delay until halfway through
 		.L("bitloop")
-			.out("pindirs", 1)				[7]		// Serialise write data (all-ones if reading)
-			.nop()					.side(1)[2]		// SCL rising edge
-			.wait(1, "pin", 1)				[4]		// Allow clock to be stretched
-			.in("pins", 1)					[7]		// Sample read data in middle of SCL pulse
-			.jmp("x--", "bitloop")	.side(0)[7]		// SCL falling edge
-		// Handle ACK pulse
-			.out("pindirs", 1)				[7]		// On reads, we provide the ACK.
-			.nop()					.side(1)[7]		// SCL rising edge
-			.wait(1, "pin", 1)				[7]		// Allow clock to be stretched
-			.jmp("pin", "do_nack")	.side(0)[2]		// Test SDA for ACK/NAK, fall through if ACK
-		.L("entry_point")							// public entry_point:
+			.in("pins", 1)						// Shift data bit into ISR
+			.jmp("x--", "bitloop")			[6]	// Loop 8 times, each loop iteration is 8 cycles
+			.jmp("pin", "good_stop")			// Check stop bit (should be high)
+			.irq(4).rel()						// Either a framing error or a break. Set a sticky flag,
+			.wait(1, "pin", 0)					// and wait for line to return to idle state.
+			.jmp("start")						// Don't push data if we didn't see good framing.
+		.L("good_stop")							// No delay before returning to start; a little slack is
+			.push()								// important in case the TX clock is slightly too fast
+		.end();
+		CheckProgram(program, uart_rx_program);
+	} while (0);
+	do {
+		PIO::Program program;
+		program
+		.program("uart_tx")
+		.side_set(1).opt()
+			.pull()			.side(1)		[7]	// Assert stop bit, or stall with line in idle state
+			.set("x", 7)	.side(0)		[7]	// Preload bit counter, assert start bit for 8 clocks
+		.L("bitloop")							// This loop will run 8 times (8n1 UART)
+			.out("pins", 1)						// Shift 1 bit from OSR to the first OUT pin
+			.jmp("x--", "bitloop")			[6]	// Each loop iteration is 8 cycles.
+		.end();
+		CheckProgram(program, uart_tx_program);
+	} while (0);
+	do {
+		const uint T1 = 3;
+		const uint T2 = 3;
+		const uint T3 = 4;
+		PIO::Program program;
+		program
+		.program("ws2812")
+		.side_set(1)
 		.wrap_target()
-			.out("x", 6)							// Unpack Instr count
-			.out("y", 1)							// Unpack the NAK ignore bit
-			.jmp("!x", "do_byte")					// Instr == 0, this is a data record.
-			.out("null", 32)						// Instr > 0, remainder of this OSR is invalid
-		.L("do_exec")
-			.out("exec", 16)						// Execute one instruction per FIFO word
-			.jmp("x--", "do_exec")					// Repeat n + 1 times
+		.L("bitloop")
+			.out("x", 1)			.side(0)	[T3 - 1]	// Side-set still takes place when instruction stalls
+			.jmp("!x", "do_zero")	.side(1)	[T1 - 1]	// Branch on the bit we shifted out. Positive pulse
+		.L("do_one")
+			.jmp("bitloop")			.side(1)	[T2 - 1]	// Continue driving high, for a long pulse
+		.L("do_zero")
+			.nop()					.side(0)	[T2 - 1]	// Or drive low, for a short pulse
 		.wrap()
 		.end();
-		CheckProgram(program, i2c_program);
-	} while (0);
-	do {
-		PIO::Program program;
-		program
-		.program("differential_manchester_tx")
-		.side_set(1).opt()
-		.L("start")									// public start:
-		.L("initial_high")
-			.out("x", 1)							// Start of bit period: always assert transition
-			.jmp("!x", "high_0")	.side(1)	[6]	// Test the data bit we just shifted out of OSR
-		.L("high_1")
-			.nop()
-			.jmp("initial_high")	.side(0)	[6]	// For `1` bits, also transition in the middle
-		.L("high_0")
-			.jmp("initial_low")					[7]	// Otherwise, the line is stable in the middle
-		.L("initial_low")
-			.out("x", 1)							// Always shift 1 bit from OSR to X so we can
-			.jmp("!x", "low_0")		.side(0)	[6]	// branch on it. Autopull refills OSR for us.
-		.L("low_1")
-			.nop()
-			.jmp("initial_low")		.side(1)	[6]	// If there are two transitions, return to
-		.L("low_0")
-			.jmp("initial_high")				[7]	// the initial line state is flipped!
-		.end();
-		CheckProgram(program, differential_manchester_tx_program);
-	} while (0);
-	do {
-		PIO::Program program;
-		program
-		.program("addition")
-			.pull()									// Pull first operand
-			.mov("x", "~osr")						// Store complement of first operand in x
-			.pull()									// Pull second operand
-			.mov("y", "osr")						// Store second operand in y
-			.jmp("test")							// this loop is equivalent to the following C code:
-		.L("incr")									// while (y--)
-			.jmp("x--", "test")						//     x--;
-		.L("test")									// This has the effect of subtracting y from x, eventually.
-			.jmp("y--", "incr")
-			.mov("isr", "~x")						// Store result (complement of x) in ISR
-			.push()									// Push result to FIFO
-		.end();
-		CheckProgram(program, addition_program);
+		CheckProgram(program, ws2812_program);
 	} while (0);
 	::printf("done\n");
 	for (;;) ::tight_loop_contents();
