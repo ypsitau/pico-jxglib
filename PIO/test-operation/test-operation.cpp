c39a7a40b0b77ae88b46c9218de7aa21ba9b8b81
ypsitau <ypsitau@nifty.com>
2025-07-19 12:27:50 +0900

    implementing PIO

--- PIO/test-operation/test-operation.cpp
+++ PIO/test-operation/test-operation.cpp
@@ -12,9 +12,9 @@ int main()
 		PIO::Program program;
 		program
 		.program("pull_mov_push")
-			.pull()
-			.mov("isr", "osr")
-			.push()
+			.pull()				// osr <- txfifo
+			.mov("isr", "osr")	// isr <- osr
+			.push()				// rxfifo <- isr
 		.end();
 		PIO::StateMachine sm;
 		sm.ClaimResource(program).init();
@@ -29,12 +29,12 @@ int main()
 		PIO::Program program;
 		program
 		.program("autopull_autopush_32bits")
-			.out("x", 32)	// osr <- txfifo, x[0:31] <- osr[0:31], osr <<= 32
-			.in("x", 32)	// isr[0:31] <- x[0:31], rxfifo <- isr
+			.out("x", 32)	// auto-pull (osr <- txfifo), x[31:0] <- osr[31:0], osr[31:0] = 0
+			.in("x", 32)	// isr[31:0] <- x[31:0], auto-push (rxfifo <- isr)
 		.end();
 		PIO::StateMachine sm;
-		sm.config.set_out_shift_left(true, 32);	// see 3.5.4. Autopush and Autopull
-		sm.config.set_in_shift_left(true, 32);	// see 3.5.4. Autopush and Autopull
+		sm.config.set_out_shift_right(true, 32);	// see 3.5.4. Autopush and Autopull
+		sm.config.set_in_shift_right(true, 32);		// see 3.5.4. Autopush and Autopull
 		sm.ClaimResource(program).init();
 		sm.set_enabled();
 		uint32_t num;
@@ -43,26 +43,78 @@ int main()
 		num = 0x89abcdef; ::printf("%08x -> %08x\n", num, sm.put(num).get());
 		sm.UnclaimResource();
 	} while (0);
-#if 0
 	do {
 		PIO::Program program;
 		program
-		.program("autopull_autopush_8its")
-			.out("isr", 8)	// osr <- txfifo, x <- osr[0:7], osr <<= 8
+		.program("autopull_right_8bits")
+			.out("isr", 8)	// auto-pull (osr <- txfifo) when empty, x <- osr[7:0], osr[23:0] = osr[31:8], osr[31:24] = 0
 			.push()
 		.end();
 		PIO::StateMachine sm;
-		sm.config.set_out_shift_left(true, 8);	// see 3.5.4. Autopush and Autopull
-		//sm.config.set_in_shift_left(true, 8);	// see 3.5.4. Autopush and Autopull
+		sm.config.set_out_shift_right(true, 32);	// see 3.5.4. Autopush and Autopull
 		sm.ClaimResource(program).init();
 		sm.set_enabled();
 		uint32_t num;
 		::printf("program: %s\n", program.GetName());
-		printf("%08x -> %08x\n", num, sm.put(0x12).get());
-		printf("%08x -> %08x\n", num, sm.put(0xab).get());
+		num = 0x01234567; sm.put(num); printf("%08x -> %02x, %02x, %02x, %02x\n", num, sm.get(), sm.get(), sm.get(), sm.get());
+		num = 0x89abcdef; sm.put(num); printf("%08x -> %02x, %02x, %02x, %02x\n", num, sm.get(), sm.get(), sm.get(), sm.get());
 		sm.UnclaimResource();
 	} while (0);
-#endif
+	do {
+		PIO::Program program;
+		program
+		.program("autopull_right_4bits")
+			.out("isr", 4)	// auto-pull (osr <- txfifo) when empty, x <- osr[3:0], osr[27:0] = osr[31:28], osr[31:28] = 0
+			.push()
+		.end();
+		PIO::StateMachine sm;
+		sm.config.set_out_shift_right(true, 32);	// see 3.5.4. Autopush and Autopull
+		sm.ClaimResource(program).init();
+		sm.set_enabled();
+		uint32_t num;
+		::printf("program: %s\n", program.GetName());
+		num = 0x01234567; sm.put(num); printf("%08x -> %x, %x, %x, %x, %x, %x, %x, %x\n",
+				num, sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get());
+		num = 0x89abcdef; sm.put(num); printf("%08x -> %x, %x, %x, %x, %x, %x, %x, %x\n",
+				num, sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get());
+		sm.UnclaimResource();
+	} while (0);
+	do {
+		PIO::Program program;
+		program
+		.program("autopull_left_8bits")
+			.out("isr", 8)	// auto-pull (osr <- txfifo) when empty, x <- osr[31:24], osr[31:8] = osr[23:0], osr[7:0] = 0
+			.push()
+		.end();
+		PIO::StateMachine sm;
+		sm.config.set_out_shift_left(true, 32);	// see 3.5.4. Autopush and Autopull
+		sm.ClaimResource(program).init();
+		sm.set_enabled();
+		uint32_t num;
+		::printf("program: %s\n", program.GetName());
+		num = 0x01234567; sm.put(num); printf("%08x -> %02x, %02x, %02x, %02x\n", num, sm.get(), sm.get(), sm.get(), sm.get());
+		num = 0x89abcdef; sm.put(num); printf("%08x -> %02x, %02x, %02x, %02x\n", num, sm.get(), sm.get(), sm.get(), sm.get());
+		sm.UnclaimResource();
+	} while (0);
+	do {
+		PIO::Program program;
+		program
+		.program("autopull_left_4bits")
+			.out("isr", 4)	// auto-pull (osr <- txfifo) when empty, x <- osr[31:28], osr[31:4] = osr[27:0], osr[3:0] = 0
+			.push()
+		.end();
+		PIO::StateMachine sm;
+		sm.config.set_out_shift_left(true, 32);	// see 3.5.4. Autopush and Autopull
+		sm.ClaimResource(program).init();
+		sm.set_enabled();
+		uint32_t num;
+		::printf("program: %s\n", program.GetName());
+		num = 0x01234567; sm.put(num); printf("%08x -> %x, %x, %x, %x, %x, %x, %x, %x\n",
+				num, sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get());
+		num = 0x89abcdef; sm.put(num); printf("%08x -> %x, %x, %x, %x, %x, %x, %x, %x\n",
+				num, sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get(), sm.get());
+		sm.UnclaimResource();
+	} while (0);
 	do {
 		PIO::Program program;
 		program
@@ -149,7 +201,7 @@ int main()
 		.program("four_bits_left_shift")
 			.pull()
 			.mov("isr", "osr")
-			.in("null", 4)			// isr[31:4] <- isr[27:0], isr[3:0] <- null[3:0]
+			.in("null", 4)			// isr[31:4] <- isr[27:0], isr[3:0] <- 0
 			.push()
 		.end();
 		PIO::StateMachine sm;
