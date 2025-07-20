733b8c1551924ce158e7aea4cd75933e51b6b4d9
ypsitau <ypsitau@nifty.com>
2025-07-20 20:02:43 +0900

    added LogicAnalyzer

---
+++ LogicAnalyzer/include/jxglib/LogicAnalyzer.h
@@ -0,0 +1,21 @@
+//==============================================================================
+// jxglib/LogicAnalyzer.h
+//==============================================================================
+#ifndef PICO_JXGLIB_LOGICANALYZER_H
+#define PICO_JXGLIB_LOGICANALYZER_H
+#include "pico/stdlib.h"
+#include "jxglib/Common.h"
+
+namespace jxglib {
+
+//------------------------------------------------------------------------------
+// LogicAnalyzer
+//------------------------------------------------------------------------------
+class LogicAnalyzer {
+public:
+	LogicAnalyzer() {}
+};
+
+}
+
+#endif
--- PIO/test-DMA/test-DMA.cpp
+++ PIO/test-DMA/test-DMA.cpp
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "jxglib/LABOPlatform.h"
 #include "jxglib/PIO.h"
+#include "jxglib/DMA.h"
 
 using namespace jxglib;
 
@@ -48,6 +49,27 @@ int main()
 	.end();
 	sm.config.set_in_shift_left(true, 32);	// shift left, autopush enabled, push threshold 32
 	sm.set_program(program).set_listen_pins(gpio, -1).init().set_enabled();
+	DMA::Channel& channel = *DMA::claim_unused_channel();
+	DMA::ChannelConfig config;
+	uint32_t dst[32];
+	config.set_enable(true)
+		.set_transfer_data_size(DMA_SIZE_32)
+		.set_read_increment(false)
+		.set_write_increment(true)
+		.set_dreq(sm.get_dreq_rx()) // set DREQ of StateMachine's rx
+		.set_chain_to(channel)		// disable by setting chain_to to itself
+		.set_ring_read(0)
+		.set_bswap(false)
+		.set_irq_quiet(false)
+		.set_sniff_enable(false)
+		.set_high_priority(false);
+	::printf("Transferred by DMA#%d: ", static_cast<uint>(channel));
+	channel.set_config(config)
+		.set_read_addr(sm.get_rxf())
+		.set_write_addr(dst)
+		.set_trans_count_trig(count_of(dst))
+		.wait_for_finish_blocking();
+
 	//-------------------------------------------------------------------------
 	LABOPlatform laboPlatform;
 	laboPlatform.AttachStdio().Initialize();
