98976772736efc10149f6394ffbd37a9b2d7c827
ypsitau <ypsitau@nifty.com>
2025-07-21 14:09:05 +0900

    implementing LogicAnalyzer

--- LogicAnalyzer/test-LogicAnalyzer/test-LogicAnalyzer.cpp
+++ LogicAnalyzer/test-LogicAnalyzer/test-LogicAnalyzer.cpp
@@ -19,6 +19,9 @@ private:
 	DMA::ChannelConfig config_;
 	DMA::Channel* pChannel_;
 	std::unique_ptr<Event> eventBuff_;
+	float usecReso_;
+	int nEventsToPrint_;
+	int nClocksPerLoop_;
 public:
 	LogicAnalyzer();
 	~LogicAnalyzer();
@@ -27,11 +30,15 @@ public:
 	LogicAnalyzer& Restart();
 	LogicAnalyzer& Stop();
 	LogicAnalyzer& Clear();
+	LogicAnalyzer& SetResolution(float usecReso) { usecReso_ = usecReso; return *this; }
+	LogicAnalyzer& SetEventCountToPrint(int nEventsToPrint) { nEventsToPrint_ = nEventsToPrint; return *this; }
+	float GetResolution() const { return usecReso_; }
 	int GetEventCount() const;
 	void PrintWave(Printable& tout) const;
+	const Event& GetEvent(int iEvent) const { return eventBuff_.get()[iEvent]; }
 };
 
-LogicAnalyzer::LogicAnalyzer() : pChannel_{nullptr}
+LogicAnalyzer::LogicAnalyzer() : pChannel_{nullptr}, usecReso_{1'000}, nEventsToPrint_{80}, nClocksPerLoop_{1}
 {}
 
 LogicAnalyzer::~LogicAnalyzer()
@@ -71,6 +78,7 @@ LogicAnalyzer& LogicAnalyzer::Start()
 		.mov("y", "x")				// save current pins state in y
 	.wrap()
 	.end();
+	nClocksPerLoop_ = 10;
 	sm_.config.set_in_shift_left(true, 32); // shift left, autopush enabled, push threshold 32
 	sm_.set_program(program_).set_listen_pins(gpio, -1).init().set_enabled();
 	pChannel_ = DMA::claim_unused_channel();
@@ -123,23 +131,23 @@ int LogicAnalyzer::GetEventCount() const
 void LogicAnalyzer::PrintWave(Printable& tout) const
 {
 	int nBitsToDisp = 16;
-	float tick = 1'000; // usec
-	float clockPIO = static_cast<float>(::clock_get_hz(clk_sys) / 10);
-	int nEvents = GetEventCount();
-	const Event& event = eventBuff_.get()[0];
+	float clockPIOProgram = static_cast<float>(::clock_get_hz(clk_sys) / nClocksPerLoop_);
+	int nEvents = ChooseMin(GetEventCount(), nEventsToPrint_);
+	if (nEvents == 0) return;
+	int iEventStart = GetEventCount() - nEvents;
+	const Event& eventStart = GetEvent(iEventStart);
 	tout.Printf("%*s", 14, "");
 	for (int iBit = 0; iBit < nBitsToDisp; ++iBit) {
-		tout.Print((event.bits & (1 << iBit))? "   |" : " |  ");
+		tout.Print((eventStart.bits & (1 << iBit))? "   |" : " |  ");
 	}
 	tout.Println();
-	uint32_t timeStampStart = eventBuff_.get()[1].timeStamp;
-	for (int iEvent = 1; iEvent < nEvents; ++iEvent) {
-		const Event& eventPrev = eventBuff_.get()[iEvent - 1];
-		const Event& event = eventBuff_.get()[iEvent];
+	for (int i = 1; i < nEvents; ++i) {
+		const Event& eventPrev = GetEvent(iEventStart + i - 1);
+		const Event& event = GetEvent(iEventStart + i);
 		if (eventPrev.bits == event.bits) continue; // skip if no change
-		float delta = static_cast<float>(event.timeStamp - eventPrev.timeStamp) * 1000'000 / clockPIO;
+		float delta = static_cast<float>(event.timeStamp - eventPrev.timeStamp) * 1000'000 / clockPIOProgram;
 		if (delta == 0) continue; // skip if no time difference
-		int nDelta = static_cast<int>(delta / tick);
+		int nDelta = static_cast<int>(delta / usecReso_);
 		if (nDelta < 40) {
 			for (int i = 0; i < nDelta; ++i) {
 				tout.Printf("%*s", 14, "");
@@ -149,18 +157,21 @@ void LogicAnalyzer::PrintWave(Printable& tout) const
 				tout.Println();
 			}
 		} else {
+			tout.Println();
 			tout.Printf("%*s", 14, "");
 			for (int iBit = 0; iBit < nBitsToDisp; ++iBit) {
 				tout.Print((eventPrev.bits & (1 << iBit))? "   :" : " :  ");
 			}
 			tout.Println();
+			tout.Println();
 		}
 		uint32_t bitsTransition = event.bits ^ eventPrev.bits;	
-		tout.Printf("%14.3f", static_cast<float>(event.timeStamp - timeStampStart) * 1000'000 / clockPIO);
+		tout.Printf("%14.3f", static_cast<float>(event.timeStamp - eventStart.timeStamp) * 1000'000 / clockPIOProgram);
 		for (int iBit = 0; iBit < nBitsToDisp; ++iBit) {
 			tout.Print((bitsTransition & (1 << iBit))? " +-+" : (event.bits & (1 << iBit))? "   |" : " |  ");
 		}
 		tout.Println();
+		if (Tickable::TickSub()) break;
 	}
 }
 
@@ -169,7 +180,9 @@ LogicAnalyzer logicAnalyzer;
 ShellCmd(la, "Logic Analyzer")
 {
 	static const Arg::Opt optTbl[] = {
-		Arg::OptBool("help",	'h', "prints this help"),
+		Arg::OptBool("help",		'h', "prints this help"),
+		Arg::OptString("reso",		'r', "resolution in microseconds (default 1000)", "RESO"),
+		Arg::OptString("events",	'e', "number of events to print (default 80)", "NUM"),
 	};
 	Arg arg(optTbl, count_of(optTbl));
 	if (!arg.Parse(terr, argc, argv)) return Result::Error;
@@ -180,9 +193,28 @@ ShellCmd(la, "Logic Analyzer")
 		tout.Printf("Sub Commands:\n");
 		return Result::Success;
 	}
+	const char* value;
+	if (arg.GetString("reso", &value)) {
+		char* endptr = nullptr;
+		float usecReso = ::strtod(value, &endptr);
+		if (endptr == value || *endptr != '\0' || usecReso <= 0) {
+			terr.Printf("Invalid resolution: %s\n", value);
+			return Result::Error;
+		}
+		logicAnalyzer.SetResolution(usecReso);
+	}
+	if (arg.GetString("events", &value)) {
+		char* endptr = nullptr;
+		int nEventsToPrint = ::strtol(value, &endptr, 10);
+		if (endptr == value || *endptr != '\0' || nEventsToPrint <= 0) {
+			terr.Printf("Invalid resolution: %s\n", value);
+			return Result::Error;
+		}
+		logicAnalyzer.SetEventCountToPrint(nEventsToPrint);
+	}
 	if (argc < 2) {
-		int nEvents = logicAnalyzer.GetEventCount();
-		tout.Printf("Number of events: %d\n", nEvents);
+		tout.Printf("reso:%.2fusec recorded-events:%d\n",
+					logicAnalyzer.GetResolution(), logicAnalyzer.GetEventCount());
 		return Result::Success;
 	}
 	const char* subCmd = argv[1];
