3569dad5f2ecf46eeee8e73a12711d7e7c4b52ff
ypsitau <ypsitau@nifty.com>
2025-08-10 11:37:43 +0900

    implementing Resets

--- Resets/include/jxglib/Resets.h
+++ Resets/include/jxglib/Resets.h
@@ -4,17 +4,41 @@
 #ifndef PICO_JXGLIB_RESETS_H
 #define PICO_JXGLIB_RESETS_H
 #include "pico/stdlib.h"
+#include "hardware/resets.h"
 #include "jxglib/Common.h"
 
 namespace jxglib {
 
-//------------------------------------------------------------------------------
-// Resets
-//------------------------------------------------------------------------------
-class Resets {
-public:
-	Resets() {}
-};
+inline void ResetMask(uint mask) { ::reset_block_mask(mask); }
+inline void UnresetMask(uint mask) { ::unreset_block_mask(mask); }
+inline void UnresetBlockingMask(uint mask) { ::unreset_block_mask_wait_blocking(mask); }
+inline void ResetUnresetBlockingMask(uint mask) {
+	::reset_block_mask(mask); ::unreset_block_mask_wait_blocking(mask);
+}
+
+template<typename... Args> void Reset(Args... bits)
+{
+	static_assert(sizeof...(bits) > 0, "At least one bit must be specified");
+	ResetMask(((1u << bits) | ...));
+}
+
+template<typename... Args> void Unreset(Args... bits)
+{
+	static_assert(sizeof...(bits) > 0, "At least one bit must be specified");
+	UnresetMask(((1u << bits) | ...));
+}
+
+template<typename... Args> void UnresetBlocking(Args... bits)
+{
+	static_assert(sizeof...(bits) > 0, "At least one bit must be specified");
+	UnresetBlockingMask(((1u << bits) | ...));
+}
+
+template<typename... Args> void ResetUnresetBlocking(Args... bits)
+{
+	static_assert(sizeof...(bits) > 0, "At least one bit must be specified");
+	ResetUnresetBlocking(((1u << bits) | ...));
+}
 
 }
 
--- Resets/test-Resets/test-Resets.cpp
+++ Resets/test-Resets/test-Resets.cpp
@@ -1,10 +1,12 @@
 #include "pico/stdlib.h"
 #include "jxglib/LABOPlatform.h"
+#include "jxglib/Resets.h"
 
 using namespace jxglib;
 
 int main(void)
 {
+	ResetUnresetBlocking(RESET_ADC, RESET_UART0);
 	::stdio_init_all();
 	LABOPlatform::Instance.AttachStdio().Initialize();
 	::adc_init();
