9b25f021c0031e594fd231c289e2c11f9518b0e6
ypsitau <ypsitau@nifty.com>
2025-09-16 14:16:35 +0900

    implementing WiFi

--- WiFi/test-WiFi/test-WiFi.cpp
+++ WiFi/test-WiFi/test-WiFi.cpp
@@ -17,12 +17,6 @@ using namespace jxglib;
 typedef struct TCP_SERVER_T_ {
 	struct tcp_pcb *server_pcb;
 	struct tcp_pcb *client_pcb;
-	bool complete;
-	uint8_t buffer_sent[BUF_SIZE];
-	uint8_t buffer_recv[BUF_SIZE];
-	int sent_len;
-	int recv_len;
-	int run_count;
 } TCP_SERVER_T;
 
 err_t callback_accept(void *arg, struct tcp_pcb *client_pcb, err_t err);
@@ -30,29 +24,28 @@ err_t callback_sent(void *arg, struct tcp_pcb *tpcb, u16_t len);
 err_t callback_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err);
 err_t callback_poll(void *arg, struct tcp_pcb *tpcb);
 void callback_err(void *arg, err_t err);
-err_t tcp_server_send_data(void *arg, struct tcp_pcb *tpcb);
 
 err_t tcp_server_close(void *arg)
 {
 	TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
 	err_t err = ERR_OK;
 	if (state->client_pcb != NULL) {
-		tcp_arg(state->client_pcb, NULL);
-		tcp_poll(state->client_pcb, NULL, 0);
-		tcp_sent(state->client_pcb, NULL);
-		tcp_recv(state->client_pcb, NULL);
-		tcp_err(state->client_pcb, NULL);
-		err = tcp_close(state->client_pcb);
+		::tcp_arg(state->client_pcb, NULL);
+		::tcp_poll(state->client_pcb, NULL, 0);
+		::tcp_sent(state->client_pcb, NULL);
+		::tcp_recv(state->client_pcb, NULL);
+		::tcp_err(state->client_pcb, NULL);
+		err = ::tcp_close(state->client_pcb);
 		if (err != ERR_OK) {
 			DEBUG_printf("close failed %d, calling abort\n", err);
-			tcp_abort(state->client_pcb);
+			::tcp_abort(state->client_pcb);
 			err = ERR_ABRT;
 		}
 		state->client_pcb = NULL;
 	}
 	if (state->server_pcb) {
-		tcp_arg(state->server_pcb, NULL);
-		tcp_close(state->server_pcb);
+		::tcp_arg(state->server_pcb, NULL);
+		::tcp_close(state->server_pcb);
 		state->server_pcb = NULL;
 	}
 	return err;
@@ -66,7 +59,6 @@ err_t tcp_server_result(void *arg, int status)
 	} else {
 		DEBUG_printf("test failed %d\n", status);
 	}
-	state->complete = true;
 	return tcp_server_close(arg);
 }
 
@@ -81,43 +73,48 @@ err_t callback_accept(void *arg, struct tcp_pcb *client_pcb, err_t err)
 	DEBUG_printf("Client connected\n");
 
 	state->client_pcb = client_pcb;
-	tcp_arg(client_pcb, state);
-	tcp_sent(client_pcb, callback_sent);
-	tcp_recv(client_pcb, callback_recv);
-	tcp_poll(client_pcb, callback_poll, POLL_TIME_S * 2);
-	tcp_err(client_pcb, callback_err);
+	::tcp_arg(client_pcb, state);
+	::tcp_sent(client_pcb, callback_sent);
+	::tcp_recv(client_pcb, callback_recv);
+	::tcp_poll(client_pcb, callback_poll, POLL_TIME_S * 2);
+	::tcp_err(client_pcb, callback_err);
 	return ERR_OK;
-	//return tcp_server_send_data(arg, state->client_pcb);
 }
 
 err_t callback_sent(void *arg, struct tcp_pcb *tpcb, u16_t len)
 {
-	TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-	DEBUG_printf("callback_sent %u\n", len);
-	state->sent_len += len;
-
-	if (state->sent_len >= BUF_SIZE) {
-
-		// We should get the data back from the client
-		state->recv_len = 0;
-		DEBUG_printf("Waiting for buffer from client\n");
-	}
-
 	return ERR_OK;
 }
 
 err_t callback_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *pbuf, err_t err)
 {
+	static bool firstFlag = true;
 	TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
 	if (!pbuf) return ERR_OK;
 	cyw43_arch_lwip_check();
 	::printf("callback_recv %d bytes\n", pbuf->tot_len);
 	if (pbuf->tot_len > 0) {
 		// Receive the buffer
-		const uint16_t buffer_left = BUF_SIZE - state->recv_len;
-		state->recv_len += ::pbuf_copy_partial(pbuf, state->buffer_recv + state->recv_len,
-											pbuf->tot_len > buffer_left ? buffer_left : pbuf->tot_len, 0);
+		uint8_t buff[128];
+		int len = ::pbuf_copy_partial(pbuf, buff, ChooseMin(pbuf->tot_len, sizeof(buff)), 0);
+		Dump(buff, len);
 		::tcp_recved(tpcb, pbuf->tot_len);
+		static const uint8_t buffSend[] = {
+			0xff, 0xfb, 0x01,
+			0xff, 0xfb, 0x03,
+			0xff, 0xfd, 0x18,
+			0xff, 0xfd, 0x1f,
+			0xff, 0xfd, 0x20,
+		};
+		if (buff[0] == 0xff && firstFlag) {
+			// telnet negotiation
+			::tcp_write(tpcb, buffSend, sizeof(buffSend), TCP_WRITE_FLAG_COPY);
+		} else {
+			// echo back
+			::tcp_write(tpcb, buff, len, TCP_WRITE_FLAG_COPY);
+		}
+		firstFlag = false;
+		//::tcp_write(tpcb, buffSend, sizeof(buffSend), TCP_WRITE_FLAG_COPY);
 	}
 	::pbuf_free(pbuf);
 	return ERR_OK;
@@ -125,38 +122,11 @@ err_t callback_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *pbuf, err_t er
 
 err_t callback_poll(void *arg, struct tcp_pcb *tpcb)
 {
-	//DEBUG_printf("callback_poll\n");
-	//return tcp_server_result(arg, -1); // no response is an error?
 	return ERR_OK;
 }
 
 void callback_err(void *arg, err_t err)
 {
-	if (err != ERR_ABRT) {
-		DEBUG_printf("callback_err %d\n", err);
-		tcp_server_result(arg, err);
-	}
-}
-
-err_t tcp_server_send_data(void *arg, struct tcp_pcb *tpcb)
-{
-	TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-	for(int i=0; i< BUF_SIZE; i++) {
-		state->buffer_sent[i] = rand();
-	}
-
-	state->sent_len = 0;
-	DEBUG_printf("Writing %ld bytes to client\n", BUF_SIZE);
-	// this method is callback from lwIP, so cyw43_arch_lwip_begin is not required, however you
-	// can use this method to cause an assertion in debug mode, if this method is called when
-	// cyw43_arch_lwip_begin IS needed
-	cyw43_arch_lwip_check();
-	err_t err = tcp_write(tpcb, state->buffer_sent, BUF_SIZE, TCP_WRITE_FLAG_COPY);
-	if (err != ERR_OK) {
-		DEBUG_printf("Failed to write data %d\n", err);
-		return tcp_server_result(arg, -1);
-	}
-	return ERR_OK;
 }
 
 void run_tcp_server_test(void)
@@ -168,51 +138,30 @@ void run_tcp_server_test(void)
 	}
 	DEBUG_printf("Starting server at %s on port %u\n", ip4addr_ntoa(netif_ip4_addr(netif_list)), TCP_PORT);
 
-	struct tcp_pcb *pcb = tcp_new_ip_type(IPADDR_TYPE_ANY);
+	struct tcp_pcb *pcb = ::tcp_new_ip_type(IPADDR_TYPE_ANY);
 	if (!pcb) {
 		DEBUG_printf("failed to create pcb\n");
 		return;
 	}
 
-	err_t err = tcp_bind(pcb, NULL, TCP_PORT);
+	err_t err = ::tcp_bind(pcb, NULL, TCP_PORT);
 	if (err) {
 		DEBUG_printf("failed to bind to port %u\n", TCP_PORT);
 		return;
 	}
 
-	state->server_pcb = tcp_listen_with_backlog(pcb, 1);
+	state->server_pcb = ::tcp_listen_with_backlog(pcb, 1);
 	if (!state->server_pcb) {
 		DEBUG_printf("failed to listen\n");
 		if (pcb) {
-			tcp_close(pcb);
+			::tcp_close(pcb);
 		}
 		return;
 	}
 
-	tcp_arg(state->server_pcb, state);
-	tcp_accept(state->server_pcb, callback_accept);
-
-
-
-	while(!state->complete) {
-		// the following #ifdef is only here so this same example can be used in multiple modes;
-		// you do not need it in your code
-#if PICO_CYW43_ARCH_POLL
-		// if you are using pico_cyw43_arch_poll, then you must poll periodically from your
-		// main loop (not from a timer) to check for Wi-Fi driver or lwIP work that needs to be done.
-		//cyw43_arch_poll();
-		// you can poll as often as you like, however if you have nothing else to do you can
-		// choose to sleep until either a specified time, or cyw43_arch_poll() has work to do:
-		//cyw43_arch_wait_for_work_until(make_timeout_time_ms(1000));
-		Tickable::Tick();
-#else
-		// if you are not using pico_cyw43_arch_poll, then WiFI driver and lwIP work
-		// is done via interrupt in the background. This sleep is just an example of some (blocking)
-		// work you might be doing.
-		sleep_ms(1000);
-#endif
-	}
-	free(state);
+	::tcp_arg(state->server_pcb, state);
+	::tcp_accept(state->server_pcb, callback_accept);
+	//free(state);
 }
 
 WiFi wifi;
