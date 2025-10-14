bcb55fd6a06cdb5b36fec3a6e029eab9ac7258a0
ypsitau <ypsitau@nifty.com>
2025-10-14 15:32:30 +0900

    implementing Font

--- Display/test-ST7789/test-ST7789.cpp
+++ Display/test-ST7789/test-ST7789.cpp
@@ -14,7 +14,7 @@ int main()
 	GPIO15.set_function_SPI1_TX();
 	Display::ST7789 display(spi1, 240, 320, {RST: GPIO10, DC: GPIO11, CS: GPIO12, BL: GPIO13});
 	display.Initialize(Display::Dir::Rotate0);
-	DrawableTest::DrawString(display);
+	//DrawableTest::DrawString(display);
 	//DrawableTest::RotateImage(display);
-	//DrawableTest::DrawFonts(display);
+	DrawableTest::DrawFonts(display);
 }
