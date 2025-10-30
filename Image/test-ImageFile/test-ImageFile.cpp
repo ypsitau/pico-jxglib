adf4bfde33e4cc2276ab3428dcd8630307a3af65
ypsitau <ypsitau@nifty.com>
2025-10-30 16:15:17 +0900

    added ShellCmd_ImageFile

--- Image/test-ImageFile/test-ImageFile.cpp
+++ Image/test-ImageFile/test-ImageFile.cpp
@@ -2,30 +2,8 @@
 #include "pico/stdlib.h"
 #include "jxglib/ImageFile.h"
 #include "jxglib/LABOPlatform.h"
-#include "jxglib/Display.h"
 
 using namespace jxglib;
-
-ShellCmd_Named(show_image, "show-image", "show image file")
-{
-	if (argc < 2) {
-		printf("Usage: show-image <image-file>\n");
-		return Result::Error;
-	}
-	const char* fileName = argv[1];
-	std::unique_ptr<FS::File> pFile(FS::OpenFile(fileName, "r"));
-	if (!pFile) {
-		printf("Failed to open file: %s\n", fileName);
-		return Result::Error;
-	}
-	Image image(Image::Format::RGB);
-	if (!ImageFile::Read(image, *pFile)) {
-		printf("Failed to read image file: %s\n", fileName);
-		return Result::Error;
-	}
-	Display::GetInstance(0).DrawImage(0, 0, image);
-	return Result::Success;
-}
 
 int main()
 {
