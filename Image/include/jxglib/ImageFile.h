6eb2a07438f182bfdc974fc4aa7c5e0b36db5011
ypsitau <ypsitau@nifty.com>
2025-10-30 15:49:46 +0900

    added ImageFile

---
+++ Image/include/jxglib/ImageFile.h
@@ -0,0 +1,10 @@
+//==============================================================================
+// jxglib/ImageFile.h
+//==============================================================================
+#ifndef PICO_JXGLIB_IMAGEFILE_H
+#define PICO_JXGLIB_IMAGEFILE_H
+
+namespace jxglib {
+}
+
+#endif
