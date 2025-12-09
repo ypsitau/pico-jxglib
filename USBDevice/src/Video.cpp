bcc4c35c97243999824c08bfa1fd72a4dc615466
ypsitau <ypsitau@nifty.com>
2025-12-09 16:21:35 +0900

    implementing USBDevice::Video

--- USBDevice/src/Video.cpp
+++ USBDevice/src/Video.cpp
@@ -5,31 +5,6 @@
 //==============================================================================
 #include "jxglib/USBDevice/Video.h"
 
-struct TU_ATTR_PACKED uvc_control_desc_t {
-	tusb_desc_interface_t itf;
-	tusb_desc_video_control_header_1itf_t header;
-	tusb_desc_video_control_camera_terminal_t camera_terminal;
-	tusb_desc_video_control_output_terminal_t output_terminal;
-};
-
-struct TU_ATTR_PACKED uvc_streaming_desc_t {
-	tusb_desc_interface_t itf;
-	tusb_desc_video_streaming_input_header_1byte_t header;
-	tusb_desc_video_format_uncompressed_t format;
-	tusb_desc_video_frame_uncompressed_continuous_t frame;
-	tusb_desc_video_streaming_color_matching_t color;
-#if !CFG_TUD_VIDEO_STREAMING_BULK
-	// For ISO streaming, USB spec requires to alternate interface
-	tusb_desc_interface_t itf_alt;
-#endif
-	tusb_desc_endpoint_t ep;
-};
-struct TU_ATTR_PACKED ConfigDesc {
-	tusb_desc_interface_assoc_t iad;
-	uvc_control_desc_t video_control;
-	uvc_streaming_desc_t video_streaming;
-};
-
 #if CFG_TUD_VIDEO > 0
 
 //-----------------------------------------------------------------------------
