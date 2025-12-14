ede336f21c25887ec908d4f9ae7f7ea203f720f7
ypsitau <ypsitau@nifty.com>
2025-12-14 14:48:52 +0900

    implementint Camera command

--- LABOPlatform/test-LABOPlatform/test-LABOPlatform.cpp
+++ LABOPlatform/test-LABOPlatform/test-LABOPlatform.cpp
@@ -11,45 +11,4 @@ int main(void)
 	::adc_init();
 	::adc_set_temp_sensor_enabled(true);
 	for (;;) Tickable::Tick();
-#if 0
-	VideoTransmitter& videoTransmitter = VideoTransmitter::GetInstance(0);
-	const Size& size = videoTransmitter.GetSize();
-	uint8_t* buffFrame = reinterpret_cast<uint8_t*>(::malloc(size.width * size.height * 16 / 8));
-	int startPos = 0;
-	for (;;) {
-		if (videoTransmitter.CanTransmit()) {
-			static const uint8_t bar_color[8][4] = {
-				//  Y,   U,   Y,   V
-				{ 235, 128, 235, 128 }, // 100% White
-				{ 219,  16, 219, 138 }, // Yellow
-				{ 188, 154, 188,  16 }, // Cyan
-				{ 173,  42, 173,  26 }, // Green
-				{  78, 214,  78, 230 }, // Magenta
-				{  63, 102,  63, 240 }, // Red
-				{  32, 240,  32, 118 }, // Blue
-				{  16, 128,  16, 128 }, // Black
-			};
-			uint8_t* end = &buffFrame[size.width * 2];
-			unsigned idx = (size.width / 2 - 1) - (startPos % (size.width / 2));
-			uint8_t* p = &buffFrame[idx * 4];
-			for (unsigned i = 0; i < 8; ++i) {
-				for (int j = 0; j < size.width / (2 * 8); ++j) {
-					memcpy(p, &bar_color[i], 4);
-					p += 4;
-					if (end <= p) {
-						p = buffFrame;
-					}
-				}
-			}
-			p = &buffFrame[size.width * 2];
-			for (unsigned i = 1; i < size.height; ++i) {
-				memcpy(p, buffFrame, size.width * 2);
-				p += size.width * 2;
-			}
-			startPos++;
-			videoTransmitter.Transmit(buffFrame);
-		}
-		Tickable::Tick();
-	}
-#endif
 }
