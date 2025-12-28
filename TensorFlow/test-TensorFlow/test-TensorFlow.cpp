80abf0190ef031e3d2e07e0c334c9631bc4ae1bb
ypsitau <ypsitau@nifty.com>
2025-12-28 12:04:35 +0900

    added test-TensorFlow

--- TensorFlow/test-TensorFlow/test-TensorFlow.cpp
+++ TensorFlow/test-TensorFlow/test-TensorFlow.cpp
@@ -1,11 +1,29 @@
 #include "pico/stdlib.h"
 #include "jxglib/LABOPlatform.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_log.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
 
 using namespace jxglib;
+
+uint8_t tensorArena[32768];
 
 int main(void)
 {
 	::stdio_init_all();
+	tflite::InitializeTarget();
+	const tflite::Model* model = nullptr; //tflite::GetModel(mnist_model);
+	static tflite::MicroMutableOpResolver<5> resolver;
+	resolver.AddConv2D();
+    resolver.AddMaxPool2D();
+    resolver.AddFullyConnected();
+    resolver.AddReshape();
+    resolver.AddSoftmax();
+	static tflite::MicroInterpreter interpreter(model, resolver, tensorArena, sizeof(tensorArena));
+	TfLiteTensor* input = interpreter.input(0);
+	TfLiteTensor* output = interpreter.output(0);
 	LABOPlatform::Instance.AttachStdio().Initialize();
 	//LABOPlatform::Instance.Initialize();
 	::adc_init();
