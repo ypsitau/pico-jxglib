95c0844f108131a6c2c989ec8d20e314e10bc1d7
ypsitau <ypsitau@nifty.com>
2026-04-24 12:59:42 +0900

    writing documents

--- docs/library/usbdev/sample/usbdev-msc/usbdev-msc.cpp
+++ docs/library/usbdev/sample/usbdev-msc/usbdev-msc.cpp
@@ -1,16 +1,39 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "jxglib/Serial.h"
 #include "jxglib/Shell.h"
+#include "jxglib/Serial.h"
+#include "jxglib/USBDevice/MSCDrive.h"
+#include "jxglib/FAT/Flash.h"
 
 using namespace jxglib;
 
-int main()
+int main(void)
 {
-    ::stdio_init_all();
-    // Prepare the Shell with Stdio
+    ::stdio_init_all(); 
+    // Prepare the shell
     Serial::Terminal terminal;
     Shell::AttachTerminal(terminal.Initialize());
+    // Prepare the USB Device Descriptor
+    USBDevice::Controller deviceController({
+            bcdUSB:				0x0200,
+            bDeviceClass:		0x00,		// Use Interface Class (Composite Device)
+            bDeviceSubClass:	0x00,		// Unused
+            bDeviceProtocol:	0x00,		// Unused
+            bMaxPacketSize0:	CFG_TUD_ENDPOINT0_SIZE,
+            idVendor:			0xcafe,
+            idProduct:			USBDevice::GenerateSpecificProductId(0x4000),
+            bcdDevice:			0x0100,
+        },
+        0x0409,					// Language: English (United States)
+        "RPi Flash",			// Manufacturer
+        "RPi Flash Device",		// Product
+        "3141592653"			// Serial Number (must be unique for each device)
+    );
+    // Prepare the FAT Drive
+    FAT::Flash fat("*Flash:", 0x010'0000);
+    // Prepare the MSC Drive with the addresses for the Bulk OUT and Bulk IN endpoints
+    USBDevice::MSCDrive mscDrive(deviceController, 0x01, 0x81);
+    // Initialize the USB Device and the MSC Drive
+    deviceController.Initialize();
+    mscDrive.Initialize(fat);
     for (;;) {
         Tickable::Tick();
     }
