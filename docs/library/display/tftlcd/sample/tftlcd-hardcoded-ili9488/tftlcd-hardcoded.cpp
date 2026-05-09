54f87933bc5c80b225e0c326e2e17c77c97550e0
ypsitau <ypsitau@nifty.com>
2026-05-09 15:24:30 +0900

    writing documents

---
+++ docs/library/display/tftlcd/sample/tftlcd-hardcoded-ili9488/tftlcd-hardcoded.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "jxglib/Shell.h"
+#include "jxglib/Serial.h"
+#include "jxglib/Display/ILI9488.h"
+#include "jxglib/DrawableTest.h"
+
+using namespace jxglib;
+
+ShellCmd_Named(draw_rotate, "draw-rotate", "Rotate the image")
+{
+    auto& display = Display::GetInstance(0);
+    DrawableTest::RotateImage(display);
+    return Result::Success;
+}
+
+ShellCmd_Named(draw_string, "draw-string", "Draw a string")
+{
+    auto& display = Display::GetInstance(0);
+    DrawableTest::DrawString(display);
+    return Result::Success;
+}
+
+ShellCmd_Named(draw_fonts, "draw-fonts", "Draw fonts")
+{
+    auto& display = Display::GetInstance(0);
+    DrawableTest::DrawFonts(display);
+    return Result::Success;
+}
+
+int main()
+{
+    ::stdio_init_all();
+    // Prepare the Shell with Stdio
+    Serial::Terminal terminal;
+    Shell::AttachTerminal(terminal.Initialize());
+    // Initialize the display
+    ::spi_init(spi1, 125 * 1000 * 1000);
+    GPIO14.set_function_SPI1_SCK();
+    GPIO15.set_function_SPI1_TX();
+    Display::ILI9488 display(spi1, 320, 480, {RST: GPIO10, DC: GPIO11, CS: GPIO12, BL: GPIO13});
+    display.Initialize(Display::Dir::Rotate0);
+    for (;;) {
+        Tickable::Tick();
+    }
+}
