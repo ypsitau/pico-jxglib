42b7411694b918f7e42e2743bb583665f43f4fc6
ypsitau <ypsitau@nifty.com>
2026-06-17 17:32:06 +0900

    implementing GPIO::KeyboardMatrix

--- src/Common/test-GPIO-KeyboardMatrix/test-GPIO-KeyboardMatrix.cpp
+++ src/Common/test-GPIO-KeyboardMatrix/test-GPIO-KeyboardMatrix.cpp
@@ -10,15 +10,17 @@ GPIO::KeyboardMatrix keyboard;
 int main()
 {
 	::stdio_init_all();
-	static const Keyboard::KeySet keySetTbl[] = {
-		VK_1,    VK_2, VK_3,     VK_BACK,
-		VK_4,    VK_5, VK_6,     VK_UP,
-		VK_7,    VK_8, VK_9,     VK_DOWN,
-		VK_LEFT, VK_0, VK_RIGHT, VK_RETURN, //{0, Keyboard::Mod::ShiftL},
-	};
-	static const GPIO::KeyRow keyRowTbl[] = { GPIO16, GPIO17, GPIO18, GPIO19 };
-	static const GPIO::KeyCol keyColTbl[] = { GPIO20, GPIO21, GPIO26, GPIO27 };
-	keyboard.Initialize(keySetTbl, keyRowTbl, count_of(keyRowTbl), keyColTbl, count_of(keyColTbl), GPIO::LogicNeg | GPIO::PullUp);
+	do {
+		static const Keyboard::KeySet keySetTbl[] = {
+			VK_1,    VK_2, VK_3,     VK_BACK,
+			VK_4,    VK_5, VK_6,     VK_UP,
+			VK_7,    VK_8, VK_9,     VK_DOWN,
+			VK_LEFT, VK_0, VK_RIGHT, VK_RETURN, //{0, Keyboard::Mod::ShiftL},
+		};
+		static const GPIO::KeyRow keyOutTbl[] = { GPIO16, GPIO17, GPIO18, GPIO19 };
+		static const GPIO::KeyCol keyInTbl[] = { GPIO20, GPIO21, GPIO26, GPIO27 };
+		keyboard.Initialize(keySetTbl, keyOutTbl, count_of(keyOutTbl), keyInTbl, count_of(keyInTbl), GPIO::LogicNeg | GPIO::PullUp);
+	} while (0);
 	Serial::Terminal terminal;
 	terminal.Initialize();
 	Shell::AttachTerminal(terminal);
